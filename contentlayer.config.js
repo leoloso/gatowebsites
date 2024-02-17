de820405e7bd58b6f5ea7bc3220a2cec0fca2ddf
leoloso <leo@getpop.org>
2024-02-17 13:28:44 +0800

    Added

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -26,7 +26,48 @@ const Update = defineDocumentType(() => ({
   },
 }))
 
+const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `blog/**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true
+    },
+    publishedAt: {
+      type: 'date',
+      required: true
+    },
+    summary: {
+      type: 'string',
+      required: true,
+    },
+    author: {
+      type: 'string',
+      required: true,
+    },
+    authorImg: {
+      type: 'string',
+      required: true,
+    },
+    tags: {
+      type: 'list',
+      of: { type: 'string' },
+    },
+    image: {
+      type: 'string',
+    },        
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (doc) => doc._raw.flattenedPath.replace(/blog\/?/, ''),
+    },    
+  },
+}))
+
 export default makeSource({
   contentDirPath: 'content',
-  documentTypes: [Update],
+  documentTypes: [Update, Post],
 })
