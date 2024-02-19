2d738e17507947945f805bd7c9cae8737f88d850
leoloso <leo@getpop.org>
2024-02-19 20:06:13 +0800

    simplify

--- app/(docs)/docs/page.tsx
+++ app/(docs)/docs/page.tsx
@@ -2,25 +2,17 @@ import { notFound, redirect } from 'next/navigation'
 import { allDocs } from 'contentlayer/generated'
 import { allDocTopics } from 'contentlayer/generated'
 import AppConfig from '@/app/app.config'
-import {
-  sortDocumentTopics,
-  sortDocuments,
-} from '@/components/utils/document'
+import { sortDocuments } from '@/components/utils/document'
 
 // Redirect to the first item on the docs collection
 export default function Home() {
 
-  // Sort docs and doc topics by order  
-  allDocTopics.sort(sortDocumentTopics)
+  if (allDocs.length === 0) notFound()
+
+  // Sort docs (this already takes into account the DocTopic)
   allDocs.sort(sortDocuments)
 
-  // First DocTopic
-  if (allDocTopics.length === 0) notFound()
-  const docTopic = allDocTopics[0]
-
-  // First Doc for that DocTopic
-  const doc = allDocs.find((doc) => doc.slug.startsWith(`${docTopic.slug}/`))
-  if (!doc) notFound()
-
+  // Redirect to the first one
+  const doc = allDocs[0];
   redirect(`/${AppConfig.paths.docs}/${doc.slug}`)
 }
