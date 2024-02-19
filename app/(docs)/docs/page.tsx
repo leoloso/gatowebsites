deb828923fae53924f5a1c22e97cd74194383132
leoloso <leo@getpop.org>
2024-02-19 17:06:59 +0800

    redirect to 1st item

--- app/(docs)/docs/page.tsx
+++ app/(docs)/docs/page.tsx
@@ -1,5 +1,25 @@
-import { redirect } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
+import { allDocs } from 'contentlayer/generated'
+import { allDocTopics } from 'contentlayer/generated'
 
+// Redirect to the first item on the docs collection
 export default function Home() {
-  redirect('/docs/documentation/fundamentals')
+
+  // Sort docs and doc topics by order
+  allDocs.sort((a, b) => {
+    return (new Date(a.order) > new Date(b.order)) ? -1 : 1
+  })  
+  allDocTopics.sort((a, b) => {
+    return (new Date(a.order) > new Date(b.order)) ? -1 : 1
+  })
+
+  // First DocTopic
+  if (allDocTopics.length === 0) notFound()
+  const docTopic = allDocTopics[0]
+
+  // First Doc for that DocTopic
+  const doc = allDocs.find((doc) => doc.topic.slug === docTopic.slug)
+  if (!doc) notFound()
+
+  redirect(`/docs/${doc.slug}`)
 }
