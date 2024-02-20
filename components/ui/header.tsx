9ad1d0f4f554dee04b01c020bb365933f4d39302
leoloso <leo@getpop.org>
2024-02-20 13:00:32 +0800

    2 level docs

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -3,6 +3,7 @@ import Logo from './logo'
 import MobileMenu from './mobile-menu'
 import Search from './search'
 import ThemeToggle from './theme-toggle'
+import Dropdown from './../utils/dropdown'
 
 interface HeaderProps {
   documentation?: boolean
@@ -37,12 +38,16 @@ export default function Header({
                   <li>
                     <Link className="font-medium text-sm text-slate-300 hover:text-white mx-4 lg:mx-5 transition duration-150 ease-in-out" href="/pricing">Pricing</Link>
                   </li>
-                  {/* <li>
-                    <Link className="font-medium text-sm text-slate-300 hover:text-white mx-4 lg:mx-5 transition duration-150 ease-in-out" href="/guides">How to guides</Link>
-                  </li> */}
-                  <li>
-                    <Link className="font-medium text-sm text-slate-300 hover:text-white mx-4 lg:mx-5 transition duration-150 ease-in-out" href="/docs">Documentation</Link>
-                  </li>
+                  {/* 1st level: hover */}
+                  <Dropdown title="Documentation">
+                    {/* 2nd level: hover */}
+                    <li>
+                      <Link className="font-medium text-sm text-slate-300 hover:text-white mx-4 lg:mx-5 transition duration-150 ease-in-out" href="/guides">How to guides</Link>
+                    </li>
+                    <li>
+                      <Link className="font-medium text-sm text-slate-300 hover:text-white mx-4 lg:mx-5 transition duration-150 ease-in-out" href="/docs">Reference docs</Link>
+                    </li>
+                  </Dropdown>
                 </>
               )}
               { documentation && (
