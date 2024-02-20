3e7aafb73a9fba5c9dc26ac9eb45e37891720904
leoloso <leo@getpop.org>
2024-02-20 15:14:57 +0800

    urls in AppConfig

--- app/app.config.js
+++ app/app.config.js
@@ -5,5 +5,14 @@ module.exports = {
     docs: "docs",
     docTopics: "doc-topics",
     guides: "guides"
+  },
+  urls: {
+    download: "https://downloads.wordpress.org/plugin/gatographql.latest-stable.zip",
+    sandboxDemo: "https://app.instawp.io/launch?t=gatographql-demo&d=v2",
+    wpDirectory: "https://wordpress.org/plugins/gatographql/",
+    affiliateProgram: "https://shop.gatographql.com/affiliates",
+    github: "https://github.com/GatoGraphQL/GatoGraphQL",
+    extensionStarter: "https://github.com/GatoGraphQL/ExtensionStarter",
+    clientOrders: "https://app.lemonsqueezy.com/my-orders"
   }
 }
