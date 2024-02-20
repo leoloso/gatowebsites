561ef78c4e46dfff75df316f907e476e16650df7
leoloso <leo@getpop.org>
2024-02-20 15:20:14 +0800

    renamed vars

--- app/app.config.js
+++ app/app.config.js
@@ -7,16 +7,16 @@ module.exports = {
     guides: "guides"
   },
   urls: {
-    download: "https://downloads.wordpress.org/plugin/gatographql.latest-stable.zip",
-    sandboxDemo: "https://app.instawp.io/launch?t=gatographql-demo&d=v2",
+    wpDownload: "https://downloads.wordpress.org/plugin/gatographql.latest-stable.zip",
+    instawpSandboxDemo: "https://app.instawp.io/launch?t=gatographql-demo&d=v2",
     wpDirectory: "https://wordpress.org/plugins/gatographql/",
-    affiliateProgram: "https://shop.gatographql.com/affiliates",
-    github: "https://github.com/GatoGraphQL/GatoGraphQL",
-    extensionStarter: "https://github.com/GatoGraphQL/ExtensionStarter",
-    clientOrders: "https://app.lemonsqueezy.com/my-orders",
-    purchase: "https://shop.gatographql.com/checkout/buy/834ead62-1125-4e96-9bcb-5deb8ea768f5?embed=1&logo=0&desc=1&dark=1",
-    purchaseVariation1: "https://shop.gatographql.com/checkout/buy/834ead62-1125-4e96-9bcb-5deb8ea768f5?embed=1&logo=0&desc=1&dark=1",
-    purchaseVariation2: "https://shop.gatographql.com/checkout/buy/e8254ca0-ba6b-4875-a090-e0557b59c7d1?embed=1&logo=0&desc=1&dark=1",
-    purchaseVariation3: "https://shop.gatographql.com/checkout/buy/017ca707-0114-4a9a-bf41-7afd278fec03?embed=1&logo=0&desc=1&dark=1"
+    shopAffiliateProgram: "https://shop.gatographql.com/affiliates",
+    githubRepo: "https://github.com/GatoGraphQL/GatoGraphQL",
+    githubExtensionStarter: "https://github.com/GatoGraphQL/ExtensionStarter",
+    shopClientOrders: "https://app.lemonsqueezy.com/my-orders",
+    shopPurchase: "https://shop.gatographql.com/checkout/buy/834ead62-1125-4e96-9bcb-5deb8ea768f5?embed=1&logo=0&desc=1&dark=1",
+    shopPurchaseVariation1: "https://shop.gatographql.com/checkout/buy/834ead62-1125-4e96-9bcb-5deb8ea768f5?embed=1&logo=0&desc=1&dark=1",
+    shopPurchaseVariation2: "https://shop.gatographql.com/checkout/buy/e8254ca0-ba6b-4875-a090-e0557b59c7d1?embed=1&logo=0&desc=1&dark=1",
+    shopPurchaseVariation3: "https://shop.gatographql.com/checkout/buy/017ca707-0114-4a9a-bf41-7afd278fec03?embed=1&logo=0&desc=1&dark=1"
   }
 }
