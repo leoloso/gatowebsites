9ae4a5c9a4e05e6f9ef4f09a5514c1df0ccfa812
leoloso <leo@getpop.org>
2024-02-20 15:19:04 +0800

    purchase URLs

--- app/app.config.js
+++ app/app.config.js
@@ -13,6 +13,10 @@ module.exports = {
     affiliateProgram: "https://shop.gatographql.com/affiliates",
     github: "https://github.com/GatoGraphQL/GatoGraphQL",
     extensionStarter: "https://github.com/GatoGraphQL/ExtensionStarter",
-    clientOrders: "https://app.lemonsqueezy.com/my-orders"
+    clientOrders: "https://app.lemonsqueezy.com/my-orders",
+    purchase: "https://shop.gatographql.com/checkout/buy/834ead62-1125-4e96-9bcb-5deb8ea768f5?embed=1&logo=0&desc=1&dark=1",
+    purchaseVariation1: "https://shop.gatographql.com/checkout/buy/834ead62-1125-4e96-9bcb-5deb8ea768f5?embed=1&logo=0&desc=1&dark=1",
+    purchaseVariation2: "https://shop.gatographql.com/checkout/buy/e8254ca0-ba6b-4875-a090-e0557b59c7d1?embed=1&logo=0&desc=1&dark=1",
+    purchaseVariation3: "https://shop.gatographql.com/checkout/buy/017ca707-0114-4a9a-bf41-7afd278fec03?embed=1&logo=0&desc=1&dark=1"
   }
 }
