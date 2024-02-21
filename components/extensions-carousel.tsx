f27f65b77884fc6fac66dc17dbe62ab5c6225fb3
leoloso <leo@getpop.org>
2024-02-21 13:27:31 +0800

    removed stale code

--- components/extensions-carousel.tsx
+++ components/extensions-carousel.tsx
@@ -60,35 +60,6 @@ export default function ExtensionsCarousel({ extensions }: ExtensionsSectionProp
             {extensions.map((extension, extensionIndex) => (
               <ExtensionCard index={extensionIndex} extension={extension} />
             ))}
-            <div className="bg-gradient-to-tr from-slate-800 to-slate-800/25 rounded-3xl border border-slate-800 hover:border-slate-700/60 transition-colors group relative">
-              <div className="flex flex-col p-5 h-full">
-                <div className="flex items-center space-x-3 mb-3">
-                  <div className="relative">
-                    <Image src={IntegrationImg01} width={40} height={40} alt="Icon 01" />
-                    <Image className="absolute top-0 -right-1" src={Star} width={16} height={16} alt="Star" aria-hidden="true" />
-                  </div>
-                  <Link className="font-semibold bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 group-hover:before:absolute group-hover:before:inset-0" href="/extensions/single-post">Retool</Link>
-                </div>
-                <div className="grow mb-4">
-                  <div className="text-sm text-slate-400">Stellar makes it easy to build extensions by providing an authentication provider that handles the OAuth flow.</div>
-                </div>
-                <div className="flex justify-between">
-                  <div className="flex -space-x-3 -ml-0.5">
-                    <Image className="rounded-full border-2 border-slate-800 box-content" src={Avatar01} width={24} height={24} alt="Avatar 01" />
-                    <Image className="rounded-full border-2 border-slate-800 box-content" src={Avatar02} width={24} height={24} alt="Avatar 02" />
-                    <Image className="rounded-full border-2 border-slate-800 box-content" src={Avatar03} width={24} height={24} alt="Avatar 03" />
-                    <Image className="rounded-full border-2 border-slate-800 box-content" src={Avatar04} width={24} height={24} alt="Avatar 04" />
-                  </div>
-                  <button className="flex items-center space-x-2 font-medium text-sm text-slate-300 hover:text-white transition-colors">
-                    <span className="sr-only">Like</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
-                      <path className="fill-slate-500" d="M11.86 15.154 8 13.125l-3.86 2.03c-.726.386-1.591-.236-1.45-1.055l.737-4.299L.303 6.757a1 1 0 0 1 .555-1.706l4.316-.627L7.104.512c.337-.683 1.458-.683 1.794 0l1.93 3.911 4.317.627a1.001 1.001 0 0 1 .555 1.706l-3.124 3.045.737 4.3c.14.822-.726 1.435-1.452 1.053ZM8.468 11.11l2.532 1.331-.483-2.82a1 1 0 0 1 .287-.885l2.049-1.998-2.831-.41a.996.996 0 0 1-.753-.548L8 3.214 6.734 5.78a1 1 0 0 1-.753.547l-2.831.411 2.049 1.998a1 1 0 0 1 .287.885l-.483 2.82 2.532-1.33a.998.998 0 0 1 .932 0Z" />
-                    </svg>
-                    <span>2.3K</span>
-                  </button>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
