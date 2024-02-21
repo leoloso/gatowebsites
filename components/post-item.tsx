fcd114fb212d51c2f8c252d8a3e76ce32527ade3
leoloso <leo@getpop.org>
2024-02-21 14:45:39 +0800

    getPostURL

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import PostDate from '@/components/post-date'
 import { Post } from '@/.contentlayer/generated'
+import { getPostURL } from '@/utils/application-urls'
 
 export default function PostItem({ post }: {
   post: Post
@@ -11,7 +12,7 @@ export default function PostItem({ post }: {
       <div>
         <header>
           <h2 className="h4 mb-2">
-            <Link href={`/blog/${post.slug}`} className="hover:underline">{post.title}</Link>
+            <Link href={getPostURL(post)} className="hover:underline">{post.title}</Link>
           </h2>
         </header>
         <div className="text-lg text-gray-600 mb-4">{post.summary}</div>
@@ -31,7 +32,7 @@ export default function PostItem({ post }: {
           </div>
         </footer>
       </div>
-      <Link href={`/blog/${post.slug}`} className="block shrink-0 ml-6">
+      <Link href={getPostURL(post)} className="block shrink-0 ml-6">
         <span className="sr-only">Read more</span>
         <svg className="w-4 h-4 fill-current text-blue-600" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
           <path d="M9.3 14.7l-1.4-1.4L12.2 9H0V7h12.2L7.9 2.7l1.4-1.4L16 8z" />
