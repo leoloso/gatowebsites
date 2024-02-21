4c7a0a9f169e2ad4dae40f8ba6aa534042e7bc4e
leoloso <leo@getpop.org>
2024-02-21 19:25:53 +0800

    added Newsletter to single post and guide

--- app/(default)/guides/[slug]/page.tsx
+++ app/(default)/guides/[slug]/page.tsx
@@ -9,6 +9,7 @@ import PostNav from './post-nav'
 import PostTags from '@/components/post-tags'
 import AppConfig from '@/app/app.config'
 import StunningBackground from '@/components/stunning-background'
+import Newsletter from '@/components/newsletter'
 
 export async function generateStaticParams() {
   return allGuides.map((guide) => ({
@@ -41,86 +42,89 @@ export default async function SinglePost({ params }: {
   if (!guide) notFound()
 
   return (
-    <section className="relative">
+    <>
+      <section className="relative">
 
-      <StunningBackground />
-      
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-          <div className="max-w-3xl mx-auto lg:max-w-none">
+        <StunningBackground />
+        
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+            <div className="max-w-3xl mx-auto lg:max-w-none">
 
-            <article>
+              <article>
 
-              {/* Article header */}
-              <header className="max-w-3xl mx-auto mb-20">
-                {/* Title */}
-                <h1 className="h1 text-center mb-4">{guide.title}</h1>
-              </header>
+                {/* Article header */}
+                <header className="max-w-3xl mx-auto mb-20">
+                  {/* Title */}
+                  <h1 className="h1 text-center mb-4">{guide.title}</h1>
+                </header>
 
-              {/* Article content */}
-              <div className="lg:flex lg:justify-between">
+                {/* Article content */}
+                <div className="lg:flex lg:justify-between">
 
-                {/* Sidebar */}
-                <PostNav />
+                  {/* Sidebar */}
+                  <PostNav />
 
-                {/* Main content */}
-                <div>
-
-                  {/* Article meta */}
-                  <div className="md:flex md:items-center md:justify-between mt-3">
-                    <div className="flex items-center mb-6">
-                      <div className="flex shrink-0 mr-3">
-                        <a className="relative" href="#0">
-                          <span className="absolute inset-0 -m-px" aria-hidden="true"><span className="absolute inset-0 -m-px bg-white rounded-full"></span></span>
-                          <Image className="relative rounded-full" src={guide.authorImg} width={32} height={32} alt={guide.author} />
-                        </a>
-                      </div>
-                      <div>
-                        <span className="text-gray-600">By </span>
-                        <a className="font-medium hover:underline" href="#0">{guide.author}</a>
-                        <span className="text-gray-600"> · <PostDate dateString={guide.publishedAt} /></span>
-                      </div>
-                    </div>
-                    {/* Article tags */}
-                    {guide.tags &&
-                      <div className="flex justify-center mt-4 md:mt-0 items-center mb-6">
-                        <PostTags tags={guide.tags} />
-                      </div>
-                    }
-                  </div>
-                  <hr className="w-16 h-px pt-px bg-gray-200 border-0 mb-6" />
-
-                  {/* Article body */}
+                  {/* Main content */}
                   <div>
-                    <PostMdx code={guide.body.code} />
-                  </div>
 
-                  <div className="text-lg text-gray-600">
-                    <hr className="w-full h-px pt-px mt-8 bg-gray-200 border-0" />
-                    <div className="mt-8">
-                      Interested in more tips like this? Check out <a className="text-gray-500 underline" href="#0">Introducing the Testing Field Guide</a>.
+                    {/* Article meta */}
+                    <div className="md:flex md:items-center md:justify-between mt-3">
+                      <div className="flex items-center mb-6">
+                        <div className="flex shrink-0 mr-3">
+                          <a className="relative" href="#0">
+                            <span className="absolute inset-0 -m-px" aria-hidden="true"><span className="absolute inset-0 -m-px bg-white rounded-full"></span></span>
+                            <Image className="relative rounded-full" src={guide.authorImg} width={32} height={32} alt={guide.author} />
+                          </a>
+                        </div>
+                        <div>
+                          <span className="text-gray-600">By </span>
+                          <a className="font-medium hover:underline" href="#0">{guide.author}</a>
+                          <span className="text-gray-600"> · <PostDate dateString={guide.publishedAt} /></span>
+                        </div>
+                      </div>
+                      {/* Article tags */}
+                      {guide.tags &&
+                        <div className="flex justify-center mt-4 md:mt-0 items-center mb-6">
+                          <PostTags tags={guide.tags} />
+                        </div>
+                      }
                     </div>
-                    <div className="mt-6">
-                      <Link href={`/${AppConfig.paths.guides}`} className="inline-flex items-center text-base text-blue-600 font-medium hover:underline">
-                        <svg className="w-3 h-3 fill-current text-blue-400 shrink-0 mr-2" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M.293 5.282L5 .5l1.414 1.436-3 3.048H12v2.032H3.414l3 3.048L5 11.5.293 6.718a1.027 1.027 0 010-1.436z" />
-                        </svg>
-                        <span>Back to the guides</span>
-                      </Link>
+                    <hr className="w-16 h-px pt-px bg-gray-200 border-0 mb-6" />
+
+                    {/* Article body */}
+                    <div>
+                      <PostMdx code={guide.body.code} />
                     </div>
-                  </div>                  
+
+                    <div className="text-lg text-gray-600">
+                      <hr className="w-full h-px pt-px mt-8 bg-gray-200 border-0" />
+                      <div className="mt-8">
+                        Interested in more tips like this? Check out <a className="text-gray-500 underline" href="#0">Introducing the Testing Field Guide</a>.
+                      </div>
+                      <div className="mt-6">
+                        <Link href={`/${AppConfig.paths.guides}`} className="inline-flex items-center text-base text-blue-600 font-medium hover:underline">
+                          <svg className="w-3 h-3 fill-current text-blue-400 shrink-0 mr-2" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M.293 5.282L5 .5l1.414 1.436-3 3.048H12v2.032H3.414l3 3.048L5 11.5.293 6.718a1.027 1.027 0 010-1.436z" />
+                          </svg>
+                          <span>Back to the guides</span>
+                        </Link>
+                      </div>
+                    </div>                  
+
+                  </div>
 
                 </div>
 
-              </div>
+                {/* Article footer */}
+              </article>
 
-              {/* Article footer */}
-            </article>
+            </div>
 
           </div>
-
         </div>
-      </div>
-    </section>
+      </section>
+      <Newsletter />
+    </>
   )
 }
