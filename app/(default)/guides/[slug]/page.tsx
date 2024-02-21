3004e2b34a7b299ddc2b83c445cee29c4b7d90cd
leoloso <leo@getpop.org>
2024-02-21 19:19:43 +0800

    Added <StunningBackground /> to guides

--- app/(default)/guides/[slug]/page.tsx
+++ app/(default)/guides/[slug]/page.tsx
@@ -8,6 +8,7 @@ import { PostMdx } from '@/components/mdx/post-mdx'
 import PostNav from './post-nav'
 import PostTags from '@/components/post-tags'
 import AppConfig from '@/app/app.config'
+import StunningBackground from '@/components/stunning-background'
 
 export async function generateStaticParams() {
   return allGuides.map((guide) => ({
@@ -41,6 +42,9 @@ export default async function SinglePost({ params }: {
 
   return (
     <section>
+
+      <StunningBackground />
+      
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           <div className="max-w-3xl mx-auto lg:max-w-none">
