26dd8dcfa700f47978a383b07807f0b2216feb0f
leoloso <leo@getpop.org>
2024-02-21 13:34:33 +0800

    fixed and moved sort

--- app/(default)/extensions/page.tsx
+++ app/(default)/extensions/page.tsx
@@ -15,7 +15,7 @@ export const metadata = {
 export default function Integrations() {
   // Sort extensions
   allExtensions.sort(sortExtensions)  
-  const featuredExtensions = getFeaturedExtensions().sort(sortExtensions)  
+  const featuredExtensions = getFeaturedExtensions()
   return (
     <>
       <ExtensionsSection
--- components/utils/extension.tsx
+++ components/utils/extension.tsx
@@ -2,10 +2,10 @@ import { Extension } from "@/.contentlayer/generated";
 import { allExtensions } from 'contentlayer/generated'
 
 export function getFeaturedExtensions() {
-  return allExtensions.filter((extension) => extension.featured)
+  return allExtensions.filter((extension) => extension.featured).sort(sortExtensions)
 }
 
 // Sort extensions by name
 export function sortExtensions(a: Extension, b: Extension) {
-  return (a.name > b.name) ? -1 : 1
+  return (a.name > b.name) ? 1 : -1
 }
