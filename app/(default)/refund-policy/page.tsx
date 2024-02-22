979e4ebbc37ce5032cab4862586c37b7de6d4abf
leoloso <leo@getpop.org>
2024-02-22 11:16:26 +0800

    PageSection

--- app/(default)/refund-policy/page.tsx
+++ app/(default)/refund-policy/page.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
 import { allPages } from 'contentlayer/generated'
 import { notFound } from 'next/navigation'
-import { PostMdx } from '@/components/mdx/post-mdx'
-import StunningBackground from '@/components/stunning-background'
+import PageSection from '@/components/sections/page'
 
 export async function generateStaticParams() {
   return allPages.map((page) => ({
@@ -33,32 +32,6 @@ export default async function SinglePage() {
   if (!page) notFound()
 
   return (
-    <section className="relative">
-
-      <StunningBackground />
-
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-          <div className="max-w-3xl mx-auto">
-
-            <article>
-
-              <header className="mb-8">
-                {/* Title and excerpt */}
-                <div className="text-center md:text-left">
-                  <h1 className="h1 mb-4" data-aos="fade-up">{page.title}</h1>
-                  <p className="text-xl text-gray-400" data-aos="fade-up" data-aos-delay="200">{page.summary}</p>
-                </div>
-              </header>
-
-              {/* Article content */}
-              <PostMdx code={page.body.code} />
-            </article>
-
-          </div>
-
-        </div>
-      </div>
-    </section>
+    <PageSection page={page} />
   )
 }
