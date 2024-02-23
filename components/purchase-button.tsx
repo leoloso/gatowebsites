da63755d51769b1729596fd1a74c9054d4e02a4c
leoloso <leo@getpop.org>
2024-02-23 15:54:54 +0800

    light purchase btn

--- components/purchase-button.tsx
+++ components/purchase-button.tsx
@@ -3,9 +3,9 @@ import AppConfig from '@/app/app.config'
 
 export default function PurchaseButton() {
   return (
-    <Link className="btn-sm text-slate-300 hover:text-white transition duration-150 ease-in-out w-full group [background:linear-gradient(theme(colors.purple.900),_theme(colors.purple.900))_padding-box,_conic-gradient(theme(colors.purple.400),_theme(colors.purple.700)_25%,_theme(colors.purple.700)_75%,_theme(colors.purple.400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none" href={AppConfig.urls.shopPurchase}>
+    <Link className="btn-sm text-white hover:text-purple-100 dark:text-slate-300 dark:hover:text-white transition duration-150 ease-in-out w-full group [background:linear-gradient(theme(colors.purple.500),_theme(colors.purple.500))_padding-box,_conic-gradient(theme(colors.purple.100),_theme(colors.purple.400)_25%,_theme(colors.purple.400)_75%,_theme(colors.purple.100)_100%)_border-box] dark:[background:linear-gradient(theme(colors.purple.900),_theme(colors.purple.900))_padding-box,_conic-gradient(theme(colors.purple.400),_theme(colors.purple.700)_25%,_theme(colors.purple.700)_75%,_theme(colors.purple.400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none" href={AppConfig.urls.shopPurchase}>
       <span className="relative inline-flex items-center">
-        Go PRO <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+        Go PRO <span className="tracking-normal text-purple-100 dark:text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
       </span>
     </Link>
   )
