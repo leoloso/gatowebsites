61ab912e06705d1e9c7cc948a37d4e8ca6bed8c9
leoloso <leo@getpop.org>
2024-02-23 14:26:59 +0800

    showSearchInput

--- components/ui/search.tsx
+++ components/ui/search.tsx
@@ -3,7 +3,11 @@
 import { useState, useEffect } from 'react'
 import SearchModal from './search-modal'
 
-export default function Search() {
+export default function Search({
+  showSearchInput = true,
+}: {
+  showSearchInput?: boolean
+}) {
 
   const [searchModalOpen, setSearchModalOpen] = useState<boolean>(false)
 
@@ -19,9 +23,9 @@ export default function Search() {
   }, [searchModalOpen])
 
   return (
-    <div className="grow ml-4 md:ml-8">
+    <div className={showSearchInput ? 'grow ml-4 md:ml-8' : ''}>
       <button
-        className="w-[200px] sm:w-[340px] text-[15px] bg-white text-slate-400 inline-flex items-center justify-between leading-5 pl-3 pr-2 py-[7px] rounded border border-slate-200 hover:border-slate-300 shadow-sm whitespace-nowrap dark:text-slate-500 dark:bg-slate-800 dark:border-slate-700 dark:hover:border-slate-600"
+        className={`${showSearchInput ? 'w-[200px] sm:w-[340px] text-[15px] pr-2 ' : '' } bg-white text-slate-400 inline-flex items-center justify-between leading-5 pl-3 py-[7px] rounded border border-slate-200 hover:border-slate-300 shadow-sm whitespace-nowrap dark:text-slate-500 dark:bg-slate-800 dark:border-slate-700 dark:hover:border-slate-600`}
         onClick={() => { setSearchModalOpen(true) }}
       >
         <div className="flex items-center justify-center">
@@ -34,13 +38,17 @@ export default function Search() {
           >
             <path d="m14.707 13.293-1.414 1.414-2.4-2.4 1.414-1.414 2.4 2.4ZM6.8 12.6A5.8 5.8 0 1 1 6.8 1a5.8 5.8 0 0 1 0 11.6Zm0-2a3.8 3.8 0 1 0 0-7.6 3.8 3.8 0 0 0 0 7.6Z" />
           </svg>
-          <span>
-            Search<span className="hidden sm:inline"> for anything</span>…
-          </span>
+          {showSearchInput && (
+            <span>
+              Search<span className="hidden sm:inline"> for anything</span>…
+            </span>
+          )}
         </div>
-        <div className="flex items-center justify-center h-5 w-5 font-medium text-slate-500 rounded border border-slate-200 shadow-sm ml-3 dark:bg-slate-700 dark:text-slate-400 dark:border-slate-600">
-          /
-        </div>
+        {showSearchInput && (
+          <div className="flex items-center justify-center h-5 w-5 font-medium text-slate-500 rounded border border-slate-200 shadow-sm ml-3 dark:bg-slate-700 dark:text-slate-400 dark:border-slate-600">
+            /
+          </div>
+        )}
       </button>
       <div>
         <SearchModal isOpen={searchModalOpen} setIsOpen={setSearchModalOpen} />
