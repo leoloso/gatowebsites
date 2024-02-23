9a70ecf8074ec3f50bae7771afece4a719a95249
leoloso <leo@getpop.org>
2024-02-23 16:39:05 +0800

    blue line

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -84,7 +84,7 @@ export default function MobileMenu({
           <li>
             <Link className="flex font-medium text-sm text-slate-600 hover:text-slate-900 dark:text-slate-300 dark:hover:text-white py-1.5" href={`/${AppConfig.paths.videoPosts}`}>Videos</Link>
           </li>
-          <li className="py-2 my-2 border-t border-b border-gray-500 dark:border-gray-700">
+          <li className="py-2 my-2 border-t border-b border-blue-300 dark:border-gray-700">
             <span className="flex font-medium text-sm text-slate-600 hover:text-slate-900 dark:text-slate-300 dark:hover:text-white py-1.5">Documentation</span>
             <ul className="pl-4">
               <li>
