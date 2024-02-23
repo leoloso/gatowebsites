22a7265845f3f7c59748989f8dfee7ebe00979fd
leoloso <leo@getpop.org>
2024-02-23 17:58:36 +0800

    Swap single post for Videos and Blog

--- app/(default)/videos/[slug]/page.tsx
+++ app/(default)/videos/[slug]/page.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from 'next'
 import { allVideoPosts } from 'contentlayer/generated'
 import { notFound } from 'next/navigation'
-import Image from 'next/image'
 import Link from 'next/link'
+import Image from 'next/image'
 import PostDate from '@/components/post-date'
-import { PostMdx } from '@/components/mdx/post-mdx'
-import PostNav from './post-nav'
 import PostTags from '@/components/post-tags'
-import AppConfig from '@/app/app.config'
+import { PostMdx } from '@/components/mdx/post-mdx'
 import StunningBackground from '@/components/stunning-background'
 import Newsletter from '@/components/newsletter'
 
@@ -33,7 +31,7 @@ export async function generateMetadata({ params }: {
   }
 }
 
-export default async function SingleVideoPost({ params }: {
+export default async function SinglePost({ params }: {
   params: { slug: string }
 }) {
 
@@ -46,77 +44,80 @@ export default async function SingleVideoPost({ params }: {
       <section className="relative">
 
         <StunningBackground />
-        
+
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
           <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-            <div className="max-w-3xl mx-auto lg:max-w-none">
+            <div className="max-w-3xl mx-auto">
 
               <article>
 
-                {/* Article header */}
-                <header className="max-w-3xl mx-auto mb-20">
-                  {/* Title */}
-                  <h1 className="h1 text-center mb-4">{videoPost.title}</h1>
+                <header className="mb-8">
+                  {/* Title and excerpt */}
+                  <div className="text-center md:text-left">
+                    <h1 className="h1 mb-4" data-aos="fade-up">{videoPost.title}</h1>
+                    <p className="text-xl text-gray-400" data-aos="fade-up" data-aos-delay="200">{videoPost.summary}</p>
+                  </div>
+                  {/* Article meta */}
+                  <div className="md:flex md:items-center md:justify-between mt-3">
+                    {/* Author meta */}
+                    <div className="flex items-center justify-center" data-aos="fade-up" data-aos-delay="400">
+                      <Link href="#">
+                        <Image className="rounded-full shrink-0 mr-4" src={videoPost.authorImg} width={40} height={40} alt={videoPost.author} />
+                      </Link>
+                      <div>
+                        <Link href="#" className="font-medium text-gray-200 hover:text-gray-100 transition duration-150 ease-in-out">{videoPost.author}</Link>
+                        <span className="text-gray-700"> - </span>
+                        <span className="text-gray-500"><PostDate dateString={videoPost.publishedAt} /></span>
+                      </div>
+                    </div>
+                    {/* Article tags */}
+                    {videoPost.tags &&
+                      <div className="flex justify-center mt-4 md:mt-0" data-aos="fade-up" data-aos-delay="600">
+                        <PostTags tags={videoPost.tags} />
+                      </div>
+                    }
+                  </div>
                 </header>
 
+                {/* Article image */}
+                {videoPost.image &&
+                  <figure className="mb-8 lg:-ml-32 lg:-mr-32" data-aos="fade-up" data-aos-delay="600">
+                    <Image className="w-full" src={videoPost.image} width={1024} height={576} alt={videoPost.title} priority />
+                  </figure>
+                }
+
                 {/* Article content */}
-                <div className="lg:flex lg:justify-between">
-
-                  {/* Sidebar */}
-                  <PostNav />
-
-                  {/* Main content */}
-                  <div>
-
-                    {/* Article meta */}
-                    <div className="md:flex md:items-center md:justify-between mt-3">
-                      <div className="flex items-center mb-6">
-                        <div className="flex shrink-0 mr-3">
-                          <a className="relative" href="#0">
-                            <span className="absolute inset-0 -m-px" aria-hidden="true"><span className="absolute inset-0 -m-px bg-white rounded-full"></span></span>
-                            <Image className="relative rounded-full" src={videoPost.authorImg} width={32} height={32} alt={videoPost.author} />
-                          </a>
-                        </div>
-                        <div>
-                          <span className="text-gray-600">By </span>
-                          <a className="font-medium hover:underline" href="#0">{videoPost.author}</a>
-                          <span className="text-gray-600"> · <PostDate dateString={videoPost.publishedAt} /></span>
-                        </div>
-                      </div>
-                      {/* Article tags */}
-                      {videoPost.tags &&
-                        <div className="flex justify-center mt-4 md:mt-0 items-center mb-6">
-                          <PostTags tags={videoPost.tags} />
-                        </div>
-                      }
-                    </div>
-                    <hr className="w-16 h-px pt-px bg-gray-200 border-0 mb-6" />
-
-                    {/* Article body */}
-                    <div>
-                      <PostMdx code={videoPost.body.code} />
-                    </div>
-
-                    <div className="text-lg text-gray-600">
-                      <hr className="w-full h-px pt-px mt-8 bg-gray-200 border-0" />
-                      <div className="mt-8">
-                        Interested in more tips like this? Check out <a className="text-gray-500 underline" href="#0">Introducing the Testing Field VideoPost</a>.
-                      </div>
-                      <div className="mt-6">
-                        <Link href={`/${AppConfig.paths.videoPosts}`} className="inline-flex items-center text-base text-blue-600 font-medium hover:underline">
-                          <svg className="w-3 h-3 fill-current text-blue-400 shrink-0 mr-2" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M.293 5.282L5 .5l1.414 1.436-3 3.048H12v2.032H3.414l3 3.048L5 11.5.293 6.718a1.027 1.027 0 010-1.436z" />
-                          </svg>
-                          <span>Back to the videoPosts</span>
-                        </Link>
-                      </div>
-                    </div>                  
-
-                  </div>
-
-                </div>
+                <PostMdx code={videoPost.body.code} />
 
                 {/* Article footer */}
+                <footer>
+                  <div className="md:flex md:items-center md:justify-between text-center md:text-left">
+                    <div className="text-lg text-gray-400 italic">Originally published at <Link href="https://cruip.com/" className="text-gray-200">https://cruip.com/</Link>.</div>
+                    <ul className="inline-flex mt-4 md:ml-4 md:mb-0">
+                      <li>
+                        <Link href="#" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out">
+                          <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                            <path d="m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z" />
+                          </svg>
+                        </Link>
+                      </li>
+                      <li className="ml-4">
+                        <Link href="#" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out">
+                          <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
+                          </svg>
+                        </Link>
+                      </li>
+                      <li className="ml-4">
+                        <Link href="#" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out">
+                          <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z" />
+                          </svg>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </footer>
               </article>
 
             </div>
@@ -124,7 +125,7 @@ export default async function SingleVideoPost({ params }: {
           </div>
         </div>
       </section>
-      <Newsletter label="Want more tutorials & guides?" />
+      <Newsletter label="Want more news & posts?"/>
     </>
   )
 }
