bec4ca4dc81f52dc561108219ff7e9660561f373
leoloso <leo@getpop.org>
2024-02-23 18:01:13 +0800

    fixed link

--- app/(default)/blog/[slug]/page.tsx
+++ app/(default)/blog/[slug]/page.tsx
@@ -103,7 +103,7 @@ export default async function SingleVideoPost({ params }: {
                         Interested in more tips like this? Check out <a className="text-gray-500 underline" href="#0">Introducing the Testing Field VideoPost</a>.
                       </div>
                       <div className="mt-6">
-                        <Link href={`/${AppConfig.paths.posts}`} className="inline-flex items-center text-base text-blue-600 font-medium hover:underline">
+                        <Link href={`/${AppConfig.paths.blog}`} className="inline-flex items-center text-base text-blue-600 font-medium hover:underline">
                           <svg className="w-3 h-3 fill-current text-blue-400 shrink-0 mr-2" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                             <path d="M.293 5.282L5 .5l1.414 1.436-3 3.048H12v2.032H3.414l3 3.048L5 11.5.293 6.718a1.027 1.027 0 010-1.436z" />
                           </svg>
