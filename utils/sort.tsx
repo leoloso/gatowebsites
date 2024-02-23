96928975b283d4f7d7e5d804100c8b505a1f4b2c
leoloso <leo@getpop.org>
2024-02-23 14:06:33 +0800

    sortByTitle

--- utils/sort.tsx
+++ utils/sort.tsx
@@ -2,6 +2,10 @@ export function sortByName(a: { name: string }, b: { name: string }) {
   return (a.name > b.name) ? 1 : -1
 }
 
+export function sortByTitle(a: { title: string }, b: { title: string }) {
+  return (a.title > b.title) ? 1 : -1
+}
+
 export function sortByOrder(a: { order: number }, b: { order: number }) {
   return (a.order > b.order) ? 1 : -1
 }
