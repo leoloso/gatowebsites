985b3e13a404cf64f00f74a31f9efd4925e5264b
leoloso <leo@getpop.org>
2024-02-23 17:50:22 +0800

    SortByNameProps

--- utils/sort.tsx
+++ utils/sort.tsx
@@ -2,7 +2,11 @@ export function sortAlphabetically(a: string, b: string) {
   return (a > b) ? 1 : -1
 }
 
-export function sortByName(a: { name: string }, b: { name: string }) {
+type SortByNameProps = {
+  name: string,
+}
+
+export function sortByName(a: SortByNameProps, b: SortByNameProps) {
   return (a.name > b.name) ? 1 : -1
 }
 
