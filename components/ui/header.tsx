005599df97c4ae81b651752f0453102fbf985f48
leoloso <leo@getpop.org>
2024-02-26 16:33:15 +0800

    smaller header

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -19,7 +19,7 @@ export default function Header({
         aria-hidden="true"
       />
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="flex items-center justify-between h-16 md:h-20">
+        <div className="flex items-center justify-between h-16">
 
           {/* Site branding */}
           <div className="flex-1">
