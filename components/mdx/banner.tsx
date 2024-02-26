a277a356f4acb0a3911078cdc7c04adbc7e7ed6e
leoloso <leo@getpop.org>
2024-02-26 14:30:23 +0800

    <Banner type="success">

--- components/mdx/banner.tsx
+++ components/mdx/banner.tsx
@@ -19,9 +19,15 @@ export default function PostBanner({ type, ...props }: PostBannerProps) {
             <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8Zm0 12c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1Zm1-3H7V4h2v5Z" />
           </svg>
         );
-      default:
+      case 'success':
         return (
           <svg className="fill-teal-500 shrink-0 mr-4" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8ZM7 11.4 3.6 8 5 6.6l2 2 4-4L12.4 6 7 11.4Z" />
+          </svg>
+        );
+      default:
+        return (
+          <svg className="fill-yellow-500 shrink-0 mr-4" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
             <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8ZM7 11.4 3.6 8 5 6.6l2 2 4-4L12.4 6 7 11.4Z" />
           </svg>
         )
