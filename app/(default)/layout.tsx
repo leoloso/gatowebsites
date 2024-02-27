95eafe0751db9c79dd8387ce519c6842c0cb14e1
leoloso <leo@getpop.org>
2024-02-27 15:38:18 +0800

    Added light/dark mode on default layout

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -7,6 +7,8 @@ import 'aos/dist/aos.css'
 
 import Footer from '@/components/ui/footer'
 import Header from '@/components/ui/header'
+import Theme from '../(docs)/theme-provider'
+import AppProvider from '../(docs)/app-provider'
 
 export default function DefaultLayout({
   children,
@@ -24,16 +26,22 @@ export default function DefaultLayout({
   })
 
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip dark">
-      <Header />
-      
-      <main className="grow">
+    <div className={`text-slate-800 bg-white dark:bg-transparent dark:text-slate-200`}>
+      <Theme>
+        <AppProvider>
+          <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
+            <Header enableLightDarkVersionToggleMode={true} />
+            
+            <main className="grow">
 
-        {children}
+              {children}
 
-      </main>
+            </main>
 
-      <Footer />
+            <Footer />
+          </div>
+        </AppProvider>
+      </Theme>
     </div>
   )
 }
