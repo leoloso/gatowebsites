e2e65d3842c69ac4486972e5cdc895668777cf0e
leoloso <leo@getpop.org>
2024-02-27 14:35:11 +0800

    fix font color on search input

--- components/ui/search-modal.tsx
+++ components/ui/search-modal.tsx
@@ -54,7 +54,7 @@ export default function SearchModal({
                 </label>
                 <input
                   id="search-modal"
-                  className="text-sm w-full bg-white border-0 focus:ring-transparent placeholder-slate-400 appearance-none py-3 pl-2 pr-4 dark:bg-slate-800 dark:placeholder:text-slate-500"
+                  className="text-sm text-slate-700 dark:text-slate-200 w-full bg-white border-0 focus:ring-transparent placeholder-slate-400 appearance-none py-3 pl-2 pr-4 dark:bg-slate-800 dark:placeholder:text-slate-500"
                   type="search"
                   placeholder={placeholder}
                 />
