452ae6ef897350c782abc01adea4eb76fffaa292
leoloso <leo@getpop.org>
2024-02-28 12:04:05 +0800

    default params

--- components/extensions-list.tsx
+++ components/extensions-list.tsx
@@ -4,18 +4,14 @@ import { allExtensions } from 'contentlayer/generated'
 
 export default function ExtensionsList({
   extensions = allExtensions,
-  showTopbar = true,
-  showSearch = false,
 }: {
   extensions?: Array<Extension>,
-  showTopbar?: boolean
-  showSearch?: boolean
 }) {
   return (
     <ArtifactsList
       artifacts={extensions}
-      showTopbar={showTopbar}
-      showSearch={showSearch}
+      showTopbar={true}
+      showSearch={false}
     />
   )
 }
