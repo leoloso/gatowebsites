11134db2c39f1f02a004617b20896d24e35ff081
leoloso <leo@getpop.org>
2024-02-28 16:43:48 +0800

    migrated

--- components/features-list.tsx
+++ components/features-list.tsx
@@ -1,6 +1,7 @@
 import { sortByOrderAndTitle } from '@/utils/sort';
 import ArtifactsList from './artifacts-list';
 import { allFeatures } from 'contentlayer/generated'
+import SectionHeader from './section-header';
 
 export default function FeaturesList() {
   // Sort features
@@ -25,13 +26,11 @@ export default function FeaturesList() {
         addRadialGradient={true}
       >
         <div className="pt-12 md:pt-20">
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <div>
-              <div className="inline-flex font-medium bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-purple-200 pb-3">Pricing plans</div>
-            </div>
-            <h2 className="h2 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">Meet our customers</h2>
-            <p className="text-lg text-slate-400">There are many variations available, but the majority have suffered, by injected humour, or randomised words which don't look even slightly believable.</p>
-          </div>
+          <SectionHeader
+            leading='Pricing plans'
+            title='Meet our customers'
+            description="There are many variations available, but the majority have suffered, by injected humour, or randomised words which don't look even slightly believable."
+          />
         </div>
       </ArtifactsList>
     </>
