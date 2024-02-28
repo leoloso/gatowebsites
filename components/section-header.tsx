3dfba79ce158dca80a74150d903a0fb759a0c500
leoloso <leo@getpop.org>
2024-02-28 16:47:34 +0800

    {leading}

--- components/section-header.tsx
+++ components/section-header.tsx
@@ -13,7 +13,7 @@ export default function SectionHeader({
     <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
       {leading && (
         <div>
-          <div className="inline-flex font-medium bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-purple-200 pb-3">Pricing plans</div>
+          <div className="inline-flex font-medium bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-purple-200 pb-3">{leading}</div>
         </div>
       )}
       <h2 className="h2 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">{title}</h2>
