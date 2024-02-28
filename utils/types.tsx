1f030200194b59c7ed74f391ec8683658fa9bf69
leoloso <leo@getpop.org>
2024-02-28 11:40:41 +0800

    added Artifact type

--- utils/types.tsx
+++ utils/types.tsx
@@ -1,8 +1,12 @@
-import { Doc, Post } from "@/.contentlayer/generated";
+import { Doc, Extension, Feature, Post } from "@/.contentlayer/generated";
 
 export type Article = Doc | Post;
+export type Artifact = Extension | Feature;
 
 export function isPost(article: Article): article is Post {
   return article.type === 'Post'
+}
+export function isExtension(artifact: Artifact): artifact is Extension {
+  return artifact.type === 'Extension'
 }
 
