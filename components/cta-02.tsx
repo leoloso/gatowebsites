da7fe50bcb403535d3d5a26f7faa0b265cf0529c
leoloso <leo@getpop.org>
2024-02-28 16:54:12 +0800

    migrated with children

--- components/cta-02.tsx
+++ components/cta-02.tsx
@@ -1,4 +1,5 @@
 import Particles from './particles'
+import SectionHeader from './section-header'
 
 export default function Cta02() {
   return (
@@ -24,18 +25,17 @@ export default function Cta02() {
             </svg>
           </div>
           {/* Content */}
-          <div className="max-w-3xl mx-auto text-center">
-            <div>
-              <div className="inline-flex font-medium bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-purple-200 pb-3">The security first platform</div>
-            </div>
-            <h2 className="h2 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">Supercharge your security</h2>
-            <p className="text-lg text-slate-400">All the lorem ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+          <SectionHeader
+            leading='The security first platform'
+            title='Supercharge your security'
+            description='All the lorem ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.'
+          >
             <div className="mt-8">
               <a className="btn text-slate-900 bg-gradient-to-r from-white/80 via-white to-white/80 hover:bg-white transition duration-150 ease-in-out group" href="#0">
                 Start Building <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
               </a>
             </div>
-          </div>
+          </SectionHeader>
         </div>
       </div>
     </section>
