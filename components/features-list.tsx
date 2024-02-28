0d22e61fd2425215a08a3f751ad04f52ed014330
leoloso <leo@getpop.org>
2024-02-28 15:55:49 +0800

    divided into 2

--- components/features-list.tsx
+++ components/features-list.tsx
@@ -1,26 +1,35 @@
-import { Feature } from '@/.contentlayer/generated'
+import { sortByOrderAndTitle } from '@/utils/sort';
 import ArtifactsList from './artifacts-list';
 import { allFeatures } from 'contentlayer/generated'
 
-export default function FeaturesList({
-  features = allFeatures,
-}: {
-  features?: Array<Feature>,
-}) {
+export default function FeaturesList() {
+  // Sort features
+  const features = allFeatures.sort(sortByOrderAndTitle)  
+
+  const freePluginFeatures = features.filter((feature) => feature.category === 'Free plugin')
+  const proPluginFeatures = features.filter((feature) => feature.category === 'PRO plugin')
+  
   return (
-    <ArtifactsList
-      artifacts={features}
-      showTopbar={false}
-      showSearch={false}
-      addRadialGradient={true}
-    >
-      <div className="py-12 md:py-20">
-        <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-          <h2 className="h2 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">Meet our customers</h2>
-          <p className="text-lg text-slate-400">There are many variations available, but the majority have suffered, by injected humour, or randomised words which don't look even slightly believable.</p>
+    <>
+      <ArtifactsList
+        artifacts={freePluginFeatures}
+        showTopbar={false}
+        showSearch={false}
+      />
+      <ArtifactsList
+        artifacts={proPluginFeatures}
+        showTopbar={false}
+        showSearch={false}
+        addRadialGradient={true}
+      >
+        <div className="py-12 md:py-20">
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">Meet our customers</h2>
+            <p className="text-lg text-slate-400">There are many variations available, but the majority have suffered, by injected humour, or randomised words which don't look even slightly believable.</p>
+          </div>
         </div>
-      </div>
-    </ArtifactsList>
+      </ArtifactsList>
+    </>
   )
 }
 
