c0f204e55203846a3c6d01f5c529c045a9ec27f7
leoloso <leo@getpop.org>
2024-02-28 17:04:20 +0800

    PageHeader

---
+++ components/page-header.tsx
@@ -0,0 +1,30 @@
+export default function PageHeader({
+  leading,
+  title,
+  description,
+  children,
+  paddingClassname = "pb-12 md:pb-20",
+}: {
+  leading?: string,
+  title: string,
+  description?: string,
+  children?: React.ReactNode,
+  paddingClassname?: string,
+}) {
+  return (
+    <div className={`text-center ${paddingClassname}`}>
+      {leading && (
+        <div>
+          <div className="inline-flex font-medium bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-purple-200 pb-3">{leading}</div>
+        </div>
+      )}
+      <h1 className="h1 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">{title}</h1>
+      {description && (
+        <div className="max-w-3xl mx-auto">
+          <p className="text-lg text-slate-400">{description}</p>
+        </div>
+      )}
+      {children}
+    </div>
+  )
+}
