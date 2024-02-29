eaa2f36b86eb492eb0942a07b61b0bc220685f2e
leoloso <leo@getpop.org>
2024-02-29 15:56:13 +0800

    invert PRO btn color

--- components/pricing-tier.tsx
+++ components/pricing-tier.tsx
@@ -24,8 +24,8 @@ export default function PricingTier({
         <div className="text-slate-500">{description}</div>
       </div>
       <div className="pb-4 border-b border-slate-800">
-        <a className={`btn-sm w-full transition duration-150 ease-in-out group ${highlight ? 'text-white bg-purple-500 hover:bg-purple-600' : (isPRO ? 'text-white bg-purple-700 hover:bg-purple-800' : 'text-slate-900 bg-gradient-to-r from-white/80 via-white to-white/80 hover:bg-white')}`} href={buttonURL}>
-          {buttonLabel} <span className={`tracking-normal group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1 ${highlight ? 'text-purple-300' : (isPRO ? 'text-purple-500' : 'text-purple-500')}`}>-&gt;</span>
+        <a className={`btn-sm w-full transition duration-150 ease-in-out group ${highlight ? 'text-white bg-purple-700 hover:bg-purple-800' : (isPRO ? 'text-white bg-purple-500 hover:bg-purple-600' : 'text-slate-900 bg-gradient-to-r from-white/80 via-white to-white/80 hover:bg-white')}`} href={buttonURL}>
+          {buttonLabel} <span className={`tracking-normal group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1 ${highlight ? 'text-purple-500' : (isPRO ? 'text-purple-300' : 'text-purple-500')}`}>-&gt;</span>
         </a>
       </div>
     </div>
