1c934eebd8f416cddf927ad96e3eb3ffa3eac0a3
leoloso <leo@getpop.org>
2024-02-29 15:18:37 +0800

    isProd => ...

--- app/app.config.js
+++ app/app.config.js
@@ -1,3 +1,4 @@
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   paths: {
     blog: "blog",
@@ -18,10 +19,18 @@ module.exports = {
     instawpSandboxDemo: "https://app.instawp.io/launch?t=gatographql-demo&d=v2",
     shopAffiliateProgram: "https://shop.gatographql.com/affiliates",
     shopClientOrders: "https://app.lemonsqueezy.com/my-orders",
-    shopPurchase: "https://shop.gatographql.com/checkout/buy/834ead62-1125-4e96-9bcb-5deb8ea768f5?embed=1&logo=0&desc=1&dark=1",
-    shopPurchaseTier1: "https://shop.gatographql.com/checkout/buy/834ead62-1125-4e96-9bcb-5deb8ea768f5?embed=1&logo=0&desc=1&dark=1",
-    shopPurchaseTier2: "https://shop.gatographql.com/checkout/buy/e8254ca0-ba6b-4875-a090-e0557b59c7d1?embed=1&logo=0&desc=1&dark=1",
-    shopPurchaseTier3: "https://shop.gatographql.com/checkout/buy/017ca707-0114-4a9a-bf41-7afd278fec03?embed=1&logo=0&desc=1&dark=1",
+    shopPurchase: isProd
+      ? "https://shop.gatographql.com/checkout/buy/834ead62-1125-4e96-9bcb-5deb8ea768f5?embed=1&logo=0&desc=1&dark=1"
+      : "https://shop.gatographql.com/checkout/buy/8fec9bf7-5809-455a-90e7-475312f32488",
+    shopPurchaseTier1: isProd
+      ? "https://shop.gatographql.com/checkout/buy/834ead62-1125-4e96-9bcb-5deb8ea768f5?embed=1&logo=0&desc=1&dark=1"
+      : "https://shop.gatographql.com/checkout/buy/8fec9bf7-5809-455a-90e7-475312f32488",
+    shopPurchaseTier2: isProd
+      ? "https://shop.gatographql.com/checkout/buy/e8254ca0-ba6b-4875-a090-e0557b59c7d1?embed=1&logo=0&desc=1&dark=1"
+      : "https://shop.gatographql.com/checkout/buy/7cd80f3d-da3f-4d83-a712-15cdacdcd4be",
+    shopPurchaseTier3: isProd
+      ? "https://shop.gatographql.com/checkout/buy/017ca707-0114-4a9a-bf41-7afd278fec03?embed=1&logo=0&desc=1&dark=1"
+      : "https://shop.gatographql.com/checkout/buy/0eb25649-d977-4784-bf96-68aeed24211f",
     wpDirectory: "https://wordpress.org/plugins/gatographql/",
     wpDownload: "https://downloads.wordpress.org/plugin/gatographql.latest-stable.zip",
   },
