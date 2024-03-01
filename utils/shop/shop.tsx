539aa9ab920f9522ff278656fbf80fc79b022ef9
leoloso <leo@getpop.org>
2024-03-01 17:03:39 +0800

    getShopAnchorClassname

--- utils/shop/shop.tsx
+++ utils/shop/shop.tsx
@@ -1,6 +1,14 @@
-import { getLemonSqueezyShopURL } from "./lemonsqueezy";
+import {
+  getLemonSqueezyShopURL,
+  getLemonSqueezyShopAnchorClassname,
+} from "./lemonsqueezy";
 
 export function getShopURL(url: string) {
   // Shop provider: Lemon Squeezy
   return getLemonSqueezyShopURL(url)
+}
+
+export function getShopAnchorClassname() {
+  // Shop provider: Lemon Squeezy
+  return getLemonSqueezyShopAnchorClassname()
 }
