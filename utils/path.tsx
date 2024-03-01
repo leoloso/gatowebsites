31b3a7f5eb65742a7ac1fdbf269f0a4f1b01a83c
leoloso <leo@getpop.org>
2024-03-01 10:21:47 +0800

    page slug can include a parent folder

--- utils/path.tsx
+++ utils/path.tsx
@@ -1,4 +1,5 @@
 // Get the page slug from the folder name
+// It can include a parent folder, such as contact/success.mdx
 export function getPageSlugFromPageScriptFile(pageFile: string) {
-  return pageFile.replace(/^.*\/([a-zA-Z0-9_-]+)\/page\.[a-z]+$/, '$1')
+  return pageFile.replace(/^.*\/app\/\(default\)\/([\/a-zA-Z0-9_-]+)\/page\.[a-z]+$/, '$1')
 }
