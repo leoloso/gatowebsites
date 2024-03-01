ad9b0e78995768754d603dd9749fb33bdf3957b7
leoloso <leo@getpop.org>
2024-03-01 14:38:53 +0800

    NewsletterForm()

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -1,3 +1,5 @@
+import NewsletterForm from "./forms/newsletter"
+
 export default function Newsletter({
   label = "Want to receive news from us?"
 }: {
@@ -45,15 +47,7 @@ export default function Newsletter({
                 <p className="text-purple-300 text-lg mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit nemo expedita voluptas culpa sapiente.</p>
 
                 {/* CTA form */}
-                <form className="w-full lg:w-auto">
-                  <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
-                    <input type="email" className="form-input w-full appearance-none bg-purple-800 border border-purple-700 focus:border-purple-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-purple-500" placeholder="Your email…" aria-label="Your email…" />
-                    <a className="btn text-white bg-blue-600 hover:bg-blue-700 shadow" href="#0">Subscribe</a>
-                  </div>
-                  {/* Success message */}
-                  {/* <p className="text-sm text-purple-400 mt-3">Thanks for subscribing!</p> */}
-                  <p className="text-sm text-purple-400 mt-3">No spam. You can unsubscribe at any time.</p>
-                </form>
+                <NewsletterForm />
               </div>
 
             </div>
