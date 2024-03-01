128078367237489b75108da33568c2cb2299c674
leoloso <leo@getpop.org>
2024-03-01 13:40:13 +0800

    HowdyHero

--- app/(default)/howdy/page.tsx
+++ app/(default)/howdy/page.tsx
@@ -3,14 +3,14 @@ export const metadata = {
   description: 'Page description',
 }
 
-import Hero from '@/components/howdy-hero'
+import HowdyHero from '@/components/howdy-hero'
 import HowdyMain from '@/components/howdy-main'
 import Cta from '@/components/cta-02'
 
 export default function Howdy() {
   return (
     <>
-      <Hero />
+      <HowdyHero />
       <HowdyMain />
       <Cta />
     </>
