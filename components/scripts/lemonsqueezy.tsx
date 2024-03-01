f5c619bf2e086d031d4fcaf54de53969c0532213
leoloso <leo@getpop.org>
2024-03-01 17:53:50 +0800

    use Script

--- components/scripts/lemonsqueezy.tsx
+++ components/scripts/lemonsqueezy.tsx
@@ -1,26 +1,36 @@
+import Script from 'next/script'
 import AppConfig from '@/app/app.config'
 
 export default function LemonSqueezyScriptProvider() {
   return (
     <>
       {/* @see https://docs.lemonsqueezy.com/help/lemonjs/what-is-lemonjs#loading-lemon-js-in-next-js */}
-      <script src="https://assets.lemonsqueezy.com/lemon.js" defer></script>
-      {/* <Script
+      {/* <script src="https://assets.lemonsqueezy.com/lemon.js" defer></script> */}
+      <Script
+        id="lemonsqueezy-lemon"
         src="https://assets.lemonsqueezy.com/lemon.js"
         strategy="afterInteractive"
-      ></Script> */}
+      />
 
       {/* LemonSqueezy affiliate tracking script */}
       {/* @see: https://docs.lemonsqueezy.com/guides/tutorials/affiliate-landing-pages */}
       {process.env.NODE_ENV === 'production' && (
         <>
-          <script dangerouslySetInnerHTML={{__html: `
-            window.lemonSqueezyAffiliateConfig = {
-              store: "${AppConfig.services.shop.affiliateTrackingShopSlug}",
-              trackOnLoad: false
-            };
-          `}} />
-          <script src="https://lmsqueezy.com/affiliate.js" defer></script>
+          <Script
+            id="lemonsqueezy-affiliates-config"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{__html: `
+              window.lemonSqueezyAffiliateConfig = {
+                store: "${AppConfig.services.shop.affiliateTrackingShopSlug}",
+                trackOnLoad: false
+              };
+            `}}
+          />
+          <Script
+            id="lemonsqueezy-affiliates"
+            src="https://lmsqueezy.com/affiliate.js"
+            strategy="afterInteractive"
+          />
         </>
       )}
 
