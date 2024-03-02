524ea45a5f6d96b3ae01974d495b56deab03d0cc
leoloso <leo@getpop.org>
2024-03-02 20:00:19 +0800

    commented out Topics

--- app/(default)/blog/page.tsx
+++ app/(default)/blog/page.tsx
@@ -45,7 +45,7 @@ export default function Blog() {
               {/* Sidebar */}
               <aside className="relative mt-12 md:mt-0 md:w-64 md:ml-12 lg:ml-20 md:shrink-0">
                 <PopularPosts />
-                <Topics />
+                {/* <Topics /> */}
               </aside>
 
             </div>
