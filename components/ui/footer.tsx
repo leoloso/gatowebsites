a34d1eebca84443283a5421b3e8d2623a5cf598d
leoloso <leo@getpop.org>
2024-03-03 15:04:45 +0800

    completely removed links

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -20,14 +20,7 @@ export default function Footer() {
                   <div className="text-sm text-slate-500">© Gato GraphQL <span className="text-slate-600">-</span> All rights reserved.</div>
                 </div>
                 {/* Social links */}
-                <ul className="flex">
-                  {/* <li>
-                    <a className="flex justify-center items-center text-purple-500 hover:text-purple-400 transition duration-150 ease-in-out" href="#0" aria-label="Twitter">
-                      <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                        <path d="m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z" />
-                      </svg>
-                    </a>
-                  </li> */}
+                {/* <ul className="flex">
                   <li className="ml-2">
                     <a
                       className="flex justify-center items-center text-purple-500 hover:text-purple-400 transition duration-150 ease-in-out"
@@ -40,7 +33,7 @@ export default function Footer() {
                       </svg>
                     </a>
                   </li>
-                </ul>
+                </ul> */}
               </div>
             </div>
           </FooterMenu>
