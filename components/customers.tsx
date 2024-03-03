9d2b9e227d47a59d41b54de042ec3faf04ecd8d2
leoloso <leo@getpop.org>
2024-03-03 14:45:02 +0800

    use getTestimonials()

--- components/customers.tsx
+++ components/customers.tsx
@@ -1,71 +1,11 @@
 import Image from 'next/image'
-import CustomerImg01 from '@/public/assets/theme/customer-01.jpg'
-import CustomerImg02 from '@/public/assets/theme/customer-02.jpg'
-import CustomerImg04 from '@/public/assets/theme/customer-04.jpg'
-import CustomerImg06 from '@/public/assets/theme/customer-06.jpg'
-import CustomerImg08 from '@/public/assets/theme/customer-08.jpg'
-import CustomerImg09 from '@/public/assets/theme/customer-09.jpg'
 import RadiantGradient from './radial-gradient'
 import SectionHeader from './section-header'
+import { getTestimonials } from './data/testimonials'
 
 export default function Customers() {
 
-  const items = [
-    {
-      img: CustomerImg01,
-      quote: "You have built an incredibly well-thought out and powerful tool—plus the support material is stellar.",
-      name: 'Quint R.',
-      // twitter: {
-      //   handle: '@MikeSmets',
-      //   link: '#0'
-      // }
-    },
-    {
-      img: CustomerImg08,
-      quote: "We loved this plugin and are annoyed that it’s better than a similar thing we are working on 😂 (we have a network of news sites and Gato GraphQL will save us a lot of time!)",
-      name: 'Joebe W.',
-      // twitter: {
-      //   handle: '@mike0point7',
-      //   link: '#0'
-      // }
-    },
-    {
-      img: CustomerImg04,
-      quote: "Amazing! I have translated most of my posts and pages, this was brilliant. Gato GraphQL saved me a fortune and hours of time and effort.",
-      name: 'Patrick T.',
-      // twitter: {
-      //   handle: '@liz-park82',
-      //   link: '#0'
-      // }
-    },
-    {
-      img: CustomerImg06,
-      quote: "This plugin is extraordinary, it unlocks a new world of possibilities within WordPress.",
-      name: 'Nathan W.',
-      // twitter: {
-      //   handle: '@jessiem9',
-      //   link: '#0'
-      // }
-    },
-    {
-      img: CustomerImg09,
-      quote: "A very good developer toolkit with many features and tons of guides on the website.",
-      name: 'Louis G.',
-      // twitter: {
-      //   handle: '@deva07p',
-      //   link: '#0'
-      // }
-    },
-    {
-      img: CustomerImg02,
-      quote: "This plugin brings all of the power of GraphQL to WordPress, but adding easier entry points for non-developers",
-      name: 'David W.',
-      // twitter: {
-      //   handle: '@mark-gerk',
-      //   link: '#0'
-      // }
-    }
-  ]
+  const items = getTestimonials()
 
   return (
     <section className="relative">
