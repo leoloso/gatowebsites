3be3da9f115053ce17b7753cd861d27ff5958f88
leoloso <leo@getpop.org>
2024-03-03 18:31:28 +0800

    about uses cta03

--- app/(default)/about/page.tsx
+++ app/(default)/about/page.tsx
@@ -8,7 +8,7 @@ import Story from '@/components/story'
 // import Team from '@/components/team'
 // import Recruitment from '@/components/recruitment'
 // import Testimonials from '@/components/testimonials-02'
-import Cta from '@/components/cta-02'
+import Cta from '@/components/cta-03'
 
 export default function About() {
   return (
