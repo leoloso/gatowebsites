0d40baeb5df67050fc4073ef824dc5d45e5a2df4
leoloso <leo@getpop.org>
2024-03-03 13:59:41 +0800

    Link => span

--- components/post-tags.tsx
+++ components/post-tags.tsx
@@ -51,9 +51,9 @@ export default function PostTags({ tags }: {
     <ul className="flex flex-wrap text-xs font-medium -m-1">
       {tags.map((tag, tagIndex) => (
         <li key={tagIndex} className="m-1">
-          <Link href="#" className={`inline-flex text-center py-1 px-3 rounded-full transition duration-150 ease-in-out ${tagColor(tag)}`}>
+          <span className={`inline-flex text-center py-1 px-3 rounded-full transition duration-150 ease-in-out ${tagColor(tag)}`}>
             {tag}
-          </Link>
+          </span>
         </li>
       ))}
     </ul>
