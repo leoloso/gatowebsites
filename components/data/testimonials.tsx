744b99a0206d0d8e7a4a7874b9b7d2c1dc8410a3
leoloso <leo@getpop.org>
2024-03-03 14:44:06 +0800

    added data items

--- components/data/testimonials.tsx
+++ components/data/testimonials.tsx
@@ -1,8 +1,11 @@
 import { StaticImageData } from 'next/image'
 
 import CustomerImg01 from '@/public/assets/theme/customer-01.jpg'
-import CustomerImg08 from '@/public/assets/theme/customer-08.jpg'
+import CustomerImg02 from '@/public/assets/theme/customer-02.jpg'
 import CustomerImg04 from '@/public/assets/theme/customer-04.jpg'
+import CustomerImg06 from '@/public/assets/theme/customer-06.jpg'
+import CustomerImg08 from '@/public/assets/theme/customer-08.jpg'
+import CustomerImg09 from '@/public/assets/theme/customer-09.jpg'
 
 interface TestimonialItem {
   img: StaticImageData
@@ -31,6 +34,36 @@ export function getTestimonials(): TestimonialItem[] {
       quote: "Amazing! I have translated most of my posts and pages, this was brilliant. Gato GraphQL saved me a fortune and hours of time and effort.",
       name: 'Patrick T.',
       role: 'Agency dev'
+    },
+    {
+      img: CustomerImg06,
+      quote: "This plugin is extraordinary, it unlocks a new world of possibilities within WordPress.",
+      name: 'Nathan W.',
+      role: 'Podcaster',
+      // twitter: {
+      //   handle: '@jessiem9',
+      //   link: '#0'
+      // }
+    },
+    {
+      img: CustomerImg09,
+      quote: "A very good developer toolkit with many features and tons of guides on the website.",
+      name: 'Louis G.',
+      role: 'Plugin business owner',
+      // twitter: {
+      //   handle: '@deva07p',
+      //   link: '#0'
+      // }
+    },
+    {
+      img: CustomerImg02,
+      quote: "This plugin brings all of the power of GraphQL to WordPress, but adding easier entry points for non-developers",
+      name: 'David W.',
+      role: 'Agency owner',
+      // twitter: {
+      //   handle: '@mark-gerk',
+      //   link: '#0'
+      // }
     }
   ]
 }
