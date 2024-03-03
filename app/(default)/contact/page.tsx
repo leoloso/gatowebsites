6643c1c5ebdf33bffe2ab5d2f4018aae579a3fb0
leoloso <leo@getpop.org>
2024-03-03 18:32:59 +0800

    added cta03 to /contact

--- app/(default)/contact/page.tsx
+++ app/(default)/contact/page.tsx
@@ -1,6 +1,7 @@
 import ContactForm from "@/components/forms/contact"
 import PageHeader from "@/components/page-header"
 import StunningBackground from "@/components/stunning-background"
+import Cta from '@/components/cta-03'
 
 export const metadata = {
   title: 'Contact us - Open PRO',
@@ -9,24 +10,27 @@ export const metadata = {
 
 export default function Contact() {
   return (
-    <section className="relative">
+    <>
+      <section className="relative">
 
-      <StunningBackground />
+        <StunningBackground />
 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
+          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
 
-          {/* Page header */}
-          <PageHeader
-            leading='Contact us'
-            title="How can we help you?"
-            description="Tell us your needs, and we'll contact you shortly."
-          />
+            {/* Page header */}
+            <PageHeader
+              leading='Contact us'
+              title="How can we help you?"
+              description="Tell us your needs, and we'll contact you shortly."
+            />
 
-          <ContactForm />
+            <ContactForm />
 
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <Cta />
+    </>
   )
 }
