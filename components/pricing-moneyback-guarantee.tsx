b0b750d6aebd80bb251cd7e8842af1f3957d66d8
leoloso <leo@getpop.org>
2024-03-03 18:53:20 +0800

    animation

--- components/pricing-moneyback-guarantee.tsx
+++ components/pricing-moneyback-guarantee.tsx
@@ -9,9 +9,9 @@ export default function PricingMoneyBackGuarantee() {
     <section className="relative">
 
       {/* Particles animation */}
-      {/* <div className="absolute left-1/2 -translate-x-1/2 top-0 -z-10 w-64 h-64 -mt-24">
+      <div className="absolute left-1/2 -translate-x-1/2 top-0 -z-10 w-64 h-64 -mt-24">
         <Particles className="absolute inset-0 -z-10" quantity={6} staticity={30} />
-      </div> */}
+      </div>
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pb-12 md:pb-20">
@@ -30,29 +30,9 @@ export default function PricingMoneyBackGuarantee() {
               <div className="inline-flex">
                 <Image src={Illustration} width="334" height="334" alt="Features illustration" />
               </div>
-              {/* <Image className="absolute -mt-[40%]" src={IllustrationTop} width="396" height="328" alt="Features illustration top" aria-hidden="true" /> */}
-              {/* <div className="absolute w-[200%] flex items-center justify-center space-x-5"> */}
+              <Image className="absolute -mt-[40%]" src={IllustrationTop} width="396" height="328" alt="Features illustration top" aria-hidden="true" />
               <div className="absolute w-[200%] flex items-center justify-center space-x-5">
                 <Image src={MoneyBackGuaranteePic} width={175} height={129} alt="Money back guarantee" />
-                {/* <div className="-rotate-[4deg]">
-                  <Image className="w-11 h-11 drop-shadow-lg animate-[float_2.6s_ease-in-out_infinite] opacity-10" src={Icon01} width={80} height={80} alt="Pricing icon 01" />
-                </div>
-                <div className="rotate-[4deg]">
-                  <Image className="w-14 h-14 drop-shadow-lg animate-[float_2.4s_ease-in-out_infinite] opacity-25" src={Icon02} width={80} height={80} alt="Pricing icon 02" />
-                </div>
-                <div className="-rotate-[4deg]">
-                  <Image className="w-16 h-16 drop-shadow-lg animate-[float_2.2s_ease-in-out_infinite] opacity-60" src={Icon03} width={80} height={80} alt="Pricing icon 03" />
-                </div>
-                <Image className="drop-shadow-lg animate-float" src={Icon04} width={80} height={80} alt="Pricing icon 04" />
-                <div className="rotate-[4deg]">
-                  <Image className="w-16 h-16 drop-shadow-lg animate-[float_2.2s_ease-in-out_infinite] opacity-60" src={Icon05} width={80} height={80} alt="Pricing icon 05" />
-                </div>
-                <div className="-rotate-[4deg]">
-                  <Image className="w-14 h-14 drop-shadow-lg animate-[float_2.4s_ease-in-out_infinite] opacity-25" src={Icon06} width={80} height={80} alt="Pricing icon 06" />
-                </div>
-                <div className="rotate-[4deg]">
-                  <Image className="w-11 h-11 drop-shadow-lg animate-[float_2.6ås_ease-in-out_infinite] opacity-10" src={Icon07} width={80} height={80} alt="Pricing icon 07" />
-                </div> */}
               </div>
             </div>
           </div>
