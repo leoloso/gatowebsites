2469d97337fb9a8ff16fd5042542489b75406539
leoloso <leo@getpop.org>
2024-03-03 14:11:12 +0800

    removed " prose-code:text-blue-400 "

--- components/mdx/update-mdx.tsx
+++ components/mdx/update-mdx.tsx
@@ -10,7 +10,7 @@ export function UpdateMdx({ code }: MdxProps) {
   const Component = useMDXComponent(code)
 
   return (
-    <div className="prose dark:prose-invert text-slate-400 prose-p:leading-relaxed prose-a:font-medium prose-a:text-purple-600 dark:prose-a:text-purple-300 prose-a:no-underline hover:prose-a:no-underline prose-strong:text-slate-50 prose-strong:font-medium prose-code:px-1 prose-code:py-0.5 prose-code:rounded prose-code:before:content-[''] prose-code:after:content-[''] prose-code:text-blue-400 prose-pre:bg-slate-800 prose-pre:border prose-pre:border-slate-700 prose-headings:scroll-mt-20">
+    <div className="prose dark:prose-invert text-slate-400 prose-p:leading-relaxed prose-a:font-medium prose-a:text-purple-600 dark:prose-a:text-purple-300 prose-a:no-underline hover:prose-a:no-underline prose-strong:text-slate-50 prose-strong:font-medium prose-code:px-1 prose-code:py-0.5 prose-code:rounded prose-code:before:content-[''] prose-code:after:content-[''] prose-pre:bg-slate-800 prose-pre:border prose-pre:border-slate-700 prose-headings:scroll-mt-20">
       <Component components={{ ...mdxComponents }} />
     </div>
   )
