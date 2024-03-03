bb39f3b2bf9ba40c382981406a4c45e089e146ca
leoloso <leo@getpop.org>
2024-03-03 14:37:45 +0800

    use ArtifactSection

--- app/(default)/extensions/[slug]/page.tsx
+++ app/(default)/extensions/[slug]/page.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from 'next'
 import { allExtensions } from 'contentlayer/generated'
 import { notFound } from 'next/navigation'
-import Link from 'next/link'
-import Image from 'next/image'
-import DefaultExtensionImg from '@/public/assets/theme/default/extension-image.png'
-import DefaultExtensionIcon from '@/public/assets/theme/default/extension-icon.svg'
-import Star from '@/public/assets/theme/star.svg'
-import Avatar from '@/public/assets/theme/default/extension-author-avatar.jpg'
-import { ArtifactMdx } from '@/components/mdx/artifact-mdx'
 import AppConfig from '@/app/app.config'
-import { getExtensionDocumentationURL } from '@/utils/application-urls'
-import StunningBackground from '@/components/stunning-background'
+import ArtifactSection from '@/components/sections/artifact'
 
 export async function generateStaticParams() {
   return allExtensions.map((extension) => ({
@@ -43,121 +35,31 @@ export default async function SingleExtension({ params }: {
   if (!extension) notFound()
 
   return (
-    <section className="relative">
-
-      <StunningBackground />
-
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 md:pt-40">
-
-          <div className="md:flex md:justify-between">
-
-            {/* Page content */}
-            <div className="md:grow pb-12 md:pb-20">
-              <div className="max-w-[720px]">
-
-                <div className="flex flex-col lg:flex-row space-y-6 lg:space-y-0 lg:space-x-16">
-
-                  {/* Back button */}
-                  <div className="shrink-0">
-                    <div className="sticky top-20">
-                      <Link className="flex items-center justify-center w-9 h-9 group border border-transparent rounded-full [background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none" href={`/${AppConfig.paths.extensions}`}>
-                        <span className="sr-only">Go back</span>
-                        <svg className="w-4 h-4 fill-purple-500" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M6.7 14.7l1.4-1.4L3.8 9H16V7H3.8l4.3-4.3-1.4-1.4L0 8z" />
-                        </svg>
-                      </Link>
-                    </div>
-                  </div>
-
-                  {/* Content */}
-                  <div>
-                    <article className="pb-12 mb-12 border-b [border-image:linear-gradient(to_right,transparent,theme(colors.slate.800),transparent)1]">
-
-                      <figure className="bg-slate-700/20 border border-slate-300/10 p-4 rounded-3xl mb-8">
-                        <Image className="w-full rounded-2xl" src={extension.image || DefaultExtensionImg} width={586} height={316} alt="Extension image" />
-                      </figure>
-
-                      <h1 className="sr-only">{extension.title}</h1>
-
-                      {/* Extension content */}
-                      <ArtifactMdx code={extension.body.code} />
-                    </article>
-
-                    <aside className="pl-6 border-l-2 border-purple-500">
-                      <p className="inline-flex font-medium italic text-lg bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">“ This integration is so perfect it tickles my brain. It ensures that your work is in sync across your entire team. ”</p>
-                      <footer className="flex items-center space-x-4">
-                        <Image className="shrink-0 rounded-full" src={Avatar} width={32} height={32} alt="Author" />
-                        <div className="text-sm font-medium text-slate-300">
-                          Mike Hunt <span className="text-slate-700">-</span> <a className="text-purple-500 hover:underline" href="#0">Thunderbolt</a>
-                        </div>
-                      </footer>
-                    </aside>
-                  </div>
-
-                </div>
-
-              </div>
-            </div>
-
-            {/* Sidebar */}
-            <aside className="md:w-64 lg:w-80 md:shrink-0 md:pt-[3.75rem] lg:pt-0 pb-12 md:pb-20">
-              <div className="sticky top-20 md:pl-6 lg:pl-10">
-
-                {/* Sidebar content */}
-                <div className="space-y-6">
-
-                  {/* Widget */}
-                  <div className="bg-gradient-to-tr from-slate-800 to-slate-800/25 rounded-3xl border border-slate-800">
-                    <div className="px-5 py-6">
-                      <div className="text-center mb-5">
-                        <div className="mb-4">
-                          <div className="relative inline-flex">
-                            <Image src={extension.icon || DefaultExtensionIcon} width={80} height={80} alt="Extension icon" />
-                            {!! extension.featured && (
-                              <Image className="absolute top-0 -right-1" src={Star} width={24} height={24} alt="Star" aria-hidden="true" />
-                            )}
-                          </div>
-                        </div>
-                        <a className="btn-sm text-slate-300 hover:text-white transition duration-150 ease-in-out group [background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/70 before:rounded-full before:pointer-events-none" href={getExtensionDocumentationURL(extension)}>
-                          <span className="relative inline-flex items-center">
-                            Open documentation <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
-                          </span>
-                        </a>
-                      </div>
-                      <ul className="text-sm">
-                        <li className="flex items-center justify-between space-x-4 py-3 border-t [border-image:linear-gradient(to_right,theme(colors.slate.700/.3),theme(colors.slate.700),theme(colors.slate.700/.3))1]">
-                          <span className="text-slate-400">Extension</span>
-                          <span className="text-slate-300 font-medium">{extension.title}</span>
-                        </li>
-                        {!! extension.integration && (
-                          <li className="flex items-center justify-between space-x-4 py-3 border-t [border-image:linear-gradient(to_right,theme(colors.slate.700/.3),theme(colors.slate.700),theme(colors.slate.700/.3))1]">
-                            <span className="text-slate-400">Integration for</span>
-                            <a className="text-purple-500 font-medium flex items-center space-x-1" href={extension.integration.url} target="_blank">
-                              <span>{extension.integration.name}</span>
-                              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="9" height="9">
-                                <path d="m1.285 8.514-.909-.915 5.513-5.523H1.663l.01-1.258h6.389v6.394H6.794l.01-4.226z" />
-                              </svg>
-                            </a>
-                          </li>
-                        )}
-                        <li className="flex items-center justify-between space-x-4 py-3 border-t [border-image:linear-gradient(to_right,theme(colors.slate.700/.3),theme(colors.slate.700),theme(colors.slate.700/.3))1]">
-                          <span className="text-slate-400">Category</span>
-                          <span className="text-slate-300 font-medium">{extension.category}</span>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-
-                </div>
-
-              </div>
-            </aside>
-
-          </div>
-
-        </div>
-      </div>
-    </section>
+    <ArtifactSection
+      artifact={extension}
+      sectionURL={`/${AppConfig.paths.extensions}`}
+    >
+      <ul className="text-sm">
+        <li className="flex items-center justify-between space-x-4 py-3 border-t [border-image:linear-gradient(to_right,theme(colors.slate.700/.3),theme(colors.slate.700),theme(colors.slate.700/.3))1]">
+          <span className="text-slate-400">Extension</span>
+          <span className="text-slate-300 font-medium">{extension.title}</span>
+        </li>
+        {!! extension.integration && (
+          <li className="flex items-center justify-between space-x-4 py-3 border-t [border-image:linear-gradient(to_right,theme(colors.slate.700/.3),theme(colors.slate.700),theme(colors.slate.700/.3))1]">
+            <span className="text-slate-400">Integration for</span>
+            <a className="text-purple-500 font-medium flex items-center space-x-1" href={extension.integration.url} target="_blank">
+              <span>{extension.integration.name}</span>
+              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="9" height="9">
+                <path d="m1.285 8.514-.909-.915 5.513-5.523H1.663l.01-1.258h6.389v6.394H6.794l.01-4.226z" />
+              </svg>
+            </a>
+          </li>
+        )}
+        <li className="flex items-center justify-between space-x-4 py-3 border-t [border-image:linear-gradient(to_right,theme(colors.slate.700/.3),theme(colors.slate.700),theme(colors.slate.700/.3))1]">
+          <span className="text-slate-400">Category</span>
+          <span className="text-slate-300 font-medium">{extension.category}</span>
+        </li>
+      </ul>
+    </ArtifactSection>
   )
 }
