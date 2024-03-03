a4da8c0451557a20653d575ec2e0f690bb6d4889
leoloso <leo@getpop.org>
2024-03-03 12:50:08 +0800

    getPostTags

--- utils/post.tsx
+++ utils/post.tsx
@@ -3,3 +3,11 @@ import { Post, allPosts } from 'contentlayer/generated'
 export function getFeaturedPosts(posts: Array<Post> | undefined = allPosts) {
   return posts.filter((post) => post.featured)
 }
+
+export function getPostTags(posts: Array<Post> | undefined = allPosts) {
+  return posts
+    .map((post) => post.tags)
+    .flat()
+    // filter distinct values
+    .filter((value, index, array) => array.indexOf(value) === index)
+}
