c9a72e5ac85e6162bdfc991ddbac2e44a433a5ac
leoloso <leo@getpop.org>
2024-03-03 19:06:57 +0800

    changed {/* Blurred shape */}

--- components/pricing-moneyback-guarantee.tsx
+++ components/pricing-moneyback-guarantee.tsx
@@ -8,18 +8,26 @@ export default function PricingMoneyBackGuarantee() {
   return (
     <section className="relative">
 
+    {/* Blurred shape */}
+    <div className="absolute top-0 -translate-y-1/3 left-1/2 -translate-x-1/2 ml-24 blur-2xl opacity-50 pointer-events-none -z-10" aria-hidden="true">
+      <svg xmlns="http://www.w3.org/2000/svg" width="434" height="427">
+        <defs>
+          <linearGradient id="bs3-a" x1="19.609%" x2="50%" y1="14.544%" y2="100%">
+            <stop offset="0%" stopColor="#6366F1" />
+            <stop offset="100%" stopColor="#6366F1" stopOpacity="0" />
+          </linearGradient>
+        </defs>
+        <path fill="url(#bs3-a)" fillRule="evenodd" d="m410 0 461 369-284 58z" transform="matrix(1 0 0 -1 -410 427)" />
+      </svg>
+    </div>
+
       {/* Particles animation */}
       <div className="absolute left-1/2 -translate-x-1/2 top-0 -z-10 w-64 h-64 -mt-24">
         <Particles className="absolute inset-0 -z-10" quantity={6} staticity={30} />
       </div>
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="relative px-8 py-12 md:py-20 rounded-[3rem] overflow-hidden">
-          {/* Radial gradient */}
-          <div className="absolute flex items-center justify-center top-0 -translate-y-1/2 left-1/2 -translate-x-1/2 pointer-events-none -z-10 w-1/3 aspect-square" aria-hidden="true">
-            <div className="absolute inset-0 translate-z-0 bg-purple-500 rounded-full blur-[120px] opacity-70" />
-            <div className="absolute w-1/4 h-1/4 translate-z-0 bg-purple-400 rounded-full blur-[40px]" />
-          </div>
+        <div className="py-12 md:py-20">
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12">
