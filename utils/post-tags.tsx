f361bda98e7953edfa3880550c6abbb340396704
leoloso <leo@getpop.org>
2024-03-03 13:05:36 +0800

    post-tags

---
+++ utils/post-tags.tsx
@@ -0,0 +1,27 @@
+import { Post, allPosts } from 'contentlayer/generated'
+import { COLORS } from './colors'
+import { sortAlphabetically } from './sort'
+
+export function getPostTags(posts: Array<Post> | undefined = allPosts): Array<string> {
+  return posts
+    .map((post) => post.tags || [])
+    .flat(1)
+    // filter distinct values
+    .filter((value, index, array) => array.indexOf(value) === index)
+}
+
+export function getPostTagColors(posts: Array<Post> | undefined = allPosts) {
+  const postTags = getPostTags(posts).sort(sortAlphabetically)
+  let postTagColors : { [key: string]: string } = {}
+  postTags.forEach(function (postTag: string | undefined, index: number) {
+    if (!postTag) {
+      return;
+    }
+    postTagColors[postTag] = COLORS[index]
+  })
+  return postTagColors
+}
+
+export function getPostTagColor(tag: string) {
+  return getPostTagColors()[tag]
+}
