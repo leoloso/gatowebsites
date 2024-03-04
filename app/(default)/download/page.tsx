04f5a8cc4711679d48f52f409c5d4105eb29d39b
leoloso <leo@getpop.org>
2024-03-04 22:40:48 +0800

    changed cta

--- app/(default)/download/page.tsx
+++ app/(default)/download/page.tsx
@@ -3,7 +3,7 @@ import { notFound } from 'next/navigation'
 import PageSection from '@/components/sections/page'
 import { getPage, getPageMetadata } from '@/utils/page'
 import { getPageSlugFromPageScriptFile } from '@/utils/path'
-import Cta from '@/components/cta-02'
+import Cta from '@/components/cta-03'
 
 // Get the page slug from the folder name
 const pageSlug = getPageSlugFromPageScriptFile(__filename)
@@ -24,8 +24,11 @@ export default async function SinglePage() {
   if (!page) notFound()
 
   return (
-    <PageSection
-      page={page}
-    />
+    <>
+      <PageSection
+        page={page}
+      />
+      <Cta />
+    </>
   )
 }
