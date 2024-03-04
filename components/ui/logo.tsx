934a9a57fad523672a98b61d28bf6ca6ddd4baa6
leoloso <leo@getpop.org>
2024-03-04 23:11:18 +0800

    rectangular logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import LogoImg from '@/public/assets/GatoGraphQL-logo-face.png'
+import LogoImg from '@/public/assets/GatoGraphQL-logo-suki-text-rectangular.png'
 
 export default function Logo() {
   return (
     <Link className="inline-flex" href="/" aria-label="Gato GraphQL logo">
-      <Image className="max-w-none" src={LogoImg} width={38} height={38} priority alt="Gato GraphQL logo" />
+      <Image className="max-w-none" src={LogoImg} width={210} height={40} priority alt="Gato GraphQL logo" />
     </Link>
   )
 }
