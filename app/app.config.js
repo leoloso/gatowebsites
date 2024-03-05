8d78fc5b23faef88770914c25ee0767b4e139b37
leoloso <leo@getpop.org>
2024-03-06 07:36:11 +0800

    Point to PRO bundle shop URL

--- app/app.config.js
+++ app/app.config.js
@@ -21,16 +21,16 @@ module.exports = {
     shopClientOrders: "https://app.lemonsqueezy.com/my-orders",
     shopPurchase: isProd
       ? "https://shop.gatographql.com/checkout/buy/834ead62-1125-4e96-9bcb-5deb8ea768f5"
-      : "https://shop.gatographql.com/checkout/buy/8fec9bf7-5809-455a-90e7-475312f32488",
+      : "https://shop.gatographql.com/checkout/buy/3a50305d-a275-40e9-a8ac-56c01321b83c",
     shopPurchaseTier1: isProd
       ? "https://shop.gatographql.com/checkout/buy/834ead62-1125-4e96-9bcb-5deb8ea768f5"
-      : "https://shop.gatographql.com/checkout/buy/8fec9bf7-5809-455a-90e7-475312f32488",
+      : "https://shop.gatographql.com/checkout/buy/3a50305d-a275-40e9-a8ac-56c01321b83c",
     shopPurchaseTier2: isProd
       ? "https://shop.gatographql.com/checkout/buy/e8254ca0-ba6b-4875-a090-e0557b59c7d1"
-      : "https://shop.gatographql.com/checkout/buy/7cd80f3d-da3f-4d83-a712-15cdacdcd4be",
+      : "https://shop.gatographql.com/checkout/buy/e86196ba-47de-4583-8c23-10c1e4d8859a",
     shopPurchaseTier3: isProd
       ? "https://shop.gatographql.com/checkout/buy/017ca707-0114-4a9a-bf41-7afd278fec03"
-      : "https://shop.gatographql.com/checkout/buy/0eb25649-d977-4784-bf96-68aeed24211f",
+      : "https://shop.gatographql.com/checkout/buy/6cfe250c-6b6b-41f2-bc23-f63521961089",
     wpDirectory: "https://wordpress.org/plugins/gatographql/",
     wpDownload: "https://downloads.wordpress.org/plugin/gatographql.latest-stable.zip",
   },
