9c1e27b12f6d69695d8beda2e3ee25e37b03af5a
leoloso <leo@getpop.org>
2024-03-06 09:46:29 +0800

    window as any

--- components/shop/initialize-lemonsqueezy-shop.tsx
+++ components/shop/initialize-lemonsqueezy-shop.tsx
@@ -3,11 +3,11 @@
 import { useEffect } from 'react'
 
 export default function InitializeLemonSqueezyShop() {
+  // @see https://stackoverflow.com/questions/56457935/typescript-error-property-x-does-not-exist-on-type-window
   // @see https://docs.lemonsqueezy.com/guides/tutorials/nextjs-saas-billing#creating-checkouts-and-letting-customers-subscribe
-  // Make sure Lemon.js is loaded, you need to enqueue the Lemon Squeezy SDK in your app first.
   useEffect(() => {
-    if (typeof window.createLemonSqueezy === 'function') {
-      window.createLemonSqueezy()
+    if (typeof (window as any).createLemonSqueezy === 'function') {
+      (window as any).createLemonSqueezy()
     }
   }, [])
 
