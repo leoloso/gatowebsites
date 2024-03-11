c17e0c369d3de136d8517a228c1d324ed86110a2
leoloso <leo@getpop.org>
2024-03-11 10:34:18 +0800

    Use DOMAIN in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,21 +1,24 @@
+import { DOMAIN } from '@/utils/domain'
 import { MetadataRoute } from 'next'
+
+
  
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://acme.com',
+      url: DOMAIN,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://acme.com/about',
+      url: `${DOMAIN}/about`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://acme.com/blog',
+      url: `${DOMAIN}/blog`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
