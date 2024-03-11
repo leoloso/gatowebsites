41a0b57f10e48cc559f5b38f5ccd7597a723968f
leoloso <leo@getpop.org>
2024-03-11 11:02:04 +0800

    priorities

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -15,133 +15,133 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${DOMAIN}/pricing`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 1,
+      priority: 0.6,
     },
     // Features
     {
       url: `${DOMAIN}/${AppConfig.paths.features}`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 1,
+      priority: 0.9,
     },
     // Extensions
     {
       url: `${DOMAIN}/${AppConfig.paths.extensions}`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 1,
+      priority: 0.8,
     },
     // Videos
     // {
     //   url: `${DOMAIN}/${AppConfig.paths.videoPosts}`,
     //   lastModified: new Date(),
     //   changeFrequency: 'weekly',
-    //   priority: 1,
+    //   priority: 0.7,
     // },
     // Guides
     {
       url: `${DOMAIN}/${AppConfig.paths.docs.guides}`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 1,
+      priority: 0.7,
     },
     // Extensions reference
     {
       url: `${DOMAIN}/${AppConfig.paths.docs.extensionsReference}`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 1,
+      priority: 0.7,
     },
     // Queries library
     {
       url: `${DOMAIN}/${AppConfig.paths.docs.queryLibrary}`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 1,
+      priority: 0.7,
     },
     // Schema tutorial
     {
       url: `${DOMAIN}/${AppConfig.paths.docs.tutorial}`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 1,
+      priority: 0.7,
     },
     // Changelog
     // {
     //   url: `${DOMAIN}/changelog`,
     //   lastModified: new Date(),
     //   changeFrequency: 'weekly',
-    //   priority: 1,
+    //   priority: 0.5,
     // },
     // Refund policy
     {
       url: `${DOMAIN}/refund-policy`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 1,
+      priority: 0.1,
     },
     // Blog
     {
       url: `${DOMAIN}/${AppConfig.paths.blog}`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 1,
+      priority: 0.5,
     },
     // Newsletter
     {
       url: `${DOMAIN}/newsletter`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
-      priority: 1,
+      priority: 0.3,
     },
     // Customers
     // {
     //   url: `${DOMAIN}/customers`,
     //   lastModified: new Date(),
     //   changeFrequency: 'weekly',
-    //   priority: 1,
+    //   priority: 0.5,
     // },
     // Developer Partnership Program
     {
       url: `${DOMAIN}/developers`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 1,
+      priority: 0.3,
     },
     // About us
     {
       url: `${DOMAIN}/about`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 1,
+      priority: 0.5,
     },
     // Contact us
     {
       url: `${DOMAIN}/contact`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
-      priority: 1,
+      priority: 0.5,
     },
     // WPBuilds series
     {
       url: `${DOMAIN}/specials/wpbuilds`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
-      priority: 1,
+      priority: 0.5,
     },
     // My orders
     {
       url: `${DOMAIN}/shop/my-orders`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
-      priority: 1,
+      priority: 0.1,
     },
     // Support request
     {
       url: `${DOMAIN}/support`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
-      priority: 1,
+      priority: 0.5,
     },
   ]
 }
