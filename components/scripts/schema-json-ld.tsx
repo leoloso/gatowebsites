f127509018dcf8d5e1bf07e28134350655452972
leoloso <leo@getpop.org>
2024-03-13 17:24:50 +0800

    SchemaJsonLdScript

---
+++ components/scripts/schema-json-ld.tsx
@@ -0,0 +1,57 @@
+import { Article, Graph, WithContext } from 'schema-dts';
+import { maybeAddDomain } from '@/utils/domain'
+import { DOMAIN } from '@/data/env/domain'
+import FounderPic from '@/public/assets/team/Leo-square.jpg'
+
+interface JsonLdProps {
+  headline: string,
+  url: string,
+  image?: string,
+  description: string,
+  datePublished: string,
+}
+
+// @see https://www.kozhuhds.com/blog/how-to-build-a-static-mdx-blog-with-nextjs-and-contentlayer#structured-data
+export default async function SchemaJsonLdScript({
+  headline,
+  url,
+  image,
+  description,
+  datePublished,
+}: JsonLdProps) {
+  const structuredData: WithContext<Article> = {
+    '@context': 'https://schema.org',
+    '@type': 'Article',
+    headline: headline,
+    url: url,
+    ...(image ? {
+      image: {
+        '@type': 'ImageObject',
+        url: maybeAddDomain(image),
+      }
+    } : {}),
+    description: description,
+    datePublished: datePublished,
+    author: {
+      '@type': 'Person',
+      name: 'Leonardo Losoviz',
+      url: DOMAIN,
+      image: maybeAddDomain(FounderPic.src),
+    },
+  };
+  const jsonLd: Graph = {
+    '@context': 'https://schema.org',
+    '@graph': [structuredData],
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={
+        {
+          __html: JSON.stringify(jsonLd)
+        }
+      }
+    />
+  )
+}
