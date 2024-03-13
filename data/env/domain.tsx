9b329a436a748f5f81b1f936373f3ed69b544ce9
leoloso <leo@getpop.org>
2024-03-13 14:39:42 +0800

    default VERCEL_URL

--- data/env/domain.tsx
+++ data/env/domain.tsx
@@ -1,11 +1,11 @@
 // Used for metadataBase and wherever the domain is needed.
-// First check if env var from Netlify is defined. If not, from Vercel.
+// First check if env var from Vercel is defined. If not, from Netlify.
 // If not, replicate the default response from Next.js
 export const DOMAIN = 
-  // @see https://docs.netlify.com/configure-builds/environment-variables/#deploy-urls-and-metadata
-  process.env.URL ? process.env.URL :
+  // @see https://nextjs.org/docs/app/api-reference/functions/generate-metadata#metadatabase
+  process.env.VERCEL_URL ? process.env.VERCEL_URL :
   (
-    // @see https://nextjs.org/docs/app/api-reference/functions/generate-metadata#metadatabase
-    process.env.VERCEL_URL ? process.env.VERCEL_URL :
+    // @see https://docs.netlify.com/configure-builds/environment-variables/#deploy-urls-and-metadata
+    process.env.URL ? process.env.URL :
     `http://localhost:${process.env.PORT || 3000}`
   )
