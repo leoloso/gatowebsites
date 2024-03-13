7e12053995059293e5d118c7e30774da6389d809
leoloso <leo@getpop.org>
2024-03-13 15:47:51 +0800

    format

--- utils/rss.tsx
+++ utils/rss.tsx
@@ -9,34 +9,34 @@ import Logo from '@/public/assets/GatoGraphQL-logo-face.png'
 import Favicon from '@/public/favicon.ico'
 
 export default async function generateRssFeed() {
- const posts = allPosts.sort(sortByPublishedAt) 
+  const posts = allPosts.sort(sortByPublishedAt) 
 
- const feedOptions = {
-  title: 'Gato GraphQL Blog | RSS Feed',
-  description: 'Stay up to date on the latest from Gato GraphQL and our engineering practices.',
-  id: DOMAIN,
-  link: DOMAIN,
-  image: Logo.src,
-  favicon: Favicon.src,
-  copyright: `All rights reserved ${new Date().getFullYear()}, Gato GraphQL`,
-  generator: 'Feed for Node.js',
-  feedLinks: {
-   rss2: `${DOMAIN}/feed.xml`,
-  },
- };
+  const feedOptions = {
+    title: 'Gato GraphQL Blog | RSS Feed',
+    description: 'Stay up to date on the latest from Gato GraphQL and our engineering practices.',
+    id: DOMAIN,
+    link: DOMAIN,
+    image: Logo.src,
+    favicon: Favicon.src,
+    copyright: `All rights reserved ${new Date().getFullYear()}, Gato GraphQL`,
+    generator: 'Feed for Node.js',
+    feedLinks: {
+    rss2: `${DOMAIN}/feed.xml`,
+    },
+  };
 
- const feed = new Feed(feedOptions);
+  const feed = new Feed(feedOptions);
 
- posts.forEach((post) => {
-  feed.addItem({
-   title: post.title,
-   id: slugify(getPostURL(post)),
-   link: getPostURL(post),
-   description: post.summary,
-   date: new Date(post.publishedAt),
+  posts.forEach((post) => {
+    feed.addItem({
+    title: post.title,
+    id: slugify(getPostURL(post)),
+    link: getPostURL(post),
+    description: post.summary,
+    date: new Date(post.publishedAt),
+    });
   });
- });
 
- const rss = feed.rss2()
- fs.writeFileSync('./public/feed.xml', rss);
+  const rss = feed.rss2()
+  fs.writeFileSync('./public/feed.xml', rss);
 }
