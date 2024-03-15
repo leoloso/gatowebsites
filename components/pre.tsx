ebfab43853470a9f56089a3ca9299e1ac7e9526f
leoloso <leo@getpop.org>
2024-03-15 21:22:09 +0800

    use Children.onlyText

--- components/pre.tsx
+++ components/pre.tsx
@@ -1,17 +1,19 @@
 import clsx from 'clsx'
 import { CopyButton } from './copy-button'
+import Children from 'react-children-utilities'
 
 export function Pre({
   children,
-  raw,
+  // raw,
   buttonClasses = 'absolute top-3 right-3 bg-zinc-900',
   ...props
 }: {
   children: React.ReactNode,
-  raw: string,
+  // raw: string,
   buttonClasses: string,
   className: string,
 }) {
+  const raw = Children.onlyText(children)
   return (
     <pre {...props} className={clsx('relative', props.className)}>
       {children}
