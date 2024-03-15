323d376c3ea82b9c00bee296df612027edada752
leoloso <leo@getpop.org>
2024-03-15 21:24:24 +0800

    `@see`

--- components/pre.tsx
+++ components/pre.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx'
 import { CopyButton } from './copy-button'
 import Children from 'react-children-utilities'
 
+// @see https://www.tybarho.com/articles/adding-a-copy-button-mdx-code-snippets
 export function Pre({
   children,
   buttonClasses = 'absolute top-3 right-3 bg-zinc-900',
