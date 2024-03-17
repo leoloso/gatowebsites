75c5aacb571b147f929d6e56b5581680924df10d
leoloso <leo@getpop.org>
2024-03-18 07:54:46 +0800

    fixed styles

--- components/mdx/mdx.tsx
+++ components/mdx/mdx.tsx
@@ -18,7 +18,7 @@ interface MdxProps {
 
 export function Mdx({
   code,
-  mdxClassName = "prose prose-lg dark:prose-invert text-slate-700 dark:text-slate-200 max-w-none prose-p:leading-normal prose-headings:text-slate-800 dark:prose-headings:text-slate-200 prose-a:font-medium prose-a:text-purple-600 dark:prose-a:text-purple-300 prose-a:no-underline hover:prose-a:no-underline hover:prose-a:text-purple-500 hover:dark:prose-a:text-purple-400 prose-strong:text-slate-800 dark:prose-strong:text-slate-100 prose-code:px-1 prose-code:py-0.5 prose-code:rounded prose-code:before:content-[''] prose-code:after:content-[''] prose-pre:bg-slate-800 prose-pre:border prose-pre:border-slate-700 prose-blockquote:italic prose-blockquote:pl-4 prose-blockquote:border-l-2 prose-blockquote:border-gray-200 prose-blockquote:font-normal prose-blockquote:text-gray-500 dark:prose-blockquote:text-gray-300 prose-figcaption:text-slate-500 prose-figcaption:dark:text-slate-300 prose-li:marker:text-slate-400 prose-li:marker:dark:text-slate-400 prose-headings:scroll-mt-20",
+  mdxClassName = "prose-lg text-slate-700 dark:text-slate-200 max-w-none prose-p:leading-normal prose-headings:text-slate-800 dark:prose-headings:text-slate-200 prose-strong:text-slate-800 dark:prose-strong:text-slate-100 prose-blockquote:italic prose-blockquote:pl-4 prose-blockquote:border-l-2 prose-blockquote:border-gray-200 prose-blockquote:font-normal prose-blockquote:text-gray-500 dark:prose-blockquote:text-gray-300 prose-figcaption:text-slate-500 prose-figcaption:dark:text-slate-300",
 }: MdxProps) {
   const Component = useMDXComponent(code)
 
@@ -27,7 +27,7 @@ export function Mdx({
   }
 
   return (
-    <article className={mdxClassName}>
+    <article className={`prose dark:prose-invert prose-a:font-medium prose-a:text-purple-600 dark:prose-a:text-purple-300 prose-a:no-underline hover:prose-a:no-underline hover:prose-a:text-purple-500 hover:dark:prose-a:text-purple-400 prose-code:px-1 prose-code:py-0.5 prose-code:rounded prose-code:before:content-[''] prose-code:after:content-[''] prose-pre:bg-slate-800 prose-pre:border prose-pre:border-slate-700 prose-li:marker:text-slate-400 prose-li:marker:dark:text-slate-400 prose-headings:scroll-mt-20 ${mdxClassName}`}>
       <Component components={{ ...mdxComponents, ...custom }} />
     </article>
   )
