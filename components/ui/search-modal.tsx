fca2657742d4ea0b8652aa88f9ddf9a152fc22ae
leoloso <leo@getpop.org>
2024-03-20 14:25:53 +0800

    adapted actions

--- components/ui/search-modal.tsx
+++ components/ui/search-modal.tsx
@@ -129,7 +129,21 @@ export default function SearchModal({
                 <div className="text-sm font-medium text-slate-500 px-2 mb-2">Actions</div>
                 <ul>
                   <li>
-                    <Link className="flex items-center px-2 py-1 leading-6 text-sm text-slate-800 hover:bg-slate-100 rounded dark:text-slate-200 dark:hover:bg-slate-700 focus-within:bg-slate-100 dark:focus-within:bg-slate-700 outline-none" href="#0">
+                    <Link className="flex items-center px-2 py-1 leading-6 text-sm text-slate-800 hover:bg-slate-100 rounded dark:text-slate-200 dark:hover:bg-slate-700 focus-within:bg-slate-100 dark:focus-within:bg-slate-700 outline-none" href="/contact">
+                      <svg
+                        className="w-3 h-3 fill-purple-500 shrink-0 mr-3"
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M6 0C2.691 0 0 2.362 0 5.267c0 2.905 2.691 5.266 6 5.266a6.8 6.8 0 0 0 1.036-.079l2.725 1.485a.5.5 0 0 0 .739-.439V8.711A4.893 4.893 0 0 0 12 5.267C12 2.362 9.309 0 6 0Z" />
+                      </svg>
+                      <span className="font-medium">Contact us</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="flex items-center px-2 py-1 leading-6 text-sm text-slate-800 hover:bg-slate-100 rounded dark:text-slate-200 dark:hover:bg-slate-700 focus-within:bg-slate-100 dark:focus-within:bg-slate-700 outline-none" href="/support">
                       <svg
                         className="w-3 h-3 fill-blue-600 shrink-0 mr-3"
                         width="12"
@@ -140,20 +154,6 @@ export default function SearchModal({
                         <path d="M11.854.146a.5.5 0 0 0-.525-.116l-11 4a.5.5 0 0 0-.015.934l4.8 1.921 1.921 4.8A.5.5 0 0 0 7.5 12h.008a.5.5 0 0 0 .462-.329l4-11a.5.5 0 0 0-.116-.525Z" />
                       </svg>
                       <span className="font-medium">Contact support</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="flex items-center px-2 py-1 leading-6 text-sm text-slate-800 hover:bg-slate-100 rounded dark:text-slate-200 dark:hover:bg-slate-700 focus-within:bg-slate-100 dark:focus-within:bg-slate-700 outline-none" href="#0">
-                      <svg
-                        className="w-3 h-3 fill-purple-500 shrink-0 mr-3"
-                        width="12"
-                        height="12"
-                        viewBox="0 0 12 12"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M6 0C2.691 0 0 2.362 0 5.267c0 2.905 2.691 5.266 6 5.266a6.8 6.8 0 0 0 1.036-.079l2.725 1.485a.5.5 0 0 0 .739-.439V8.711A4.893 4.893 0 0 0 12 5.267C12 2.362 9.309 0 6 0Z" />
-                      </svg>
-                      <span className="font-medium">Submit feedback</span>
                     </Link>
                   </li>
                 </ul>
