76c463ff973eb15cf57b35f839ac6c1b75b5082e
leoloso <leo@getpop.org>
2024-03-20 14:41:05 +0800

    Popular items

--- components/ui/search-modal.tsx
+++ components/ui/search-modal.tsx
@@ -123,6 +123,20 @@ export default function SearchModal({
                       <span>Nested mutations</span>
                     </Link>
                   </li>
+                  <li>
+                    <Link className="flex items-center px-2 py-1 leading-6 text-sm text-slate-800 hover:bg-slate-100 rounded dark:text-slate-200 dark:hover:bg-slate-700 focus-within:bg-slate-100 dark:focus-within:bg-slate-700 outline-none" href={`/${AppConfig.paths.features}/schema-namespacing`}>
+                      <svg
+                        className="w-3 h-3 fill-slate-400 shrink-0 mr-3 dark:fill-slate-500"
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M11.953 4.29a.5.5 0 0 0-.454-.292H6.14L6.984.62A.5.5 0 0 0 6.12.173l-6 7a.5.5 0 0 0 .379.825h5.359l-.844 3.38a.5.5 0 0 0 .864.445l6-7a.5.5 0 0 0 .075-.534Z" />
+                      </svg>
+                      <span>Schema namespacing</span>
+                    </Link>
+                  </li>
                 </ul>
               </div>
               {/* Actions */}
