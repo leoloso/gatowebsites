a56099787b4d03b9320ca94400595525d79c82cb
leoloso <leo@getpop.org>
2024-03-21 15:36:02 +0800

    added docs, extensions and features

--- app/api/deploy-algolia/route.tsx
+++ app/api/deploy-algolia/route.tsx
@@ -1,7 +1,19 @@
 import algoliaSearch from "algoliasearch"
-import { allPosts } from 'contentlayer/generated'
+import {
+  allPosts,
+  // allUpdates,
+  // allVideoPosts,
+  allDocs,
+  allExtensions,
+  allFeatures,
+} from '@/.contentlayer/generated'
 import { ALGOLIA_API_CREDENTIALS } from '@/data/env/algolia'
-import { getPostURLPath } from "@/utils/content/application-urls"
+import {
+  getDocURLPath,
+  getExtensionURLPath,
+  getFeatureURLPath,
+  getPostURLPath,
+} from "@/utils/content/application-urls"
 import slugify from "@sindresorhus/slugify"
 
 export interface SearchObject {
@@ -9,7 +21,7 @@ export interface SearchObject {
   title: string
   description: string
   slug: string
-  date: string
+  date?: string
   thumbnail?: string
 }
 
@@ -32,15 +44,76 @@ async function getAllPostsTransformed(): Promise<SearchObject[]> {
   )
 }
 
+async function getAllDocsTransformed(): Promise<SearchObject[]> {
+  // return an array of objects to be added to Algolia.
+  return (
+    allDocs?.map((doc) => {
+      const urlPath = getDocURLPath(doc)
+      return {
+        // objectID must be unique and consistent on each build
+        objectID: slugify(urlPath),
+        title: doc.title,
+        description: doc.description,
+        slug: doc.slug,
+        urlPath: urlPath,
+      }
+    }) || []
+  )
+}
+
+async function getAllExtensionsTransformed(): Promise<SearchObject[]> {
+  // return an array of objects to be added to Algolia.
+  return (
+    allExtensions?.map((extension) => {
+      const urlPath = getExtensionURLPath(extension)
+      return {
+        // objectID must be unique and consistent on each build
+        objectID: slugify(urlPath),
+        title: extension.title,
+        description: extension.description,
+        slug: extension.slug,
+        urlPath: urlPath,
+        thumbnail: extension.image,
+      }
+    }) || []
+  )
+}
+
+async function getAllFeaturesTransformed(): Promise<SearchObject[]> {
+  // return an array of objects to be added to Algolia.
+  return (
+    allFeatures?.map((feature) => {
+      const urlPath = getFeatureURLPath(feature)
+      return {
+        // objectID must be unique and consistent on each build
+        objectID: slugify(urlPath),
+        title: feature.title,
+        description: feature.description,
+        slug: feature.slug,
+        urlPath: urlPath,
+        thumbnail: feature.image,
+      }
+    }) || []
+  )
+}
+
 export async function GET(request: Request) {
   try {
     const posts = await getAllPostsTransformed()
+    const docs = await getAllDocsTransformed()
+    const extensions = await getAllExtensionsTransformed()
+    const features = await getAllFeaturesTransformed()
     const client = algoliaSearch(
       ALGOLIA_API_CREDENTIALS.appId,
       ALGOLIA_API_CREDENTIALS.searchAdminKey
     )
     const searchIndex = client.initIndex(ALGOLIA_API_CREDENTIALS.indexName)
-    const algoliaSearchIndexResponse = await searchIndex.saveObjects([...posts]) // Add any more data needed to this array
+    const algoliaSearchIndexResponse = await searchIndex.saveObjects([
+      ...posts,
+      ...docs,
+      ...extensions,
+      ...features
+    ])
     
     return new Response(`⭐️⭐️ Successfully added ${algoliaSearchIndexResponse.objectIDs.length} records to Algolia search. ⭐️⭐️`)
   } catch (err) {
