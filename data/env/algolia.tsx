6397eb9976459a0ecd4b2f4c503fc2eec9fa22b5
leoloso <leo@getpop.org>
2024-03-21 14:57:26 +0800

    ALGOLIA_API_CREDENTIALS

---
+++ data/env/algolia.tsx
@@ -0,0 +1,5 @@
+export const ALGOLIA_API_CREDENTIALS = {
+  appId: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || '',
+  searchAPIKey: process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || '',
+  searchAdminKey: process.env.ALGOLIA_SEARCH_ADMIN_KEY || ''
+} 
