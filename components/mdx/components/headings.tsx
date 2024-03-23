1fbf54d7a8d1db8fbc4e3955bb2b01236363a12d
leoloso <leo@getpop.org>
2024-03-23 19:15:42 +0800

    moved # outside heading

--- components/mdx/components/headings.tsx
+++ components/mdx/components/headings.tsx
@@ -4,7 +4,10 @@ import Link from "next/link";
 export const H1 = ({ id, children, ...rest }: { id?: string, children?: React.ReactNode }) => {
   if (id) {
     return (
-      <h1 id={`${id}`} {...rest}><span className='group'>{children}<Link href={`#${id}`} className='hidden group-hover:inline not-prose text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link></span></h1>
+      <span className='group flex items-center'>
+        <h1 id={`${id}`} {...rest}>{children}</h1>
+        <Link href={`#${id}`} className='hidden group-hover:inline not-prose scroll-smooth text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link>
+      </span>
     );
   }
   return <h1 id={`${id}`} {...rest} />;
@@ -13,7 +16,10 @@ export const H1 = ({ id, children, ...rest }: { id?: string, children?: React.Re
 export const H2 = ({ id, children, ...rest }: { id?: string, children?: React.ReactNode }) => {
   if (id) {
     return (
-      <h2 id={`${id}`} {...rest}><span className='group'>{children}<Link href={`#${id}`} className='hidden group-hover:inline not-prose text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link></span></h2>
+      <span className='group flex items-center'>
+        <h2 id={`${id}`} {...rest}>{children}</h2>
+        <Link href={`#${id}`} className='hidden group-hover:inline not-prose scroll-smooth text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link>
+      </span>
     );
   }
   return <h2 id={`${id}`} {...rest} />;
@@ -22,7 +28,10 @@ export const H2 = ({ id, children, ...rest }: { id?: string, children?: React.Re
 export const H3 = ({ id, children, ...rest }: { id?: string, children?: React.ReactNode }) => {
   if (id) {
     return (
-      <h3 id={`${id}`} {...rest}><span className='group'>{children}<Link href={`#${id}`} className='hidden group-hover:inline not-prose text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link></span></h3>
+      <span className='group flex items-center'>
+        <h3 id={`${id}`} {...rest}>{children}</h3>
+        <Link href={`#${id}`} className='hidden group-hover:inline not-prose scroll-smooth text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link>
+      </span>
     );
   }
   return <h3 id={`${id}`} {...rest} />;
@@ -31,7 +40,10 @@ export const H3 = ({ id, children, ...rest }: { id?: string, children?: React.Re
 export const H4 = ({ id, children, ...rest }: { id?: string, children?: React.ReactNode }) => {
   if (id) {
     return (
-      <h4 id={`${id}`} {...rest}><span className='group'>{children}<Link href={`#${id}`} className='hidden group-hover:inline not-prose text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link></span></h4>
+      <span className='group flex items-center'>
+        <h4 id={`${id}`} {...rest}>{children}</h4>
+        <Link href={`#${id}`} className='hidden group-hover:inline not-prose scroll-smooth text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link>
+      </span>
     );
   }
   return <h4 id={`${id}`} {...rest} />;
@@ -40,7 +52,10 @@ export const H4 = ({ id, children, ...rest }: { id?: string, children?: React.Re
 export const H5 = ({ id, children, ...rest }: { id?: string, children?: React.ReactNode }) => {
   if (id) {
     return (
-      <h5 id={`${id}`} {...rest}><span className='group'>{children}<Link href={`#${id}`} className='hidden group-hover:inline not-prose text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link></span></h5>
+      <span className='group flex items-center'>
+        <h5 id={`${id}`} {...rest}>{children}</h5>
+        <Link href={`#${id}`} className='hidden group-hover:inline not-prose scroll-smooth text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link>
+      </span>
     );
   }
   return <h5 id={`${id}`} {...rest} />;
@@ -49,7 +64,10 @@ export const H5 = ({ id, children, ...rest }: { id?: string, children?: React.Re
 export const H6 = ({ id, children, ...rest }: { id?: string, children?: React.ReactNode }) => {
   if (id) {
     return (
-      <h6 id={`${id}`} {...rest}><span className='group'>{children}<Link href={`#${id}`} className='hidden group-hover:inline not-prose text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link></span></h6>
+      <span className='group flex items-center'>
+        <h6 id={`${id}`} {...rest}>{children}</h6>
+        <Link href={`#${id}`} className='hidden group-hover:inline not-prose scroll-smooth text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link>
+      </span>
     );
   }
   return <h6 id={`${id}`} {...rest} />;
