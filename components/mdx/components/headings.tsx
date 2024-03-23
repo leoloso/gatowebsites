8ab94d55ad902b229fa509d8b8c3186be5125426
leoloso <leo@getpop.org>
2024-03-23 19:12:21 +0800

    better heading

--- components/mdx/components/headings.tsx
+++ components/mdx/components/headings.tsx
@@ -1,11 +1,10 @@
+import clsx from "clsx";
 import Link from "next/link";
 
 export const H1 = ({ id, children, ...rest }: { id?: string, children?: React.ReactNode }) => {
   if (id) {
     return (
-      <Link href={`#${id}`} className='group'>
-        <h1 id={`${id}`} {...rest}>{children}<span className='hidden group-hover:inline text-slate-300 dark:text-slate-500 ml-2'>#</span></h1>
-      </Link>
+      <h1 id={`${id}`} {...rest}><span className='group'>{children}<Link href={`#${id}`} className='hidden group-hover:inline not-prose text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link></span></h1>
     );
   }
   return <h1 id={`${id}`} {...rest} />;
@@ -14,9 +13,7 @@ export const H1 = ({ id, children, ...rest }: { id?: string, children?: React.Re
 export const H2 = ({ id, children, ...rest }: { id?: string, children?: React.ReactNode }) => {
   if (id) {
     return (
-      <Link href={`#${id}`} className='group'>
-        <h2 id={`${id}`} {...rest}>{children}<span className='hidden group-hover:inline text-slate-300 dark:text-slate-500 ml-2'>#</span></h2>
-      </Link>
+      <h2 id={`${id}`} {...rest}><span className='group'>{children}<Link href={`#${id}`} className='hidden group-hover:inline not-prose text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link></span></h2>
     );
   }
   return <h2 id={`${id}`} {...rest} />;
@@ -25,9 +22,7 @@ export const H2 = ({ id, children, ...rest }: { id?: string, children?: React.Re
 export const H3 = ({ id, children, ...rest }: { id?: string, children?: React.ReactNode }) => {
   if (id) {
     return (
-      <Link href={`#${id}`} className='group'>
-        <h3 id={`${id}`} {...rest}>{children}<span className='hidden group-hover:inline text-slate-300 dark:text-slate-500 ml-2'>#</span></h3>
-      </Link>
+      <h3 id={`${id}`} {...rest}><span className='group'>{children}<Link href={`#${id}`} className='hidden group-hover:inline not-prose text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link></span></h3>
     );
   }
   return <h3 id={`${id}`} {...rest} />;
@@ -36,9 +31,7 @@ export const H3 = ({ id, children, ...rest }: { id?: string, children?: React.Re
 export const H4 = ({ id, children, ...rest }: { id?: string, children?: React.ReactNode }) => {
   if (id) {
     return (
-      <Link href={`#${id}`} className='group'>
-        <h4 id={`${id}`} {...rest}>{children}<span className='hidden group-hover:inline text-slate-300 dark:text-slate-500 ml-2'>#</span></h4>
-      </Link>
+      <h4 id={`${id}`} {...rest}><span className='group'>{children}<Link href={`#${id}`} className='hidden group-hover:inline not-prose text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link></span></h4>
     );
   }
   return <h4 id={`${id}`} {...rest} />;
@@ -47,9 +40,7 @@ export const H4 = ({ id, children, ...rest }: { id?: string, children?: React.Re
 export const H5 = ({ id, children, ...rest }: { id?: string, children?: React.ReactNode }) => {
   if (id) {
     return (
-      <Link href={`#${id}`} className='group'>
-        <h5 id={`${id}`} {...rest}>{children}<span className='hidden group-hover:inline text-slate-300 dark:text-slate-500 ml-2'>#</span></h5>
-      </Link>
+      <h5 id={`${id}`} {...rest}><span className='group'>{children}<Link href={`#${id}`} className='hidden group-hover:inline not-prose text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link></span></h5>
     );
   }
   return <h5 id={`${id}`} {...rest} />;
@@ -58,9 +49,7 @@ export const H5 = ({ id, children, ...rest }: { id?: string, children?: React.Re
 export const H6 = ({ id, children, ...rest }: { id?: string, children?: React.ReactNode }) => {
   if (id) {
     return (
-      <Link href={`#${id}`} className='group'>
-        <h6 id={`${id}`} {...rest}>{children}<span className='hidden group-hover:inline text-slate-300 dark:text-slate-500 ml-2'>#</span></h6>
-      </Link>
+      <h6 id={`${id}`} {...rest}><span className='group'>{children}<Link href={`#${id}`} className='hidden group-hover:inline not-prose text-slate-300 dark:text-slate-500 hover:no-underline hover:text-slate-300 hover:dark:text-slate-400 ml-2'>#</Link></span></h6>
     );
   }
   return <h6 id={`${id}`} {...rest} />;
