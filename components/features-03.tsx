a953ff318bd33c15387f6f74c3d4403212687dd5
leoloso <leo@getpop.org>
2024-04-03 16:03:42 +0800

    styles

--- components/features-03.tsx
+++ components/features-03.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Highlighter, { HighlighterItem } from './highlighter'
-import Illustration from '@/public/assets/theme/glow-top.svg'
+import Particles from './particles'
 
 import FeatureImg04 from '@/public/assets/theme/feature-image-04.png'
 import SectionHeader from './section-header'
@@ -11,11 +11,22 @@ export default function Features03() {
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
 
-        {/* Illustration */}
-        <div className="absolute inset-0 -z-10 -mx-28 rounded-t-[3rem] pointer-events-none overflow-hidden" aria-hidden="true">
-          <div className="absolute left-1/2 -translate-x-1/2 top-0 -z-10">
-            <Image src={Illustration} className="max-w-none" width={1404} height={658} alt="Features Illustration" />
-          </div>
+        {/* Particles animation */}
+        <div className="absolute left-1/2 -translate-x-1/2 top-0 -z-10 w-80 h-80 -mt-24 -ml-32">
+          <Particles className="absolute inset-0 -z-10" quantity={6} staticity={30} />    
+        </div>
+
+        {/* Blurred shape */}
+        <div className="absolute top-0 mt-32 -translate-y-1/4 left-1/2 -translate-x-1/2 blur-2xl opacity-50 pointer-events-none -z-10 rotate-[90deg]" aria-hidden="true">
+          <svg xmlns="http://www.w3.org/2000/svg" width="434" height="427">
+            <defs>
+              <linearGradient id="bs3-a" x1="19.609%" x2="50%" y1="14.544%" y2="100%">
+                <stop offset="0%" stopColor="#6366F1" />
+                <stop offset="100%" stopColor="#6366F1" stopOpacity="0" />
+              </linearGradient>
+            </defs>
+            <path fill="url(#bs3-a)" fillRule="evenodd" d="m410 0 461 369-284 58z" transform="matrix(1 0 0 -1 -410 427)" />
+          </svg>
         </div>
 
         <div className="pt-16 pb-12 md:pt-32 md:pb-20">
