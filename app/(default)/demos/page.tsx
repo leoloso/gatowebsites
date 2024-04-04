dff9ddd136ffa2df8a849c865b2a5e37eb51a2ca
leoloso <leo@getpop.org>
2024-04-04 19:14:21 +0800

    Use DemoPostThumb for featured

--- app/(default)/demos/page.tsx
+++ app/(default)/demos/page.tsx
@@ -13,6 +13,7 @@ import Pagination from '@/components/pagination'
 import AppSettings from '@/app/app.settings'
 
 import DemoPostList from '@/components/demo-post-list'
+import DemoPostThumb from '@/components/demo-post-thumb'
 
 export const metadata = {
   title: 'Demos - Gato GraphQL',
@@ -51,11 +52,14 @@ export default function Demos() {
               <article className="max-w-3xl mx-auto md:max-w-none grid md:grid-cols-2 gap-6 md:gap-8 lg:gap-12 xl:gap-16 items-center">
                 <Link href={getDemoPostURLPath(featuredDemoPost)} className="relative block group" data-aos="fade-right" data-aos-delay="200">
                   <div className="absolute inset-0 bg-gray-800 hidden md:block transform md:translate-y-2 md:translate-x-4 xl:translate-y-4 xl:translate-x-8 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out pointer-events-none" aria-hidden="true"></div>
-                  {featuredDemoPost.image &&
+                  {/* {featuredDemoPost.image &&
                     <figure className="relative h-0 pb-[56.25%] md:pb-[75%] lg:pb-[56.25%] overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out">
                       <Image className="absolute inset-0 w-full h-full object-cover transform hover:scale-105 transition duration-700 ease-out" src={featuredDemoPost.image} width="540" height="303" alt={featuredDemoPost.title} />
                     </figure>
-                  }
+                  } */}
+                  <div className="relative overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out">
+                    <DemoPostThumb demoPost={featuredDemoPost} />
+                  </div>
                 </Link>
                 <div data-aos="fade-left" data-aos-delay="200">
                   <header>
