d69dedde6833a20882d592f1e89f33586a38d637
leoloso <leo@getpop.org>
2024-04-09 13:06:02 +0800

    filteredDemoPosts

--- app/(default)/demos/demo-posts-section.tsx
+++ app/(default)/demos/demo-posts-section.tsx
@@ -1,29 +1,36 @@
+'use client'
+
 import { allDemoPosts } from 'contentlayer/generated'
 import Link from 'next/link'
 import PostDate from '@/components/post-date'
 import PostTags from '@/components/post-tags'
-
 import { getDemoPostURLPath } from '@/utils/content/application-urls'
 import { sortByPublishedAt } from '@/utils/content/sort'
 import PageHeader from '@/components/page-header'
 import Pagination from '@/components/pagination'
 import AppSettings from '@/app/app.settings'
-
 import DemoPostList from '@/components/demo-post-list'
 import DemoPostThumb from '@/components/demo-post-thumb'
 import { Suspense } from 'react'
 import AppConfig from '@/app/app.config'
+import { useSearchParams } from 'next/navigation';
 
 export default function DemoPostsSection() {
 
   // Sort demoPosts by date
   allDemoPosts.sort(sortByPublishedAt)  
+  
+  // Filter posts by tag
+  const searchParams = useSearchParams();
+  const tag = searchParams.get('tag');
 
   const featuredDemoPost = allDemoPosts[0]
 
-  const demoPosts = allDemoPosts.slice(1) // Remove the featured image
+  const demoPosts = tag ? allDemoPosts : allDemoPosts.slice(1) // Remove the featured image
 
-  const totalPages = Math.ceil(demoPosts.length / AppSettings.postsPerPage.demos)
+  const filteredDemoPosts = tag ? demoPosts.filter((post) => (post.tags?.includes(tag))) : demoPosts
+
+  const totalPages = Math.ceil(filteredDemoPosts.length / AppSettings.postsPerPage.demos)
 
   return (        
     <div className="max-w-6xl mx-auto px-4 sm:px-6">
@@ -38,47 +45,49 @@ export default function DemoPostsSection() {
         />
 
         {/*  Featured article */}
-        <div className="pb-12 md:pb-20">
-          <article className="max-w-3xl mx-auto md:max-w-none grid md:grid-cols-2 gap-6 md:gap-8 lg:gap-12 xl:gap-16 items-center">
-            <Link href={getDemoPostURLPath(featuredDemoPost)} className="relative block group" data-aos="fade-right" data-aos-delay="200">
-              <div className="absolute inset-0 bg-gray-700 hidden md:block transform md:translate-y-2 md:translate-x-4 xl:translate-y-4 xl:translate-x-8 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out pointer-events-none" aria-hidden="true"></div>
-              {/* {featuredDemoPost.image &&
-                <figure className="relative h-0 pb-[56.25%] md:pb-[75%] lg:pb-[56.25%] overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out">
-                  <Image className="absolute inset-0 w-full h-full object-cover transform hover:scale-105 transition duration-700 ease-out" src={featuredDemoPost.image} width="540" height="303" alt={featuredDemoPost.title} />
-                </figure>
-              } */}
-              <div className="relative overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out">
-                <DemoPostThumb
-                  demoPost={featuredDemoPost}
-                  paddingClassname="py-5 px-4 md:py-8 md:px-6"
-                />
+        {!tag && (
+          <div className="pb-12 md:pb-20">
+            <article className="max-w-3xl mx-auto md:max-w-none grid md:grid-cols-2 gap-6 md:gap-8 lg:gap-12 xl:gap-16 items-center">
+              <Link href={getDemoPostURLPath(featuredDemoPost)} className="relative block group" data-aos="fade-right" data-aos-delay="200">
+                <div className="absolute inset-0 bg-gray-700 hidden md:block transform md:translate-y-2 md:translate-x-4 xl:translate-y-4 xl:translate-x-8 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out pointer-events-none" aria-hidden="true"></div>
+                {/* {featuredDemoPost.image &&
+                  <figure className="relative h-0 pb-[56.25%] md:pb-[75%] lg:pb-[56.25%] overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out">
+                    <Image className="absolute inset-0 w-full h-full object-cover transform hover:scale-105 transition duration-700 ease-out" src={featuredDemoPost.image} width="540" height="303" alt={featuredDemoPost.title} />
+                  </figure>
+                } */}
+                <div className="relative overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out">
+                  <DemoPostThumb
+                    demoPost={featuredDemoPost}
+                    paddingClassname="py-5 px-4 md:py-8 md:px-6"
+                  />
+                </div>
+              </Link>
+              <div data-aos="fade-left" data-aos-delay="200">
+                <header>
+                  <div className="mb-3">
+                    {featuredDemoPost.tags &&
+                      <div className="mb-3">
+                        <PostTags tags={featuredDemoPost.tags} baseURL={`/${AppConfig.paths.demoPosts}`} />
+                      </div>
+                    }
+                  </div>
+                  <h3 className="h3 text-2xl lg:text-3xl mb-2">
+                    <Link href={getDemoPostURLPath(featuredDemoPost)} className="hover:text-purple-300 transition duration-150 ease-in-out">{featuredDemoPost.title}</Link>
+                  </h3>
+                </header>
+                <p className="text-lg text-gray-400 grow">{featuredDemoPost.description}</p>
+                <footer className="flex items-center mt-4">
+                  <img className="rounded-full shrink-0 mr-4" src={featuredDemoPost.authorImg} width={40} height={40} alt={featuredDemoPost.author} />
+                  <div>
+                    <span className="font-medium text-gray-200">{featuredDemoPost.author}</span>
+                    <span className="text-gray-700"> - </span>
+                    <span className="text-gray-500"><PostDate dateString={featuredDemoPost.publishedAt} /></span>
+                  </div>
+                </footer>
               </div>
-            </Link>
-            <div data-aos="fade-left" data-aos-delay="200">
-              <header>
-                <div className="mb-3">
-                  {featuredDemoPost.tags &&
-                    <div className="mb-3">
-                      <PostTags tags={featuredDemoPost.tags} baseURL={`/${AppConfig.paths.demoPosts}`} />
-                    </div>
-                  }
-                </div>
-                <h3 className="h3 text-2xl lg:text-3xl mb-2">
-                  <Link href={getDemoPostURLPath(featuredDemoPost)} className="hover:text-purple-300 transition duration-150 ease-in-out">{featuredDemoPost.title}</Link>
-                </h3>
-              </header>
-              <p className="text-lg text-gray-400 grow">{featuredDemoPost.description}</p>
-              <footer className="flex items-center mt-4">
-                <img className="rounded-full shrink-0 mr-4" src={featuredDemoPost.authorImg} width={40} height={40} alt={featuredDemoPost.author} />
-                <div>
-                  <span className="font-medium text-gray-200">{featuredDemoPost.author}</span>
-                  <span className="text-gray-700"> - </span>
-                  <span className="text-gray-500"><PostDate dateString={featuredDemoPost.publishedAt} /></span>
-                </div>
-              </footer>
-            </div>
-          </article>
-        </div>
+            </article>
+          </div>
+        )}
 
         {/*  Articles list */}
         <div className="max-w-3xl mx-auto md:max-w-none">
@@ -88,7 +97,7 @@ export default function DemoPostsSection() {
 
           {/*  Articles container */}
           <Suspense>
-            <DemoPostList demoPosts={demoPosts} />
+            <DemoPostList demoPosts={filteredDemoPosts} />
           </Suspense>
 
         </div>
