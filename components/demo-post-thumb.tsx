f07aa99a41e1d000313613a003aae0f94dbb0ef3
leoloso <leo@getpop.org>
2024-04-10 16:48:07 +0800

    svgClassname

--- components/demo-post-thumb.tsx
+++ components/demo-post-thumb.tsx
@@ -15,6 +15,7 @@ export default function DemoPostThumb({
   svgImage,
   logoImage,
   reverseItems = false,
+  svgClassname = "mx-2 my-4",
 }: {
   demoPost: DemoPost,
   paddingClassname?: string,
@@ -23,6 +24,7 @@ export default function DemoPostThumb({
   svgImage?: StaticImageData,
   logoImage?: StaticImageData,
   reverseItems?: boolean,
+  svgClassname?: string,
 }) {
   return (
     <div
@@ -76,7 +78,7 @@ export default function DemoPostThumb({
           <div className={clsx(!isLandscape && "mb-2 md:mb-4", isLandscape &&  'mr-2')}>
             <Image src={logoImage || Logo} alt={demoPost.title} width={250} height={175} />
           </div>
-          <div className="mx-2 my-4 fill-slate-300 text-white">
+          <div className={svgClassname}>
             <Image src={svgImage || PlusImage} width={30} height={30} alt="plus image" />
           </div>
           <div className={clsx("flex items-center justify-center", !isLandscape &&  "gap-6 sm:gap-12 mt-2 md:mt-4", isLandscape &&  "flex-col ml-2")}>
