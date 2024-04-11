1fc243199be6df2c02ca4eab0cc449fec422a237
leoloso <leo@getpop.org>
2024-04-11 14:52:15 +0800

    Re-arranged footer

--- components/ui/footer-menu.tsx
+++ components/ui/footer-menu.tsx
@@ -72,6 +72,42 @@ export default function FooterMenu({
 
       {/* 3rd block */}
       <div className={columnClassname}>
+        <h6 className="text-sm text-slate-50 font-medium mb-2">Comparisons</h6>
+        <ul className="text-sm space-y-2">
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/comparisons">Overview</a>
+          </li>
+          {allComparisonPosts.map((comparisonPost, index) => (
+            <li key={index}>
+              <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={getComparisonPostURL(comparisonPost)}>{comparisonPost.title}</a>
+            </li>
+          ))}
+        </ul>
+        <h6 className="text-sm text-slate-50 font-medium mb-2 mt-8 lg:mt-6">Resources</h6>
+        <ul className="text-sm space-y-2">
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.wpDirectory} target="_blank">Plugin in WordPress.org</a>
+          </li>
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.githubRepo} target="_blank">GitHub</a>
+          </li>
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.githubExtensionStarter} target="_blank">Extension starter</a>
+          </li>
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.nextjsWordPressStarter} target="_blank">Next.js WordPress starter</a>
+          </li>
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.docs.architecture}`}>Architecture docs</a>
+          </li>
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/specials/wpbuilds">WPBuilds series</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* 4th block */}
+      <div className={columnClassname}>
         <h6 className="text-sm text-slate-50 font-medium mb-2">Company</h6>
         <ul className="text-sm space-y-2">
           <li>
@@ -92,42 +128,6 @@ export default function FooterMenu({
           </li>
           <li>
             <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/developers">Developer Partnership Program</a>
-          </li>
-        </ul>
-      </div>
-
-      {/* 4th block */}
-      <div className={columnClassname}>
-        <h6 className="text-sm text-slate-50 font-medium mb-2">Comparisons</h6>
-        <ul className="text-sm space-y-2">
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/comparisons">Overview</a>
-          </li>
-          {allComparisonPosts.map((comparisonPost, index) => (
-            <li key={index}>
-              <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={getComparisonPostURL(comparisonPost)}>{comparisonPost.title}</a>
-            </li>
-          ))}
-        </ul>
-        <h6 className="text-sm text-slate-50 font-medium mb-2 mt-8 lg:mt-6">Resources</h6>
-        <ul className="text-sm space-y-2">
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.wpDirectory} target="_blank">WordPress.org</a>
-          </li>
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.githubRepo} target="_blank">GitHub</a>
-          </li>
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.githubExtensionStarter} target="_blank">Extension starter</a>
-          </li>
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.nextjsWordPressStarter} target="_blank">Next.js WordPress starter</a>
-          </li>
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.docs.architecture}`}>Architecture docs</a>
-          </li>
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/specials/wpbuilds">WPBuilds series</a>
           </li>
         </ul>
       </div>
