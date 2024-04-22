cd811524581f1e49f397777e0d1b0a0f95d3e6d5
leoloso <leo@getpop.org>
2024-04-22 10:40:59 -0300

    WithTitleThumb

--- components/thumbnails/with-title-thumb.tsx
+++ components/thumbnails/with-title-thumb.tsx
@@ -1,22 +1,17 @@
 import Logo from '@/public/assets/GatoGraphQL-logo-suki-rectangular.png'
 import Image from 'next/image'
-import clsx from 'clsx';
 import Thumb from './thumb';
 
 export default function WithTitleThumb({
   title,
   paddingClassname,
   bgClassname,
-  isLandscape = false,
-  reverseItems = false,
-  titleClassname = "mx-2 my-4",
+  titleClassname = "h2",
   numberParticles,
 }: {
   title: string,
   paddingClassname?: string,
   bgClassname?: string,
-  isLandscape?: boolean,
-  reverseItems?: boolean,
   titleClassname?: string,
   numberParticles?: number,
 }) {
@@ -26,8 +21,8 @@ export default function WithTitleThumb({
       bgClassname={bgClassname}
       numberParticles={numberParticles}
     >
-      <div className={clsx("flex items-center justify-center", !isLandscape && "flex-col", isLandscape && reverseItems && "flex-row-reverse", !isLandscape && reverseItems && "flex-col-reverse", isLandscape && "aspect-video")}>
-        <div className={clsx(!isLandscape && "mb-2 md:mb-4", isLandscape &&  'mx-2')}>
+      <div className='flex items-center justify-center aspect-video'>
+        <div className='mx-2'>
           <Image src={Logo} alt="Gato GraphQL logo" width={250} height={175} />
         </div>
         <h2 className={titleClassname}>
