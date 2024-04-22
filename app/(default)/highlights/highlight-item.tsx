356c77ccf347336f9394ac51237a4f4871e81ba5
leoloso <leo@getpop.org>
2024-04-22 14:14:56 -0300

    scrollTop

--- app/(default)/highlights/highlight-item.tsx
+++ app/(default)/highlights/highlight-item.tsx
@@ -16,7 +16,7 @@ export default function HighlightItem({ ...props }) {
           </span>
         </div>
         <div className="grow ml-8 md:ml-0 pb-12 group-last-of-type:pb-0 border-b [border-image:linear-gradient(to_right,theme(colors.slate.700/.3),theme(colors.slate.700),theme(colors.slate.700/.3))1] group-last-of-type:border-none">
-          <header id={slugify(props.title)} className='pb-8'>
+          <header id={slugify(props.title)} className='scroll-mt-20 pb-8'>
             {/* <h2 className="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 leading-8 pb-6">{props.title}</h2> */}
             <WithTitleThumb
               title={props.title}
