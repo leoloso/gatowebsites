022f35f30526a2e013a634b538699ce8396920b1
leoloso <leo@getpop.org>
2024-04-22 10:23:00 -0300

    renamed file

--- components/demo-post-thumb.tsx
+++ components/demo-post-thumb.tsx
@@ -1,7 +1,7 @@
 
 import { DemoPost } from '@/.contentlayer/generated';
 import { StaticImageData } from 'next/image'
-import WithLogoThumb from './thumbnails/thumb';
+import WithLogoThumb from './thumbnails/with-logo-thumb';
 
 export default function DemoPostThumb({
   demoPost,
