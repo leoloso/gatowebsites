809ca7da0d09b7fb232d1500747d0679dc2fccb7
leoloso <leo@getpop.org>
2024-04-22 12:45:01 -0300

    No pagination

--- app/(default)/highlights/page.tsx
+++ app/(default)/highlights/page.tsx
@@ -51,26 +51,6 @@ export default function Highlights() {
               </div>
             </div>
 
-            {/* Pagination */}
-            <div className="max-w-3xl mx-auto">
-              <ul className="flex items-center justify-between mt-12 pl-8 md:pl-48">
-                <li>
-                  <span className="btn-sm text-slate-700 transition duration-150 ease-in-out group [background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none cursor-not-allowed">
-                    <span className="relative inline-flex items-center">
-                      <span className="tracking-normal text-slate-700 mr-1">&lt;-</span> Previous Page
-                    </span>
-                  </span>
-                </li>
-                <li>
-                  <a className="btn-sm text-slate-300 hover:text-white transition duration-150 ease-in-out group [background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none" href="#0">
-                    <span className="relative inline-flex items-center">
-                      Next Page <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
-                    </span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-
           </div>
         </div>
 
