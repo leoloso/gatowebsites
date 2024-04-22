f3c2a678640e3b93379b3fca1b7ee507b9c3bb8b
leoloso <leo@getpop.org>
2024-04-22 12:00:28 -0300

    Back to /features#pro

--- app/(default)/features/[slug]/page.tsx
+++ app/(default)/features/[slug]/page.tsx
@@ -59,7 +59,7 @@ export default async function SingleFeature({ params }: {
   return (
     <ArtifactSection
       artifact={feature}
-      sectionURL={`/${AppConfig.paths.features}`}
+      sectionURL={`/${AppConfig.paths.features}${feature.category === 'Free plugin' ? '' : '#pro'}`}
       testimonialIndex={feature.category === 'Free plugin' ? 1 : 4}
       defaultArtifactIcon={feature.category === 'Free plugin' ? DefaultArtifactIcon02 : DefaultArtifactIcon04}
       bgClassname={clsx("bg-gradient-to-tr", feature.category === 'Free plugin' && "from-slate-900 to-brown-900", feature.category !== 'Free plugin' && "from-slate-900 to-fuchsia-900")}
