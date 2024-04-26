22df50eda3123dc85b635dbd9f7f7faa903d44a9
leoloso <leo@getpop.org>
2024-04-26 12:14:28 -0300

    make newsletter forms dynamic

--- components/forms/newsletter-02.tsx
+++ components/forms/newsletter-02.tsx
@@ -1,12 +1,64 @@
+'use client';
+
 import AppConfig from '@/app/app.config'
+import { useState } from 'react';
+import Alert from '../mdx/components/alert';
+import clsx from 'clsx';
+import { handleFormSubmit, canSubmitForm, FormStatus } from './form-handler';
 
 export default function NewsletterForm() {
+  const formURL = AppConfig.services.newsletter.formActionURL
+
+  const [status, setStatus] = useState<FormStatus>('pending');
+  const [error, setError] = useState<string|null>(null);
+  const isFormEnabled = canSubmitForm(status)
+
   return (
-    <form className="inline-flex max-w-sm w-full mt-8" action={AppConfig.services.newsletter.formActionURL} method="post" target="_blank">
-      <div className="w-full flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-none">
-        <input id={AppConfig.services.newsletter.emailFieldName} name={AppConfig.services.newsletter.emailFieldName} type="email" className="form-input py-1.5 w-full mb-3 sm:mb-0 sm:mr-2 rounded-full bg-slate-800/30 border-slate-700" placeholder="Your email" aria-label="Your email" required />
-          <button className="btn text-slate-900 bg-gradient-to-r from-white/80 via-white to-white/80 hover:bg-white transition duration-150 ease-in-out group" type="submit">Subscribe <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span></button>
+    <form
+      className="mt-8 flex flex-col items-center"
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleFormSubmit(formURL, e.target as HTMLFormElement, setStatus, setError)
+      }}
+      // action={AppConfig.services.newsletter.formActionURL}
+      // method="post"
+      // target="_blank"
+    >
+      <div className="inline-flex max-w-sm w-full">
+        <div className="w-full flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-none">
+          <input
+            id={AppConfig.services.newsletter.emailFieldName}
+            name={AppConfig.services.newsletter.emailFieldName}
+            type="email"
+            className="form-input py-1.5 w-full mb-3 sm:mb-0 sm:mr-2 rounded-full bg-slate-800/30 border-slate-700"
+            placeholder="Your email…"
+            aria-label="Your email…"
+            required
+            readOnly={!isFormEnabled}
+          />
+          <button
+            className={clsx("btn text-slate-900 bg-gradient-to-r from-white/80 via-white to-white/80 hover:bg-white transition duration-150 ease-in-out group", !isFormEnabled && 'pointer-events-none')}
+            type="submit"
+            disabled={!isFormEnabled}
+          >
+            Subscribe <span className="tracking-normal text-purple-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+          </button>
+        </div>
       </div>
+      {status === 'success' && (
+        <div className="mt-3 max-w-sm">
+          <Alert type='success'>
+            Please check your email, and confirm your subscription.
+          </Alert>
+        </div>
+      )}
+      {status === 'error' && (
+        <div className="mt-3 max-w-sm">
+          <Alert type='error'>
+            {error}
+          </Alert>
+        </div>
+      )}
     </form>
   )
 }
