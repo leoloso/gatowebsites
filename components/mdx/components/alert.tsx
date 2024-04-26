125ad041fd0f373153880eb9ef9cd13e6136059f
leoloso <leo@getpop.org>
2024-04-26 10:22:15 -0300

    bgColor

--- components/mdx/components/alert.tsx
+++ components/mdx/components/alert.tsx
@@ -10,10 +10,10 @@ export default function Alert({ type, ...props }: AlertProps) {
   const alertClassnames = (type: string) => {
     switch (type) {
       case 'success':
-        return 'bg-green-700 border-green-200';
+        return 'bg-green-800 border-green-500 dark:bg-green-900 dark:border-green-600';
       default:
       // case 'error':
-        return 'bg-red-700 border-red-200';
+        return 'bg-red-800 border-red-500 dark:bg-red-900 dark:border-red-600';
     }
   }
 
@@ -23,7 +23,7 @@ export default function Alert({ type, ...props }: AlertProps) {
         return (
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="fill-green-500 stroke-current shrink-0 h-6 w-6 mr-2"
+            className="fill-green-600 stroke-current shrink-0 h-6 w-6 mr-2"
             fill="none"
             viewBox="0 0 24 24"
           >
@@ -40,7 +40,7 @@ export default function Alert({ type, ...props }: AlertProps) {
         return (
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="fill-red-500 stroke-current shrink-0 h-6 w-6 mr-2"
+            className="fill-red-600 stroke-current shrink-0 h-6 w-6 mr-2"
             fill="none"
             viewBox="0 0 24 24"
           >
