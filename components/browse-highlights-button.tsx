652749bb19870cfc6e650ad2b047c86ee410eeac
leoloso <leo@getpop.org>
2024-04-27 16:27:32 -0300

    white bg

--- components/browse-highlights-button.tsx
+++ components/browse-highlights-button.tsx
@@ -3,7 +3,7 @@ import AppConfig from '@/app/app.config'
 
 export default function BrowseHighlightsButton({
   btnClassName = "btn-sm",
-  styleClassname = "text-white hover:text-gray-100 dark:text-slate-300 dark:hover:text-white [background:linear-gradient(theme(colors.slate.500),_theme(colors.slate.500))_padding-box,_conic-gradient(theme(colors.slate.100),_theme(colors.slate.400)_25%,_theme(colors.slate.400)_75%,_theme(colors.slate.100)_100%)_border-box] dark:[background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none'} transition duration-150 ease-in-out w-full group",
+  styleClassname = "text-slate-900 bg-gradient-to-r from-white/80 via-white to-white/80 hover:bg-white transition duration-150 ease-in-out group",
 }: {
   btnClassName?: string
   styleClassname?: string,
