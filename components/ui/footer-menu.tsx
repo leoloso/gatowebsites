950528346d93081bdc351b9f1e3ab346dc51e0e7
leoloso <leo@getpop.org>
2024-05-09 11:30:59 -0300

    added link to YouTube channel

--- components/ui/footer-menu.tsx
+++ components/ui/footer-menu.tsx
@@ -97,6 +97,9 @@ export default function FooterMenu({
           <li>
             <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/specials/wpbuilds">WPBuilds series</a>
           </li>
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="https://www.youtube.com/@GatoGraphQL" target="_blank">@GatoGraphQL YouTube channel</a>
+          </li>
         </ul>
         <h6 className="text-sm text-slate-50 font-medium mb-2 mt-8 lg:mt-6">GitHub</h6>
         <ul className="text-sm space-y-2">
