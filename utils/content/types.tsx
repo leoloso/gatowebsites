7095e00f41ca07c372e332500c897b9a8b0ca702
leoloso <leo@getpop.org>
2024-09-23 15:47:01 +0800

    treat Feature as Article

--- utils/content/types.tsx
+++ utils/content/types.tsx
@@ -1,7 +1,7 @@
 import { DemoPost, Doc, Extension, Feature, BlogPost } from "@/.contentlayer/generated";
 
 export type Post = BlogPost | DemoPost;
-export type Article = Doc | Post;
+export type Article = Doc | Post | Feature;
 export type Artifact = Extension | Feature;
 
 export function isBlogPost(article: Article): article is BlogPost {
@@ -9,6 +9,9 @@ export function isBlogPost(article: Article): article is BlogPost {
 }
 export function isDemoPost(article: Article): article is DemoPost {
   return article.type === 'DemoPost'
+}
+export function isFeature(article: Article): article is Feature {
+  return article.type === 'Feature'
 }
 export function isExtension(artifact: Artifact): artifact is Extension {
   return artifact.type === 'Extension'
