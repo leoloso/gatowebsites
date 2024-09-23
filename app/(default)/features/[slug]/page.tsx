656bba39f29fc3384e2b198d66df2118420e32e6
leoloso <leo@getpop.org>
2024-09-23 18:58:39 +0800

    svg

--- app/(default)/features/[slug]/page.tsx
+++ app/(default)/features/[slug]/page.tsx
@@ -89,8 +89,11 @@ export default async function SingleFeature({ params }: {
             <div className="text-slate-400">{ relatedGuides.length === 1 ? `Related guide:` : `Related guides:` }</div>
             <ul className="mt-1 ml-1">
               {relatedGuides.map((relatedGuide, index) => (
-                <li key={index} className="mb-1">
-                  <a className="text-purple-500 font-medium" href={getDocURLPath(relatedGuide)}>
+                <li key={index} className="mb-2">
+                  <a className="text-purple-500 font-medium flex items-center" href={getDocURLPath(relatedGuide)}>
+                    <svg className="w-3 h-3 fill-slate-400 shrink-0 mr-1 dark:fill-slate-500" xmlns="http://www.w3.org/2000/svg" width="9" height="9">
+                      <path d="m1.285 8.514-.909-.915 5.513-5.523H1.663l.01-1.258h6.389v6.394H6.794l.01-4.226z" />
+                    </svg>
                     <span>{relatedGuide.title}</span>
                   </a>
                 </li>
