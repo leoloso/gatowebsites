16520d1279021a5c0da1940bccd19308c6ec26f8
leoloso <leo@getpop.org>
2024-09-24 15:46:49 +0800

    removed commented code

--- app/(default)/extensions/extensions-section.tsx
+++ app/(default)/extensions/extensions-section.tsx
@@ -20,7 +20,6 @@ export default function ExtensionsSection() {
           leading='Empower and protect your application'
           title='Extensions'
           description='Purchase and install extensions to provide additional functionality to the GraphQL server and expand the GraphQL schema'
-          // headerClassname="md:text-left mx-0"
         />
 
         {/* Extensions */}
@@ -33,11 +32,6 @@ export default function ExtensionsSection() {
                 // data-aos="fade-right" data-aos-delay="200"
               >
                 <div className="absolute inset-0 bg-gray-700 hidden md:block transform md:translate-y-2 md:translate-x-4 xl:translate-y-4 xl:translate-x-8 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out pointer-events-none" aria-hidden="true"></div>
-                {/* {extension.image &&
-                  <figure className="relative h-0 pb-[56.25%] md:pb-[75%] lg:pb-[56.25%] overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out">
-                    <Image className="absolute inset-0 w-full h-full object-cover transform hover:scale-105 transition duration-700 ease-out" src={extension.image} width="540" height="303" alt={extension.title} />
-                  </figure>
-                } */}
                 <div className="relative overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out">
                   <ExtensionThumb
                     extension={extension}
@@ -56,8 +50,6 @@ export default function ExtensionsSection() {
             </article>
           </div>
         ))}
-
-
       </div>
     </div>
   )
