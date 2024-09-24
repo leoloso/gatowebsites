3726f5a4304fc5f25dd88dc21bd1427ca47e2c2c
leoloso <leo@getpop.org>
2024-09-24 14:01:47 +0800

    getDocTopicSVG1

--- components/ui/docs/sidebar.tsx
+++ components/ui/docs/sidebar.tsx
@@ -15,6 +15,63 @@ import {
 import { getDocURLPath } from '@/utils/content/application-urls'
 import AppConstants from '@/app/app.constants'
 
+function getDocTopicSVG1() {
+  return (
+    <svg className="mr-3 shrink-0" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        className="fill-blue-400"
+        d="M19.888 7.804a.88.88 0 0 0-.314-.328l-7.11-4.346a.889.889 0 0 0-.927 0L4.426 7.476a.88.88 0 0 0-.314.328L12 12.624l7.888-4.82Z"
+      />
+      <path
+        className="fill-white dark:fill-slate-800"
+        d="M4.112 7.804a.889.889 0 0 0-.112.43v7.892c0 .31.161.597.426.758l7.11 4.346c.14.085.3.13.464.13v-8.736l-7.888-4.82Z"
+      />
+      <path
+        className="fill-blue-600"
+        d="M19.888 7.804c.073.132.112.28.112.43v7.892c0 .31-.161.597-.426.758l-7.11 4.346c-.14.085-.3.13-.464.13v-8.736l7.888-4.82Z"
+      />
+    </svg>
+  )
+}
+
+function getDocTopicSVG2() {
+  return (
+    <svg className="mr-3 shrink-0" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        className="fill-purple-400"
+        d="M19.888 7.804a.88.88 0 0 0-.314-.328l-7.11-4.346a.889.889 0 0 0-.927 0L4.426 7.476a.88.88 0 0 0-.314.328L12 12.624l7.888-4.82Z"
+      />
+      <path
+        className="fill-white dark:fill-slate-800"
+        d="M4.112 7.804a.889.889 0 0 0-.112.43v7.892c0 .31.161.597.426.758l7.11 4.346c.14.085.3.13.464.13v-8.736l-7.888-4.82Z"
+      />
+      <path
+        className="fill-purple-600"
+        d="M19.888 7.804c.073.132.112.28.112.43v7.892c0 .31-.161.597-.426.758l-7.11 4.346c-.14.085-.3.13-.464.13v-8.736l7.888-4.82Z"
+      />
+    </svg>
+  )
+}
+
+function getDocTopicSVG3() {
+  return (
+    <svg className="mr-3 shrink-0" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path
+        className="fill-sky-400"
+        d="M19.888 7.804a.88.88 0 0 0-.314-.328l-7.11-4.346a.889.889 0 0 0-.927 0L4.426 7.476a.88.88 0 0 0-.314.328L12 12.624l7.888-4.82Z"
+      />
+      <path
+        className="fill-white dark:fill-slate-800"
+        d="M4.112 7.804a.889.889 0 0 0-.112.43v7.892c0 .31.161.597.426.758l7.11 4.346c.14.085.3.13.464.13v-8.736l-7.888-4.82Z"
+      />
+      <path
+        className="fill-sky-600"
+        d="M19.888 7.804c.073.132.112.28.112.43v7.892c0 .31-.161.597-.426.758l-7.11 4.346c-.14.085-.3.13-.464.13v-8.736l7.888-4.82Z"
+      />
+    </svg>
+  )
+}
+
 export default function SupportSidebar() {
   const sidebar = useRef<HTMLDivElement>(null)
   const { sidebarOpen, setSidebarOpen } = useAppProvider()
@@ -119,20 +176,7 @@ export default function SupportSidebar() {
                                     handleClick();
                                   }}
                                 >
-                                  <svg className="mr-3 shrink-0" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                      className="fill-blue-400"
-                                      d="M19.888 7.804a.88.88 0 0 0-.314-.328l-7.11-4.346a.889.889 0 0 0-.927 0L4.426 7.476a.88.88 0 0 0-.314.328L12 12.624l7.888-4.82Z"
-                                    />
-                                    <path
-                                      className="fill-white dark:fill-slate-800"
-                                      d="M4.112 7.804a.889.889 0 0 0-.112.43v7.892c0 .31.161.597.426.758l7.11 4.346c.14.085.3.13.464.13v-8.736l-7.888-4.82Z"
-                                    />
-                                    <path
-                                      className="fill-blue-600"
-                                      d="M19.888 7.804c.073.132.112.28.112.43v7.892c0 .31-.161.597-.426.758l-7.11 4.346c-.14.085-.3.13-.464.13v-8.736l7.888-4.82Z"
-                                    />
-                                  </svg>
+                                  { getDocTopicSVG1() }
                                   <span>{docTopic.name}</span>
                                 </a>
                                 <ul className={`mb-3 ml-4 pl-6 border-l border-slate-200 dark:border-slate-800 ${!open && 'hidden'}`}>
