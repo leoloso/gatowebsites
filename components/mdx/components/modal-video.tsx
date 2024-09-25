e331812ab5d9632483821bd07e8fb18abe67fc18
leoloso <leo@getpop.org>
2024-09-25 17:21:27 +0800

    Extract ModalVideoTransition

--- components/mdx/components/modal-video.tsx
+++ components/mdx/components/modal-video.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { useState, useRef, Fragment } from 'react'
+import { useState } from 'react'
 import type { StaticImageData } from 'next/image'
-import { Dialog, Transition } from '@headlessui/react'
 import Image from 'next/image'
+import ModalVideoTransition from './modal-video-transition'
 
 interface ModalVideoProps {
   thumb: StaticImageData | string
@@ -29,7 +29,6 @@ export default function ModalVideo({
   title,
 }: ModalVideoProps) {
   const [modalOpen, setModalOpen] = useState<boolean>(false)
-  const videoRef = useRef<HTMLVideoElement>(null)
 
   return (
     <div>
@@ -53,45 +52,13 @@ export default function ModalVideo({
       </div>
       {/* End: Video thumbnail */}
 
-      <Transition show={modalOpen} as={Fragment} afterEnter={() => videoRef.current?.play()}>
-        <Dialog initialFocus={videoRef} onClose={() => setModalOpen(false)}>
-
-          {/* Modal backdrop */}
-          <Transition.Child
-            className="fixed inset-0 z-[99999] bg-slate-900 bg-opacity-20 transition-opacity"
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition ease-out duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            aria-hidden="true"
-          />
-          {/* End: Modal backdrop */}
-
-          {/* Modal dialog */}
-          <Transition.Child
-            className="fixed inset-0 z-[99999] overflow-hidden flex items-center justify-center px-4 sm:px-6"
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="ttransition ease-out duration-200"
-            leaveFrom="oopacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <div className="max-w-4xl mx-auto h-full flex items-center">
-              <Dialog.Panel className="w-full max-h-full aspect-video bg-black overflow-hidden">
-                <video ref={videoRef} width={videoWidth} height={videoHeight} controls>
-                  <source src={video} type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
-              </Dialog.Panel>
-            </div>
-          </Transition.Child>
-          {/* End: Modal dialog */}
-
-        </Dialog>
-      </Transition>
+      <ModalVideoTransition
+        video={video}
+        videoWidth={videoWidth}
+        videoHeight={videoHeight}
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+      />
 
     </div>
   )
