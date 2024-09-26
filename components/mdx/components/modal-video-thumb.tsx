a412fdbef1b0fb57d7151cc666279316232c2307
leoloso <leo@getpop.org>
2024-09-26 08:29:16 +0800

    ModalVideoTitle and ModalVideoButton

--- components/mdx/components/modal-video-thumb.tsx
+++ components/mdx/components/modal-video-thumb.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react'
 import type { StaticImageData } from 'next/image'
 import Image from 'next/image'
 import ModalVideoTransition from './modal-video-transition'
+import ModalVideoTitle from './modal-video-title'
+import ModalVideoButton from './modal-video-button'
 
 interface ModalVideoProps {
   thumb: StaticImageData | string
@@ -36,18 +38,8 @@ export default function ThumbModalVideo({
       {/* Video thumbnail */}
       <div className="relative inline-flex justify-center items-center my-2 group hover:cursor-pointer" onClick={() => { setModalOpen(true) }}>
         <Image src={thumb} width={thumbWidth} height={thumbHeight} alt={thumbAlt} />
-        {!! title && (
-          <p className="absolute bottom-0 text-slate-300 mb-4 sm:text-lg sm:mb-12" data-aos="fade-down" data-aos-delay="400">{title}</p>
-        )}
-        <button className="absolute group" aria-label="Watch the video">
-          <svg className="w-16 h-16 fill-current sm:w-20 sm:h-20 group" viewBox="0 0 88 88" xmlns="http://www.w3.org/2000/svg">
-            <circle className="text-white opacity-90 group-hover:opacity-100 transition duration-150 ease-in-out" cx="44" cy="44" r="44" />
-            <path
-              className="text-blue-600"
-              d="M52 44a.999.999 0 00-.427-.82l-10-7A1 1 0 0040 37V51a.999.999 0 001.573.82l10-7A.995.995 0 0052 44V44c0 .001 0 .001 0 0z"
-            />
-          </svg>
-        </button>
+        <ModalVideoTitle title={title} />
+        <ModalVideoButton title={title} />
         {children}
       </div>
       {/* End: Video thumbnail */}
