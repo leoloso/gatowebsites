17f28df0a02c76dd26f0d2cc703fd6ce59adc1ff
leoloso <leo@getpop.org>
2024-09-26 13:43:17 +0800

    blue chevron

--- components/standard/dropdown-full-width.tsx
+++ components/standard/dropdown-full-width.tsx
@@ -24,7 +24,7 @@ export default function FullWidthDropdown({options}: DropdownInterface) {
             <span className="flex items-center">
               <span>{options[selected].value}</span>
             </span>
-            <svg className="shrink-0 ml-1 fill-current text-gray-400 dark:text-gray-500" width="11" height="7" viewBox="0 0 11 7">
+            <svg className="shrink-0 ml-1 fill-current text-blue-400 dark:text-blue-500" width="11" height="7" viewBox="0 0 11 7">
               <path d="M5.4 6.8L0 1.4 1.4 0l4 4 4-4 1.4 1.4z" />
             </svg>
           </MenuButton>
