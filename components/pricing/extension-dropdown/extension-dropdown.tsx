f865e9eaa5185bd0cac65b16ea8477f8e00f945f
leoloso <leo@getpop.org>
2024-09-26 14:25:37 +0800

    pass state optional

--- components/pricing/extension-dropdown/extension-dropdown.tsx
+++ components/pricing/extension-dropdown/extension-dropdown.tsx
@@ -2,16 +2,19 @@
 
 import { Extension } from '@/.contentlayer/generated'
 import FullWidthDropdown from '@/components/standard/dropdown-full-width'
+import { Dispatch, SetStateAction } from 'react'
 
 export default function ExtensionDropdown({
   extensions,
+  state,
 }: {
   extensions: Extension[]
+  state?: [number, Dispatch<SetStateAction<number>>],
 }) {
 
   const extensionNames = extensions.map((extension, index) => extension.title)
   
   return (
-    <FullWidthDropdown values={extensionNames} />
+    <FullWidthDropdown values={extensionNames} state={state} />
   )
 }
