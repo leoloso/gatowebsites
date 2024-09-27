5d007cdf5ffc37f67401122f5c3c03c653be3238
leoloso <leo@getpop.org>
2024-09-27 20:24:16 +0800

    added spacing

--- components/testimonial.tsx
+++ components/testimonial.tsx
@@ -18,26 +18,33 @@ export default function Testimonial({
   const testimonial = testimonials[index]
 
   return (
-    <div className="mx-auto max-w-3xl">
-      <div className="pt-12 md:pt-20">
-        <div className="text-center">
-          <Image
-            className="mb-3 inline-flex rounded-full"
-            src={ testimonial.img }
-            width={40}
-            height={40}
-            alt="Testimonial image"
-          />
-          <p className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-3 font-nacelle text-xl font-semibold text-gray-200 text-transparent">
-            “{ testimonial.quote }”
-          </p>
-          <div className="text-sm font-medium text-gray-200">
-            <span>{ testimonial.name }</span>
-            <span className="text-gray-700"> - </span>
-            <span className="text-indigo-200/65 transition-colors hover:text-indigo-500">{ testimonial.role }</span>
+    <section>
+      <div className="max-w-3xl mx-auto px-4 sm:px-6">
+        <div className="relative px-8 py-12 md:py-20 mb-12 md:mb-20 border-t border-b [border-image:linear-gradient(to_right,transparent,theme(colors.slate.800),transparent)1]">
+          <div className="mx-auto max-w-3xl">
+            <div className="pt-12 md:pt-20">
+              <div className="text-center">
+                <Image
+                  className="mb-3 inline-flex rounded-full"
+                  src={ testimonial.img }
+                  width={40}
+                  height={40}
+                  alt="Testimonial image"
+                />
+                <p className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-3 font-nacelle text-xl font-semibold text-gray-200 text-transparent">
+                  “{ testimonial.quote }”
+                </p>
+                <div className="text-sm font-medium text-gray-200">
+                  <span>{ testimonial.name }</span>
+                  <span className="text-gray-700"> - </span>
+                  <span className="text-indigo-200/65 transition-colors hover:text-indigo-500">{ testimonial.role }</span>
+                </div>
+              </div>
+            </div>
           </div>
+
         </div>
       </div>
-    </div>
+    </section>
   )
 }
