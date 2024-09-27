6c4a43d3dfe5ad2be6076e93e3277d1ee8b687ec
leoloso <leo@getpop.org>
2024-09-27 18:08:14 +0800

    comma

--- app/(default)/extensions/[slug]/single-extension-pricing-section.tsx
+++ app/(default)/extensions/[slug]/single-extension-pricing-section.tsx
@@ -27,7 +27,7 @@ export default function SingleExtensionPricing({
           <h2 className="h2 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-2 md:pb-4">
             Get your extension
           </h2>
-          <p className="text-lg text-slate-400">Purchase the <span className='font-bold'>{ extension.title }</span> extension, or a <span className='font-bold'>bundle with all the extensions</span> with a big discount.</p>
+          <p className="text-lg text-slate-400">Purchase the <span className='font-bold'>{ extension.title }</span> extension, or a <span className='font-bold'>bundle with all the extensions</span>, with a big discount.</p>
         </div>
         <ExtensionDropdownPricing
           fixedExtension={ extension }
