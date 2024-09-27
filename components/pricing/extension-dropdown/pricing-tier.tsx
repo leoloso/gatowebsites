ea55cc29adf5329cf8ac913c7fa53b4bdb263630
leoloso <leo@getpop.org>
2024-09-27 22:00:15 +0800

    Premium support => Support

--- components/pricing/extension-dropdown/pricing-tier.tsx
+++ components/pricing/extension-dropdown/pricing-tier.tsx
@@ -82,7 +82,7 @@ export default function PricingTier({
           >
             <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
           </svg>
-          <span>Premium support</span>
+          <span>Support</span>
         </li>
         <li className="flex items-center">
           <svg
