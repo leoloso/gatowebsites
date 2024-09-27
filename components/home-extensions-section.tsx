847b9d6ef03fdcf33a05b1a600e2ead9381fbfd2
leoloso <leo@getpop.org>
2024-09-27 17:36:26 +0800

    Particles again

--- components/home-extensions-section.tsx
+++ components/home-extensions-section.tsx
@@ -6,6 +6,12 @@ import RadiantGradient from "./radial-gradient";
 export default function HomeExtensionsSection() {
   return (
     <section className="relative">
+
+      {/* Particles animation */}
+      <div className="absolute left-1/2 -translate-x-1/2 top-0 -z-10 w-80 h-80 -mt-24 -ml-32">
+        <Particles className="absolute inset-0 -z-10" quantity={6} staticity={30} />    
+      </div>
+      
       {/* Radial gradient */}
       <RadiantGradient />      
       <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
