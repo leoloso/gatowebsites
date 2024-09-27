3b642c7fdca6ae8373a23c2ed48633cfbacf37e7
leoloso <leo@getpop.org>
2024-09-27 20:25:03 +0800

    no Blurred shape

--- components/testimonial.tsx
+++ components/testimonial.tsx
@@ -23,7 +23,7 @@ export default function Testimonial({
       <div className="max-w-3xl mx-auto px-4 sm:px-6">
         <div className="relative px-8 py-12 md:py-20 mb-12 md:mb-20 border-t border-b [border-image:linear-gradient(to_right,transparent,theme(colors.slate.800),transparent)1]">
           {/* Blurred shape */}
-          <div className="absolute top-0 -mt-24 left-1/2 -translate-x-1/2 ml-24 blur-2xl opacity-70 pointer-events-none -z-10" aria-hidden="true">
+          {/* <div className="absolute top-0 -mt-24 left-1/2 -translate-x-1/2 ml-24 blur-2xl opacity-70 pointer-events-none -z-10" aria-hidden="true">
             <svg xmlns="http://www.w3.org/2000/svg" width="434" height="427">
               <defs>
                 <linearGradient id="bs4-a" x1="19.609%" x2="50%" y1="14.544%" y2="100%">
@@ -33,7 +33,7 @@ export default function Testimonial({
               </defs>
               <path fill="url(#bs4-a)" fillRule="evenodd" d="m0 0 461 369-284 58z" transform="matrix(1 0 0 -1 0 427)"></path>
             </svg>
-          </div>
+          </div> */}
 
           {/* Particles animation */}
           <div className="absolute left-1/2 -translate-x-1/2 top-0 -z-10 w-80 h-80 -mt-6">
