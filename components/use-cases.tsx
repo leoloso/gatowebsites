1507782ebf88ee0a9372cdb6dd41c34b400d09e9
leoloso <leo@getpop.org>
2024-09-28 15:12:57 +0800

    imgAlt

--- components/use-cases.tsx
+++ components/use-cases.tsx
@@ -14,19 +14,19 @@ const tabs = [
     title: "Build headless sites",
     description: "Use your JavaScript framework of choice to render your site.",
     img: CarouselImg01,
-    imgAlt: "Carousel image 01",
-  },
-  {
-    title: "Power your Gutenberg blocks",
-    description: "Simplify coding Gutenberg blocks, without creating REST controllers.",
-    img: CarouselImg02,
-    imgAlt: "Carousel image 02",
+    imgAlt: "Gato GraphQL use case image",
   },
   {
     title: "Consume your data via apps",
     description: "Expose a public GraphQL API to feed data to external apps.",
     img: CarouselImg03,
-    imgAlt: "Carousel image 03",
+    imgAlt: "Gato GraphQL use case image",
+  },
+  {
+    title: "Power your Gutenberg blocks",
+    description: "Simplify coding Gutenberg blocks, without creating REST controllers.",
+    img: CarouselImg02,
+    imgAlt: "Gato GraphQL use case image",
   },
 ];
 
