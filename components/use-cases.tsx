6ff27fcc492ef4f6064540e50114972818e1cb01
leoloso <leo@getpop.org>
2024-09-28 14:34:06 +0800

    more particles

--- components/use-cases.tsx
+++ components/use-cases.tsx
@@ -43,7 +43,7 @@ export default function UseCases() {
 
         {/* Particles animation */}
         <div className="absolute inset-0 max-w-6xl mx-auto px-4 sm:px-6">
-          <Particles className="absolute inset-0 -z-10" quantity={5} />
+          <Particles className="absolute inset-0 -z-10" quantity={10} />
         </div>
 
         <TabGroup selectedIndex={selectedTab} onChange={setSelectedTab} vertical>
