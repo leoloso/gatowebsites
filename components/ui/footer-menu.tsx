0137e7b78692fca2438186cafc5540b3caa81054
leoloso <leo@getpop.org>
2024-09-30 16:37:58 +0800

    moved items around in bottom nav

--- components/ui/footer-menu.tsx
+++ components/ui/footer-menu.tsx
@@ -56,32 +56,7 @@ export default function FooterMenu({
             <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.docs.tutorial}`}>Schema tutorial</a>
           </li>
         </ul>
-        <h6 className="text-sm text-slate-50 font-medium mb-2 mt-8 lg:mt-6">WordPress.org</h6>
-        <ul className="text-sm space-y-2">
-          {/* <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.wpDownload}>Download free plugin</a>
-          </li> */}
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.wpDirectory} target="_blank">Gato GraphQL plugin</a>
-          </li>
-        </ul>
-        <h6 className="text-sm text-slate-50 font-medium mb-2 mt-8 lg:mt-6">Product</h6>
-        <ul className="text-sm space-y-2">
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.instawpSandboxDemo} target="_blank">Try out</a>
-          </li>
-          {/* <li>
-            <a className={`text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out ${getShopAnchorClassname()}`} href={getShopURL(AppConfig.urls.shopProducts.bundles.allExtensions.defaultTier)}>Purchase</a>
-          </li> */}
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/refund-policy">Refund policy</a>
-          </li>
-        </ul>
-      </div>
-
-      {/* 3rd block */}
-      <div className={columnClassname}>
-        <h6 className="text-sm text-slate-50 font-medium mb-2">Comparisons</h6>
+        <h6 className="text-sm text-slate-50 font-medium mb-2 mt-8 lg:mt-6">Comparisons</h6>
         <ul className="text-sm space-y-2">
           <li>
             <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/comparisons">Overview</a>
@@ -91,6 +66,31 @@ export default function FooterMenu({
               <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={getComparisonPostURL(comparisonPost)}>{comparisonPost.title}</a>
             </li>
           ))}
+        </ul>
+      </div>
+
+      {/* 3rd block */}
+      <div className={columnClassname}>
+        <h6 className="text-sm text-slate-50 font-medium mb-2">GitHub</h6>
+        <ul className="text-sm space-y-2">
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.githubRepo} target="_blank">Gato GraphQL open source code</a>
+          </li>
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.githubExtensionStarter} target="_blank">Gato GraphQL extension starter</a>
+          </li>
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.nextjsWordPressStarter} target="_blank">Next.js + Gato GraphQL WordPress starter</a>
+          </li>
+        </ul>
+        <h6 className="text-sm text-slate-50 font-medium mb-2 mt-8 lg:mt-6">WordPress.org</h6>
+        <ul className="text-sm space-y-2">
+          {/* <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.wpDownload}>Download free plugin</a>
+          </li> */}
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.wpDirectory} target="_blank">Gato GraphQL plugin</a>
+          </li>
         </ul>
         <h6 className="text-sm text-slate-50 font-medium mb-2 mt-8 lg:mt-6">Resources</h6>
         <ul className="text-sm space-y-2">
@@ -102,18 +102,6 @@ export default function FooterMenu({
           </li>
           <li>
             <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="https://www.youtube.com/@GatoGraphQL" target="_blank">@GatoGraphQL YouTube channel</a>
-          </li>
-        </ul>
-        <h6 className="text-sm text-slate-50 font-medium mb-2 mt-8 lg:mt-6">GitHub</h6>
-        <ul className="text-sm space-y-2">
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.githubRepo} target="_blank">Gato GraphQL open source code</a>
-          </li>
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.githubExtensionStarter} target="_blank">Gato GraphQL extension starter</a>
-          </li>
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.nextjsWordPressStarter} target="_blank">Next.js + Gato GraphQL WordPress starter</a>
           </li>
         </ul>
       </div>
@@ -149,7 +137,19 @@ export default function FooterMenu({
 
       {/* 5th block */}
       <div className={columnClassname}>
-        <h6 className="text-sm text-slate-50 font-medium mb-2">Support</h6>
+        <h6 className="text-sm text-slate-50 font-medium mb-2">Product</h6>
+        <ul className="text-sm space-y-2">
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.instawpSandboxDemo} target="_blank">Try out</a>
+          </li>
+          {/* <li>
+            <a className={`text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out ${getShopAnchorClassname()}`} href={getShopURL(AppConfig.urls.shopProducts.bundles.allExtensions.defaultTier)}>Purchase</a>
+          </li> */}
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/refund-policy">Refund policy</a>
+          </li>
+        </ul>
+        <h6 className="text-sm text-slate-50 font-medium mb-2 mt-8 lg:mt-6">Support</h6>
         <ul className="text-sm space-y-2">
           <li>
             <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/shop/my-orders">My orders</a>
