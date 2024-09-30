46b956dcd2471d32ec1eba859667645707322fea
leoloso <leo@getpop.org>
2024-09-30 12:10:41 +0800

    bg a bit

--- components/use-cases/slide-1.tsx
+++ components/use-cases/slide-1.tsx
@@ -12,7 +12,7 @@ export default function UseCasesSlide1() {
       className={clsx(
         "flex-wrap gap-7 justify-center items-center flex max-sm:flex-col",
         "[&_h3]:h3 [&_h3]:pb-2 [&_h3]:text-xl [&_h3]:text-center [&_h3]:bg-clip-text [&_h3]:text-transparent [&_h3]:bg-gradient-to-r [&_h3]:from-slate-200/60 [&_h3]:via-slate-200 [&_h3]:to-slate-200/60",
-        "[&_pre]:!bg-transparent [&_pre]:ring-0 [&_pre_span]:text-[--shiki-dark]",
+        "[&_pre]:!bg-slate-300/10 [&_pre]:ring-0 [&_pre_span]:text-[--shiki-dark]",
         // "[&_h3]:font-extralight",
         "[&_code]:whitespace-pre-wrap" /* fix scroll on mobile for code-blocks */,
       )}
