678cc892b247bd08e2966647669916f26d923672
leoloso <leo@getpop.org>
2024-09-30 11:02:26 +0800

    logos in Slide2

--- components/use-cases/slide-2.tsx
+++ components/use-cases/slide-2.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import Logo01 from "@/public/images/logo-01.svg";
-import Logo02 from "@/public/images/logo-02.svg";
-import Logo03 from "@/public/images/logo-03.svg";
-import Logo04 from "@/public/images/logo-04.svg";
-import Logo05 from "@/public/images/logo-05.svg";
-import Logo06 from "@/public/images/logo-06.svg";
-import Logo07 from "@/public/images/logo-07.svg";
-import Logo08 from "@/public/images/logo-08.svg";
-import Logo09 from "@/public/images/logo-09.svg";
+import Logo01 from "@/public/assets/libraries/react.svg";
+import Logo02 from "@/public/assets/libraries/nextjs.svg";
+import Logo03 from "@/public/assets/libraries/vue.svg";
+import Logo04 from "@/public/assets/libraries/astro.svg";
+import Logo05 from "@/public/assets/libraries/nuxt.svg";
+import Logo06 from "@/public/assets/libraries/svelte.svg";
+import Logo07 from "@/public/assets/libraries/vitepress.svg";
+import Logo08 from "@/public/assets/libraries/hugo.svg";
+import Logo09 from "@/public/assets/external-logos/graphql-logo.svg"
 
 export default function UseCasesSlide2() {
   return (
@@ -186,7 +186,7 @@ export default function UseCasesSlide2() {
           </div>
         </div>
 
-        <div className="relative flex flex-col" ref={tabsRef}>
+        <div className="relative flex flex-col">
           <div className="absolute -translate-x-[136px]">
             <div className="animate-[breath_7s_ease-in-out_3s_infinite_both]">
               <div className="flex h-16 w-16 items-center justify-center rounded-full bg-white shadow-lg shadow-black/[0.03] before:absolute before:inset-0 before:m-[8.334%] before:rounded-[inherit] before:border before:border-gray-700/5 before:bg-gray-200/60 before:[mask-image:linear-gradient(to_bottom,black,transparent)]">
