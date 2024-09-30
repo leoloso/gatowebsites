0be61aaacdd12e1f0a73d798c800eaddfeb792ba
leoloso <leo@getpop.org>
2024-09-30 11:35:28 +0800

    better color

--- components/use-cases/slide-2.tsx
+++ components/use-cases/slide-2.tsx
@@ -162,7 +162,7 @@ export default function UseCasesSlide2() {
         {/* Horizontal lines */}
         {/* <div className="absolute inset-x-0 top-0 -z-10 h-px bg-gradient-to-r from-transparent via-gray-200 to-transparent mix-blend-multiply"></div>
         <div className="absolute inset-x-0 bottom-0 -z-10 h-px bg-gradient-to-r from-transparent via-gray-200 to-transparent mix-blend-multiply"></div> */}
-        <div className="absolute inset-x-[200px] top-1/2 -z-10 h-px bg-gradient-to-r from-transparent via-purple-500/60 to-transparent mix-blend-multiply"></div>
+        <div className="absolute inset-x-[200px] top-1/2 -z-10 h-px bg-gradient-to-r from-transparent via-purple-300/60 to-transparent mix-blend-multiply"></div>
         {/* <div className="absolute inset-x-0 top-1/2 -z-10 h-px -translate-y-[82px] bg-gradient-to-r from-transparent via-gray-200 to-transparent mix-blend-multiply before:absolute before:inset-y-0 before:w-24 before:animate-[line_10s_ease-in-out_infinite_both] before:bg-gradient-to-r before:via-purple-500"></div>
         <div className="absolute inset-x-0 top-1/2 -z-10 h-px translate-y-[82px] bg-gradient-to-r from-transparent via-gray-200 to-transparent mix-blend-multiply before:absolute before:inset-y-0 before:w-24 before:animate-[line_10s_ease-in-out_infinite_5s_both] before:bg-gradient-to-r before:via-purple-500"></div> */}
         {/* Diagonal lines */}
