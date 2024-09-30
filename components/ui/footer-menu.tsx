e39c8b9c671923e2504a5ed50b93e566e0c09cf6
leoloso <leo@getpop.org>
2024-09-30 16:28:30 +0800

    moved Blog

--- components/ui/footer-menu.tsx
+++ components/ui/footer-menu.tsx
@@ -49,9 +49,6 @@ export default function FooterMenu({
           <li>
             <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.docs.tutorial}`}>Schema tutorial</a>
           </li>
-          <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.blog}`}>Blog</a>
-          </li>
           {/* <li>
             <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/changelog">Changelog</a>
           </li> */}
@@ -122,6 +119,9 @@ export default function FooterMenu({
       <div className={columnClassname}>
         <h6 className="text-sm text-slate-50 font-medium mb-2">Company</h6>
         <ul className="text-sm space-y-2">
+          <li>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.blog}`}>Blog</a>
+          </li>
           <li>
             <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/about">About us</a>
           </li>
