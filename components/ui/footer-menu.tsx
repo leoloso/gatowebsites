d25330bbd9429e92542fe3f9deefda02ebc88e23
leoloso <leo@getpop.org>
2024-09-30 16:29:30 +0800

    "Gato GraphQL open source code"

--- components/ui/footer-menu.tsx
+++ components/ui/footer-menu.tsx
@@ -104,7 +104,7 @@ export default function FooterMenu({
         <h6 className="text-sm text-slate-50 font-medium mb-2 mt-8 lg:mt-6">GitHub</h6>
         <ul className="text-sm space-y-2">
           <li>
-            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.githubRepo} target="_blank">Gato GraphQL repo</a>
+            <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.githubRepo} target="_blank">Gato GraphQL open source code</a>
           </li>
           <li>
             <a className="text-slate-500 dark:text-slate-400 hover:text-slate-800 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.githubExtensionStarter} target="_blank">Gato GraphQL extension starter</a>
