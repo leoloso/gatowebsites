802003ab2953bcb51f21cf36a07dab4aa79b2798
leoloso <leo@getpop.org>
2024-10-01 10:33:27 +0800

    There's no Extension.featured

--- utils/content/extension.tsx
+++ utils/content/extension.tsx
@@ -1,12 +1,12 @@
-import { Extension, allExtensions } from 'contentlayer/generated'
+// import { Extension, allExtensions } from 'contentlayer/generated'
 
-export function getFeaturedExtensions(extensions: Array<Extension> | undefined = allExtensions) {
-  return extensions.filter((extension) => extension.featured)
-}
+// export function getFeaturedExtensions(extensions: Array<Extension> | undefined = allExtensions) {
+//   return extensions.filter((extension) => extension.featured)
+// }
 
-export function getExtensionCategories(extensions: Array<Extension> | undefined = allExtensions) {
-  return extensions
-    .map((extension) => extension.category)
-    // filter distinct values
-    .filter((value, index, array) => array.indexOf(value) === index)
-}
+// export function getExtensionCategories(extensions: Array<Extension> | undefined = allExtensions) {
+//   return extensions
+//     .map((extension) => extension.category)
+//     // filter distinct values
+//     .filter((value, index, array) => array.indexOf(value) === index)
+// }
