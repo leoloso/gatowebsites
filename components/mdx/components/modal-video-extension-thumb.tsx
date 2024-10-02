59a0b40a5bcc0ba70e14f49ddfb6733c8cfb0f6e
leoloso <leo@getpop.org>
2024-10-02 17:06:26 +0800

    simpler markup

--- components/mdx/components/modal-video-extension-thumb.tsx
+++ components/mdx/components/modal-video-extension-thumb.tsx
@@ -32,18 +32,16 @@ export default function ExtensionThumbModalVideo({
     <>
 
       {/* Video thumbnail */}
-      <div className=''>
-        <div className="w-full h-full aspect-video object-cover relative flex justify-center items-center my-2 group hover:cursor-pointer" onClick={() => { setModalOpen(true) }}>
-          <ExtensionThumb
-            extension={extension}
-            isLandscape={true}
-            printExtensionTitle={printExtensionTitle}
-            // bgClassname="bg-purple-900 group-hover:bg-blue-900 transition duration-700 ease-out"
-          />
-          <ModalVideoTitle title={title} extraClassname='z-30' />
-          <ModalVideoButton title={title} extraClassname='z-30 group-hover:scale-110 transition duration-700 ease-out' />
-          {children}
-        </div>
+      <div className="w-full h-full aspect-video object-cover relative flex justify-center items-center my-2 group hover:cursor-pointer" onClick={() => { setModalOpen(true) }}>
+        <ExtensionThumb
+          extension={extension}
+          isLandscape={true}
+          printExtensionTitle={printExtensionTitle}
+          // bgClassname="bg-purple-900 group-hover:bg-blue-900 transition duration-700 ease-out"
+        />
+        <ModalVideoTitle title={title} extraClassname='z-30' />
+        <ModalVideoButton title={title} extraClassname='z-30 group-hover:scale-110 transition duration-700 ease-out' />
+        {children}
       </div>
       {/* End: Video thumbnail */}
 
