139f1db732007bda346c538184c4bf69d2416845
leoloso <leo@getpop.org>
2024-10-02 14:50:45 +0800

    subitems under year

--- components/pricing/extension-dropdown/pricing-tier.tsx
+++ components/pricing/extension-dropdown/pricing-tier.tsx
@@ -74,36 +74,40 @@ export default function PricingTier({
           </svg>
           <span>{tierDomainNumber} domains</span>
         </li>
-        <li className="flex items-center">
-          <svg
-            className="mr-2 h-3 w-3 shrink-0 fill-current text-purple-500"
-            viewBox="0 0 12 12"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-          </svg>
-          <span>Support</span>
+        <li className="flex flex-col grow space-y-2 text-sm text-slate-400">
+          <div className="flex items-center">
+            <svg
+              className="mr-2 h-3 w-3 shrink-0 fill-current text-purple-500"
+              viewBox="0 0 12 12"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+            </svg>
+            <span>1 year</span>:
+          </div>
+          <ul className="ml-4 grow space-y-2 text-sm text-slate-400">
+            <li className="flex items-center">
+              <svg
+                className="mr-2 h-3 w-3 shrink-0 fill-current text-purple-500"
+                viewBox="0 0 12 12"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+              </svg>
+              <span>Support</span>
+            </li>
+            <li className="flex items-center">
+              <svg
+                className="mr-2 h-3 w-3 shrink-0 fill-current text-purple-500"
+                viewBox="0 0 12 12"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+              </svg>
+              <span>Product updates</span>
+            </li>
+          </ul>
         </li>
-        <li className="flex items-center">
-          <svg
-            className="mr-2 h-3 w-3 shrink-0 fill-current text-purple-500"
-            viewBox="0 0 12 12"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-          </svg>
-          <span>Product updates</span>
-        </li>
-        {/* <li className="flex items-center">
-          <svg
-            className="mr-2 h-3 w-3 shrink-0 fill-current text-purple-500"
-            viewBox="0 0 12 12"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-          </svg>
-          <span>Renewable every year</span>
-        </li> */}
       </ul>
     </div>
   )
