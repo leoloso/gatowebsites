4647452ad4f7a882b0dd3858590164d3bea468f3
leoloso <leo@getpop.org>
2024-10-02 13:52:28 +0800

    use purple

--- components/pricing/extension-dropdown/pricing.tsx
+++ components/pricing/extension-dropdown/pricing.tsx
@@ -114,7 +114,7 @@ export default function ExtensionDropdownPricing({
               <label className="flex items-center cursor-pointer">
                 <input type="checkbox" className="form-checkbox cursor-pointer" />
                 <span className="text-sm ml-2">
-                  <span className={clsx("text-center", selectBundle && "text-cyan-300")} aria-hidden="true">
+                  <span className={clsx("text-center", selectBundle && "text-purple-300")} aria-hidden="true">
                     Make it a Life Time Deal!
                   </span>
                 </span>
