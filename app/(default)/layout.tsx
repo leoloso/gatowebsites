29e3587348d684ed1c5eb194b6b59d568550e2d4
leoloso <leo@getpop.org>
2024-11-06 16:40:49 +0800

    place 4 banners

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -8,6 +8,10 @@ import 'aos/dist/aos.css'
 import Footer from '@/components/ui/footer'
 import Header from '@/components/ui/header'
 import AppSettings from '@/app/app.settings'
+import BlackFridayBanner1 from '@/components/ui/campaigns/black-friday-banner-1'
+import BlackFridayBanner2 from '@/components/ui/campaigns/black-friday-banner-2'
+import BlackFridayBanner3 from '@/components/ui/campaigns/black-friday-banner-3'
+import BlackFridayBanner4 from '@/components/ui/campaigns/black-friday-banner-4'
 
 export default function DefaultLayout({
   children,
@@ -27,6 +31,11 @@ export default function DefaultLayout({
   return (
     <div className={`flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip ${AppSettings.enableLightDarkThemeMode ? 'dark' : '' }`}>
       <Header />
+      
+      <BlackFridayBanner1 />
+      <BlackFridayBanner2 />
+      <BlackFridayBanner3 />
+      <BlackFridayBanner4 />
       
       <main className="grow">
 
