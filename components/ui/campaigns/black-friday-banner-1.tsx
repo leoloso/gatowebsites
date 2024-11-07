72d62289eaf2677fcd9bfd77fd713852877ad15c
leoloso <leo@getpop.org>
2024-11-07 10:32:02 +0800

    use svg

--- components/ui/campaigns/black-friday-banner-1.tsx
+++ components/ui/campaigns/black-friday-banner-1.tsx
@@ -1,4 +1,5 @@
-
+import BlackFridayImage from '@/public/assets/icons/black-friday/048-flash-sale-3.svg'
+import Image from 'next/image'
 
 export default function BlackFridayBanner1() {
   return (
@@ -8,10 +9,15 @@ export default function BlackFridayBanner1() {
                 <div role="alert" className="relative block w-full text-base font-regular px-4 py-4 rounded-lg bg-gradient-to-tr from-slate-900 to-violet-900 text-white flex">
                     <div className="">
                         <p className="font-bold text-red-500">
-                            🌟 Black Friday Sale
-                            <span className="text-gray-300"> - </span><span className="text-red-300">50% off any product!</span>
-                            <span className="ml-4 text-yellow-300"> Use discount code: <span className="text-white">BF2025</span></span>
-                            <span className="ml-4 text-red-300 font-normal text-sm">Until Nov 29th</span>
+                            <span className="flex items-center justify-center">
+                                <Image src={BlackFridayImage} width={24} height={24} alt="label" className='mr-1' />
+                                <span>
+                                    <span>Black Friday Sale</span>
+                                    <span className="text-gray-300"> - </span><span className="text-red-300">50% off any product!</span>
+                                    <span className="ml-4 text-yellow-300"> Use discount code: <span className="text-white">BF2025</span></span>
+                                    <span className="ml-4 text-red-300 font-normal text-sm">Until Nov 29th</span>
+                                </span>
+                            </span>
                         </p>
                     </div>
                     {/* <button className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
