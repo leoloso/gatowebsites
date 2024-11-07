e53659610d56e9041cc978c6cf4f5e4e07cd7ffb
leoloso <leo@getpop.org>
2024-11-07 12:34:38 +0800

    marginTopClassname

--- components/ui/campaigns/campaign-banner.tsx
+++ components/ui/campaigns/campaign-banner.tsx
@@ -1,10 +1,15 @@
 import AppSettings from '@/app/app.settings'
 import BlackFridayBanner from '@/components/ui/campaigns/black-friday-banner'
+import clsx from 'clsx'
 
-export default function CampaignBanner() {
+export default function CampaignBanner({
+  marginTopClassname = "-mt-16",
+}: {
+  marginTopClassname?: string,
+}) {
 
   return (
-    <div className="mb-16 -mt-16">
+    <div className={clsx("mb-16", marginTopClassname)}>
       { AppSettings.campaigns.enableBlackFriday && (
           <BlackFridayBanner />
       )}
