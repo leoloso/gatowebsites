bae3c7b5c17202305822b1c3e4ad14bb00ef21a1
leoloso <leo@getpop.org>
2024-11-07 10:10:06 +0800

    no button

--- components/ui/campaigns/black-friday-banner-1.tsx
+++ components/ui/campaigns/black-friday-banner-1.tsx
@@ -14,11 +14,11 @@ export default function BlackFridayBanner1() {
                         </span>
                         </p>
                     </div>
-                    <button className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
+                    {/* <button className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
                         <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
                         </span>
-                    </button>
+                    </button> */}
                 </div>
             </div>
         </div>
