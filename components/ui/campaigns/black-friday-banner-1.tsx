c336a78a5858c52375a79230d9ec2421c2392565
leoloso <leo@getpop.org>
2024-11-07 10:13:31 +0800

    text

--- components/ui/campaigns/black-friday-banner-1.tsx
+++ components/ui/campaigns/black-friday-banner-1.tsx
@@ -7,11 +7,11 @@ export default function BlackFridayBanner1() {
             <div className="grid w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
                 <div role="alert" className="relative block w-full text-base font-regular px-4 py-4 rounded-lg bg-gray-900 text-white flex">
                     <div className="">
-                        <p className="font-bold text-red-500">Black Friday Sale
-                        <span className="text-gray-300"> - </span><span className="text-red-300">50% off any product!</span>
-                        <span title="" className="inline-flex items-center justify-center text-sm font-bold text-red-500 transition-all ml-4 duration-200 rounded-md hover:text-red-700" role="button">
-                            Get Now<svg className="w-4 h-4 ml-1" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"></path><path d="M12 5l7 7-7 7"></path></svg>
-                        </span>
+                        <p className="font-bold text-red-500">
+                            🌟 Black Friday Sale
+                            <span className="text-gray-300"> - </span><span className="text-red-300">50% off any product!</span>
+                            <span className="ml-4 text-yellow-300"> Use discount code: <span className="text-white">BF2025</span></span>
+                            <span className="ml-4 text-red-300 font-normal text-sm">Until Nov 29th</span>
                         </p>
                     </div>
                     {/* <button className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
