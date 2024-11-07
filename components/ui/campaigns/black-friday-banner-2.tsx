f976c8da9be02879ca6481e141829ae694be1627
leoloso <leo@getpop.org>
2024-11-07 10:30:52 +0800

    use svg

--- components/ui/campaigns/black-friday-banner-2.tsx
+++ components/ui/campaigns/black-friday-banner-2.tsx
@@ -1,17 +1,23 @@
-
+import BlackFridayImage from '@/public/assets/icons/black-friday/048-flash-sale-3.svg'
+import Image from 'next/image'
 
 export default function BlackFridayBanner2() {
   return (
     <div className="flex items-center justify-center">
         <div>
             <div className="grid w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
-                <div role="alert" className="relative block w-full text-base font-regular px-4 py-4 rounded-lg bg-gradient-to-tr from-orange-500 to-red-500 text-white flex">
+                <div className="relative block w-full text-base font-regular px-4 py-4 rounded-lg bg-gradient-to-tr from-orange-500 to-red-500 text-white flex">
                     <div className="">
                         <p className="font-bold text-black">
-                            🌟 Black Friday Sale
-                            <span className="text-gray-300"> - </span><span className="text-white">50% off any product!</span>
-                            <span className="ml-4 text-yellow-300"> Use discount code: <span className="text-white">BF2025</span></span>
-                            <span className="ml-4 text-white font-normal text-sm">Until Nov 29th</span>
+                            <span className="flex items-center justify-center">
+                                <Image src={BlackFridayImage} width={24} height={24} alt="label" className='mr-1' />
+                                <span>
+                                    <span>Black Friday Sale</span>
+                                    <span className="text-gray-300"> - </span><span className="text-white">50% off any product!</span>
+                                    <span className="ml-4 text-yellow-300"> Use discount code: <span className="text-white">BF2025</span></span>
+                                    <span className="ml-4 text-white font-normal text-sm">Until Nov 29th</span>
+                                </span>
+                            </span>
                         </p>
                     </div>
                     {/* <button className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
