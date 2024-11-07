073211e64f1b40dccb0737ff4235fddf0ebfe698
leoloso <leo@getpop.org>
2024-11-07 10:38:39 +0800

    color

--- components/ui/campaigns/black-friday-banner-1.tsx
+++ components/ui/campaigns/black-friday-banner-1.tsx
@@ -13,9 +13,9 @@ export default function BlackFridayBanner1() {
                                 <Image src={BlackFridayImage} width={24} height={24} alt="label" className='mr-1' />
                                 <span>
                                     <span>Black Friday Sale</span>
-                                    <span className="text-gray-300"> - </span><span className="text-red-200">50% off any product!</span>
-                                    <span className="ml-4 text-yellow-300"> Use discount code: <span className="text-white">BF2025</span></span>
-                                    <span className="ml-4 text-red-200 font-normal text-sm">Until Nov 29th</span>
+                                    <span className="text-gray-300"> - </span><span className="text-white">50% off any product!</span>
+                                    <span className="ml-4 text-yellow-500"> Use discount code: <span className="text-yellow-300">BF2025</span></span>
+                                    <span className="ml-4 text-gray-300 font-normal text-sm">Until Nov 29th</span>
                                 </span>
                             </span>
                         </p>
