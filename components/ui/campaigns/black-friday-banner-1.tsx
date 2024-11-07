1c16b34a90e3ea6ffa5839f59f35a448d0b1f7bd
leoloso <leo@getpop.org>
2024-11-07 10:56:46 +0800

    indigo-600

--- components/ui/campaigns/black-friday-banner-1.tsx
+++ components/ui/campaigns/black-friday-banner-1.tsx
@@ -6,7 +6,7 @@ export default function BlackFridayBanner1() {
     <div className="flex items-center justify-center">
         <div>
             <div className="grid w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
-                <div role="alert" className="relative block w-full text-base font-regular px-4 py-4 rounded-lg bg-gradient-to-tr from-violet-900 to-indigo-900 text-white flex">
+                <div role="alert" className="relative block w-full text-base font-regular px-4 py-4 rounded-lg bg-gradient-to-tr from-violet-900 to-indigo-600 text-white flex">
                     <div className="">
                         <p className="font-bold text-red-500">
                             <span className="flex items-center justify-center">
