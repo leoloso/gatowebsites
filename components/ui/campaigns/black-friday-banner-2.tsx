cc40f430a2df16232528fceff93d7b59685f460d
leoloso <leo@getpop.org>
2024-11-07 10:20:16 +0800

    orange to red

--- components/ui/campaigns/black-friday-banner-2.tsx
+++ components/ui/campaigns/black-friday-banner-2.tsx
@@ -5,7 +5,7 @@ export default function BlackFridayBanner2() {
     <div className="flex items-center justify-center">
         <div>
             <div className="grid w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
-                <div role="alert" className="relative block w-full text-base font-regular px-4 py-4 rounded-lg bg-red-500 text-white flex">
+                <div role="alert" className="relative block w-full text-base font-regular px-4 py-4 rounded-lg bg-gradient-to-tr from-orange-500 to-red-500 text-white flex">
                     <div className="">
                         <p className="font-bold text-black">
                             🌟 Black Friday Sale
