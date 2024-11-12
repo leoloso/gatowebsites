48bd763e27ef9fe06225d8feba3adce925af5093
leoloso <leo@getpop.org>
2024-11-12 09:41:27 +0800

    fixed <pre><code> out of bounds in <Banner> In https://gatographql.com/tutorial/retrieving-data-from-an-external-api

--- components/mdx/components/banner.tsx
+++ components/mdx/components/banner.tsx
@@ -9,25 +9,25 @@ export default function PostBanner({ type, ...props }: PostBannerProps) {
     switch (type) {
       case 'important':
         return (
-          <svg className="fill-purple-500 shrink-0 mr-4" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+          <svg className="fill-purple-500 shrink-0 mr-4" width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
             <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8Zm0 12c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1Zm1-3H7V4h2v5Z" />
           </svg>
         );
       case 'info':
         return (
-          <svg className="fill-blue-500 shrink-0 mr-4" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+          <svg className="fill-blue-500 shrink-0 mr-4" width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
             <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8Zm0 12c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1Zm1-3H7V4h2v5Z" />
           </svg>
         );
       case 'success':
         return (
-          <svg className="fill-teal-500 shrink-0 mr-4" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+          <svg className="fill-teal-500 shrink-0 mr-4" width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
             <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8ZM7 11.4 3.6 8 5 6.6l2 2 4-4L12.4 6 7 11.4Z" />
           </svg>
         );
       default:
         return (
-          <svg className="fill-purple-500 shrink-0 mr-4" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+          <svg className="fill-purple-500 shrink-0 mr-4" width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
             <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8ZM7 11.4 3.6 8 5 6.6l2 2 4-4L12.4 6 7 11.4Z" />
           </svg>
         )
@@ -36,11 +36,11 @@ export default function PostBanner({ type, ...props }: PostBannerProps) {
 
   return (
     <div className="text-sm p-4 bg-slate-50 border border-slate-200 rounded dark:bg-zinc-800 dark:border-zinc-700">
-      <div className="flex items-center">
+      <div className="absolute -ml-7 -mt-7 items-center">
         {typeIcon(type)}
-        <div>
-          {props.children}
-        </div>
+      </div>
+      <div>
+        {props.children}
       </div>
     </div>
   )
