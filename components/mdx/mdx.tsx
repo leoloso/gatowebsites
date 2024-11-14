79850f2539cb36e341a1ec75ba3bc3b56e3610c5
leoloso <leo@getpop.org>
2024-11-14 14:13:01 +0800

    fix import

--- components/mdx/mdx.tsx
+++ components/mdx/mdx.tsx
@@ -1,4 +1,4 @@
-import { useMDXComponent } from 'next-contentlayer/hooks'
+import { useMDXComponent } from 'next-contentlayer2/hooks'
 import PostLink from './components/link'
 import PostImage from './components/image'
 import PostBanner from './components/banner'
