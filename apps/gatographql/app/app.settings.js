6ef1eea34e0b206787200a078a7e5d47ddfe7245
leoloso <leo@getpop.org>
2024-11-15 18:54:46 +0800

    AppSettings in shared app

--- apps/gatographql/app/app.settings.js
+++ apps/gatographql/app/app.settings.js
@@ -1,13 +1,6 @@
 module.exports = {
-  campaigns: {
-    enableBlackFriday: true,
-  },
-  useCDNForMovies: true,
-  enableLightDarkThemeMode: false,
-  useLemonSqueezyOverlay: true,
   searchDemoPosts: false,
   postsPerPage: {
     demos: 12,
-    blog: 12
   }
 }
