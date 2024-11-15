64f9e8fedd76a3cd5fbbcfdbe1d1fdf84a85a6f4
leoloso <leo@getpop.org>
2024-11-15 16:37:29 +0800

    format

--- packages/shared/components/pricing/product-dropdown/pricing-tier.tsx
+++ packages/shared/components/pricing/product-dropdown/pricing-tier.tsx
@@ -99,8 +99,7 @@ export default function PricingTier({
           >
             <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
           </svg>
-          <span>Product 
-            updates
+          <span>Product updates
             { isLTD && (
               <span className="text-purple-400"> (forever)</span>
             )}
