42afcf57271d0cf43a7a94038f4d012b39b97874
leoloso <leo@getpop.org>
2024-11-15 16:41:51 +0800

    no default value for ALGOLIA_INDEX_NAME

--- apps/gatographql/data/env/algolia.tsx
+++ apps/gatographql/data/env/algolia.tsx
@@ -1,5 +1,5 @@
 export const ALGOLIA_API_CREDENTIALS = {
-  indexName: process.env.ALGOLIA_INDEX_NAME || 'website',
+  indexName: process.env.ALGOLIA_INDEX_NAME || '',
   appId: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || '',
   searchAPIKey: process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || '',
   searchAdminKey: process.env.ALGOLIA_SEARCH_ADMIN_KEY || ''
