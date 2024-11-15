64c0d334244e908715275410213997c380470dce
leoloso <leo@getpop.org>
2024-11-15 19:19:29 +0800

    Use "Logo"

--- packages/shared/components/ui/docs/footer-logo.tsx
+++ packages/shared/components/ui/docs/footer-logo.tsx
@@ -4,8 +4,8 @@ import LogoImg from '@gato/public/assets/Gato-logo-suki-rectangular.png'
 
 export default function FooterLogo() {
   return (
-    <Link className="inline-flex mb-2 md:mb-0" href="/" aria-label="Gato GraphQL logo">
-      <Image src={LogoImg} width={100} height={70} alt="Gato GraphQL logo" />
+    <Link className="inline-flex mb-2 md:mb-0" href="/" aria-label="Logo">
+      <Image src={LogoImg} width={100} height={70} alt="Logo" />
     </Link>
   )
 }
