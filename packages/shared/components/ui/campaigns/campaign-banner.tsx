ec1f191e4e0e05447f3756f58a742d3e9d67798c
leoloso <leo@getpop.org>
2024-11-15 19:15:40 +0800

    CampaignBannerInterface

--- packages/shared/components/ui/campaigns/campaign-banner.tsx
+++ packages/shared/components/ui/campaigns/campaign-banner.tsx
@@ -1,17 +1,29 @@
 import AppSettings from '@gato/app/app.settings'
 import BlackFridayBanner from '@gato/components/ui/campaigns/black-friday-banner'
+import {BlackFridayBannerInterface} from '@gato/components/ui/campaigns/black-friday-banner'
 import clsx from 'clsx'
+
+export interface CampaignBannerInterface extends BlackFridayBannerInterface {
+  marginTopClassname?: string,
+}
 
 export default function CampaignBanner({
   marginTopClassname = "-mt-16",
-}: {
-  marginTopClassname?: string,
-}) {
+  applyStyle,
+  dealLabel,
+  discountCode,
+  endDate,
+}: CampaignBannerInterface) {
 
   return (
     <div className={clsx("mb-16", marginTopClassname)}>
       { AppSettings.campaigns.enableBlackFriday && (
-        <BlackFridayBanner />
+        <BlackFridayBanner
+          applyStyle={applyStyle}
+          dealLabel={dealLabel}
+          discountCode={discountCode}
+          endDate={endDate}
+        />
       )}
     </div>
   )
