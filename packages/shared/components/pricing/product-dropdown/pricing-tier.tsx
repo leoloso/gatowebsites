7c3721d815326139ffd28f8f357f974ab7533ba7
leoloso <leo@getpop.org>
2024-11-15 16:39:16 +0800

    removed commented code

--- packages/shared/components/pricing/product-dropdown/pricing-tier.tsx
+++ packages/shared/components/pricing/product-dropdown/pricing-tier.tsx
@@ -105,16 +105,6 @@ export default function PricingTier({
             )}
           </span>
         </li>
-        {/* <li className="flex items-center">
-          <svg
-            className="mr-2 h-3 w-3 shrink-0 fill-current text-purple-500"
-            viewBox="0 0 12 12"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-          </svg>
-          <span>Renewable every year</span>
-        </li> */}
       </ul>
     </div>
   )
