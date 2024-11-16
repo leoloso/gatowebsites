b3aa0732b5255d4c929cba2698c98b146dee613b
leoloso <leo@getpop.org>
2024-11-16 15:37:15 +0800

    remove NEXT_PUBLIC_GATOAPP_ENABLE_LIGHT_DARK_THEME_MODE

--- packages/shared/app/app.settings.js
+++ packages/shared/app/app.settings.js
@@ -3,7 +3,7 @@ module.exports = {
     enableBlackFriday: process.env.NEXT_PUBLIC_GATOAPP_ENABLE_BLACK_FRIDAY ? process.env.NEXT_PUBLIC_GATOAPP_ENABLE_BLACK_FRIDAY === "true" : false,
   },
   useCDNForMovies: true,
-  enableLightDarkThemeMode: process.env.NEXT_PUBLIC_GATOAPP_ENABLE_LIGHT_DARK_THEME_MODE ? process.env.NEXT_PUBLIC_GATOAPP_ENABLE_LIGHT_DARK_THEME_MODE === "true" : false,
+  enableLightDarkThemeMode: false,
   useLemonSqueezyOverlay: true,
   postsPerPage: {
     blog: 12
