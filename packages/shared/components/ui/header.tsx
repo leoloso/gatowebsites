8f10c559f8723c01fbbf26ae1495690a55613a11
leoloso <leo@getpop.org>
2024-11-16 19:49:20 +0800

    HeaderMenu

--- packages/shared/components/ui/header.tsx
+++ packages/shared/components/ui/header.tsx
@@ -3,9 +3,7 @@ import MobileMenu from './mobile-menu'
 import PurchasePROPluginButton from '../purchase-pro-plugin-button'
 import Search from './search'
 import ThemeToggle from '@gato/components/ui/theme-toggle'
-import Link from 'next/link'
-import Dropdown from '@gato/components/utils/dropdown'
-import AppConfig from '@/app/app.config'
+import HeaderMenu from './header-menu'
 import AppSettings from '@gato/app/app.settings'
 import TryPROPluginButton from '../try-pro-plugin-button'
 // import DownloadFreePluginButton from '../download-free-button'
@@ -31,39 +29,7 @@ export default function Header({
 
           <nav className="hidden md:flex md:grow">
             {/* Desktop menu links */}
-            <ul className="flex grow justify-center flex-wrap items-center">
-              <li>
-                <Link className="font-medium text-sm text-slate-500 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white mx-5 lg:mx-6 transition duration-150 ease-in-out" href="/pricing">Pricing</Link>
-              </li>
-              {/* <li>
-                <Link className="font-medium text-sm text-slate-500 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white mx-5 lg:mx-6 transition duration-150 ease-in-out" href={`/${AppConfig.paths.features}`}>Features</Link>
-              </li> */}
-              <li>
-                <Link className="font-medium text-sm text-slate-500 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white mx-5 lg:mx-6 transition duration-150 ease-in-out" href={`/${AppConfig.paths.extensions}`}>Extensions</Link>
-              </li>
-              {/* <li>
-                <Link className="font-medium text-sm text-slate-500 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white mx-5 lg:mx-6 transition duration-150 ease-in-out" href={`/${AppConfig.paths.demoPosts}`}>Demos</Link>
-              </li> */}
-              {/* 1st level: hover */}
-              <Dropdown title="Documentation">
-                {/* 2nd level: hover */}
-                <li>
-                  <Link className="flex font-medium text-sm text-slate-500 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white mx-5 lg:mx-6 transition duration-150 ease-in-out py-0.5" href={`/${AppConfig.paths.docs.guides}`}>Guides</Link>
-                </li>
-                <li>
-                  <Link className="flex font-medium text-sm text-slate-500 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white mx-5 lg:mx-6 transition duration-150 ease-in-out py-0.5" href={`/${AppConfig.paths.docs.extensionsReference}`}>Extensions reference</Link>
-                </li>
-                <li>
-                  <Link className="flex font-medium text-sm text-slate-500 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white mx-5 lg:mx-6 transition duration-150 ease-in-out py-0.5" href={`/${AppConfig.paths.docs.queryLibrary}`}>Queries library</Link>
-                </li>
-                <li>
-                  <Link className="flex font-medium text-sm text-slate-500 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white mx-5 lg:mx-6 transition duration-150 ease-in-out py-0.5" href={`/${AppConfig.paths.docs.tutorial}`}>Schema tutorial</Link>
-                </li>
-              </Dropdown>
-              {/* <li>
-                <Link className="font-medium text-sm text-slate-500 hover:text-slate-800 dark:text-slate-300 dark:hover:text-white mx-5 lg:mx-6 transition duration-150 ease-in-out" href={`/${AppConfig.paths.blog}`}>Blog</Link>
-              </li> */}
-            </ul>
+            <HeaderMenu />
           </nav>
 
           <ul className="hidden md:flex md:grow flex-1 flex justify-end items-center">
