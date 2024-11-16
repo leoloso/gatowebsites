4483e4d253de0b72de2ccca8aa5f335412da149c
leoloso <leo@getpop.org>
2024-11-16 20:20:45 +0800

    repeat functions to avoid bug with redirecting guides

--- apps/gatographql/utils/content/document.tsx
+++ apps/gatographql/utils/content/document.tsx
@@ -1,41 +1,54 @@
-import { RelatedGuide } from "@/.contentlayer/generated";
-import { allDocs } from '@/.contentlayer/generated'
+import { Doc, DocTopic, RelatedGuide } from "@/.contentlayer/generated";
+import { allDocs, allDocTopics } from '@/.contentlayer/generated'
 import AppConfig from '@/app/app.config'
 import { Article } from "./types";
+import { sortByOrder, sortByOrderAndTitle } from "@gato/utils/content/sort";
 
 /**
- * All commented out because transferred to the shared package
- * @todo Remove all this commented code, at some point!
+ * Watch out! These methods are repeated:
+ *
+ * @see packages/shared/utils/content/document.tsx
+ * 
+ * That's because, for some reason, `sortDocuments` fails
+ * at sorting the Doc and DocTopic, and then calling /guides will
+ * redirect to /guides/augment/dynamic-variables instead of
+ * /guides/intro/intro-to-graphql-and-gato-graphql
+ * 
+ * ----------------------------------------------------------------
  */
-// export function getDocTopic(doc: Doc) {
-//   const docTopic = allDocTopics.find((docTopic) => doc.section === docTopic.section && docTopic.slug === doc.topicSlug);
-//   if (!docTopic) {
-//     throw new Error(`There is no DocTopic with section '${doc.section}' and slug '${doc.topicSlug}'`)
-//   }
-//   return docTopic
-// }
+export function getDocTopic(doc: Doc) {
+  const docTopic = allDocTopics.find((docTopic) => doc.section === docTopic.section && docTopic.slug === doc.topicSlug);
+  if (!docTopic) {
+    throw new Error(`There is no DocTopic with section '${doc.section}' and slug '${doc.topicSlug}'`)
+  }
+  return docTopic
+}
 
-// export function getDocumentsByTopic(docTopic: DocTopic) {
-//   return allDocs.filter((doc) => doc.section === docTopic.section && doc.topicSlug === docTopic.slug)
-// }
+export function getDocumentsByTopic(docTopic: DocTopic) {
+  return allDocs.filter((doc) => doc.section === docTopic.section && doc.topicSlug === docTopic.slug)
+}
 
-// export function sortDocumentTopics(a: DocTopic, b: DocTopic) {
-//   return sortByOrder(a, b)
-// }
+export function sortDocumentTopics(a: DocTopic, b: DocTopic) {
+  return sortByOrder(a, b)
+}
 
-// // Make sure that all documents respect the order
-// // of their topics (to find the next/prev items for pagination)
-// export function sortDocuments(a: Doc, b: Doc) {
-//   if (a.topicSlug === b.topicSlug) {
-//     return sortByOrderAndTitle(a, b)
-//   }
+// Make sure that all documents respect the order
+// of their topics (to find the next/prev items for pagination)
+export function sortDocuments(a: Doc, b: Doc) {
+  if (a.topicSlug === b.topicSlug) {
+    return sortByOrderAndTitle(a, b)
+  }
 
-//   return sortDocumentTopics(getDocTopic(a), getDocTopic(b));
-// }
+  return sortDocumentTopics(getDocTopic(a), getDocTopic(b));
+}
 
-// export function getDocumentTopicsBySection(section: string) {
-//   return allDocTopics.filter((docTopic) => docTopic.section === section)
-// }
+export function getDocumentTopicsBySection(section: string) {
+  return allDocTopics.filter((docTopic) => docTopic.section === section)
+}
+/**
+ * ----------------------------------------------------------------
+ */
+
 
 function getGroupDocuments(docSection: string) {
   return allDocs.filter((doc) => doc.section === docSection)
