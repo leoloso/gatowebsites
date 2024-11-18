2ad4fc7b7546f894e5f9e634b35f8c05021ef505
leoloso <leo@getpop.org>
2024-11-18 16:59:52 +0800

    removed isExtension

--- apps/gatographql/utils/content/application-urls.tsx
+++ apps/gatographql/utils/content/application-urls.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/.contentlayer/generated";
 import AppConfig from '@/app/app.config'
 import AppConstants from "gatoapp/app/app.constants";
-import { Article, isDemoPost, isExtension, isBlogPost, isFeature } from "./types";
+import { Article, isDemoPost, isBlogPost, isFeature } from "./types";
 import { DOMAIN } from 'gatoapp/data/env/domain'
 
 export function getExtensionURLPath(extension: Extension) {
