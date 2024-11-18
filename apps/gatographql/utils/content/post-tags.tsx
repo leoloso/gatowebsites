240a9deb1f0dad9cd5f381fc543ef8497b9d3d45
leoloso <leo@getpop.org>
2024-11-18 17:07:24 +0800

    commented out code

--- apps/gatographql/utils/content/post-tags.tsx
+++ apps/gatographql/utils/content/post-tags.tsx
@@ -1,13 +1,18 @@
-import { allBlogPosts, allDemoPosts } from '@/.contentlayer/generated'
-import { getPostTags, getAllPostTagColors } from 'gatoapp/utils/content/post-tags'
+/**
+ * Commented out as all code is currently unused.
+ * Uncomment if providing another "Post" type that is
+ * not part of the shared package.
+ */
+// import { allBlogPosts, allDemoPosts } from '@/.contentlayer/generated'
+// import { getPostTags, getAllPostTagColors } from 'gatoapp/utils/content/post-tags'
 
-export function getAllPostTags(): Array<string> {
-  return [
-    ...getPostTags(allBlogPosts.map( (post) => ({ tags: post.tags || [] }))),
-    ...getPostTags(allDemoPosts.map( (post) => ({ tags: post.tags || [] })))
-  ]
-}
+// export function getAllPostTags(): Array<string> {
+//   return [
+//     ...getPostTags(allBlogPosts.map( (post) => ({ tags: post.tags || [] }))),
+//     ...getPostTags(allDemoPosts.map( (post) => ({ tags: post.tags || [] })))
+//   ]
+// }
 
-export function getPostTagColors(colors: Array<string>) {
-  return getAllPostTagColors(colors, getAllPostTags())
-}
+// export function getPostTagColors(colors: Array<string>) {
+//   return getAllPostTagColors(colors, getAllPostTags())
+// }
