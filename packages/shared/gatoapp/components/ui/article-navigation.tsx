9879eead2f1abbd883b8a91233a4fa628488c67e
leoloso <leo@getpop.org>
2024-11-18 17:38:06 +0800

    Link to urlPath

--- packages/shared/gatoapp/components/ui/article-navigation.tsx
+++ packages/shared/gatoapp/components/ui/article-navigation.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link'
 import { getArticleURLPath } from '@/utils/content/application-urls'
-import { Article } from '@/utils/content/types'
+
+type Article = {
+  title: string,
+  urlPath: string,
+}
 
 export default function ArticleNavigation({
   prevArticle,
@@ -17,7 +21,7 @@ export default function ArticleNavigation({
           <div>
             <div className="text-xs font-[650] text-blue-600 uppercase mb-1">Prev</div>
             <div>
-              <Link className="text-slate-800 font-[650] flex items-center dark:text-slate-200" href={getArticleURLPath(prevArticle)}>
+              <Link className="text-slate-800 font-[650] flex items-center dark:text-slate-200" href={prevArticle.urlPath}>
                 <svg className="fill-slate-400 shrink-0 mr-2 rotate-180 dark:fill-slate-500" width="8" height="10" xmlns="http://www.w3.org/2000/svg">
                   <path d="M1 2 2.414.586 6.828 5 2.414 9.414 1 8l3-3z" />
                 </svg>
@@ -33,7 +37,7 @@ export default function ArticleNavigation({
           <div>
             <div className="text-xs font-[650] text-blue-600 uppercase mb-1">Next</div>
             <div>
-              <Link className="text-slate-800 font-[650] flex items-center dark:text-slate-200" href={getArticleURLPath(nextArticle)}>
+              <Link className="text-slate-800 font-[650] flex items-center dark:text-slate-200" href={nextArticle.urlPath}>
                 <span>{nextArticle.title}</span>
                 <svg className="fill-slate-400 shrink-0 ml-2 dark:fill-slate-500" width="8" height="10" xmlns="http://www.w3.org/2000/svg">
                   <path d="M1 2 2.414.586 6.828 5 2.414 9.414 1 8l3-3z" />
