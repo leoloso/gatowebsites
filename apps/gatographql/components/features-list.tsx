5f2a0376a14c53248f1d68147a8cf59c7d26b87c
leoloso <leo@getpop.org>
2024-11-18 16:04:15 +0800

    remove stale import

--- apps/gatographql/components/features-list.tsx
+++ apps/gatographql/components/features-list.tsx
@@ -2,7 +2,6 @@ import { sortByOrderAndTitle } from 'gatoapp/utils/content/sort';
 import ArtifactsList from 'gatoapp/components/artifacts-list';
 import { allFeatures } from '@/.contentlayer/generated'
 import DefaultArtifactIcon02 from '@/public/assets/theme/default/artifact-icon-02.png'
-import DefaultArtifactIcon04 from '@/public/assets/theme/default/artifact-icon-04.png'
 
 export default function FeaturesList() {
   const features = allFeatures.sort(sortByOrderAndTitle)
