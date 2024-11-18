c5502f7e19fa85f31ad978d9dbb10d7e7e0b790b
leoloso <leo@getpop.org>
2024-11-19 05:13:12 +0800

    removed enableLightDarkThemeMode

--- packages/shared/gatoapp/components/ui/search-modal.tsx
+++ packages/shared/gatoapp/components/ui/search-modal.tsx
@@ -479,7 +479,7 @@ export default function SearchModal({
         />
         <TransitionChild
           as="div"
-          className={`${AppSettings.enableLightDarkThemeMode && enableLightDarkThemeModeToggle ? '' : 'dark' } fixed inset-0 z-50 overflow-hidden flex items-start top-20 mb-4 justify-center px-4 sm:px-6`}
+          className={`${enableLightDarkThemeModeToggle ? '' : 'dark' } fixed inset-0 z-50 overflow-hidden flex items-start top-20 mb-4 justify-center px-4 sm:px-6`}
           enter="transition ease-in-out duration-200"
           enterFrom="opacity-0 translate-y-4"
           enterTo="opacity-100 translate-y-0"
