de221beebceca2e548aa24a1e208b0dead509bb5
leoloso <leo@getpop.org>
2024-11-18 14:25:36 +0800

    adapted commented

--- packages/shared/components/post-item-integration.tsx
+++ packages/shared/components/post-item-integration.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-// import { DemoPostIntegration } from '@/.contentlayer/generated'
+// import { PostIntegration } from '@gato/types/types'
 
 export default function PostItemIntegration({
   ...postIntegration
