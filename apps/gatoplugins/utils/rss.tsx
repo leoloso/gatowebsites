5346ea39c4216ccfcde305528c794ea913744672
leoloso <leo@getpop.org>
2024-11-19 08:53:30 +0800

    added files

---
+++ apps/gatoplugins/utils/rss.tsx
@@ -0,0 +1,7 @@
+import { allBlogPosts } from '@/.contentlayer/generated'
+import generateRssFeed from 'gatoapp/utils/rss';
+
+export default async function generateAppRssFeed() {
+
+  return generateRssFeed('Gato Plugins', allBlogPosts)
+}
