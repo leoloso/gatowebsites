4ff4dd60b322a4418d010b156dff8f38e8a76515
leoloso <leo@getpop.org>
2024-11-19 15:21:55 +0800

    Migrated layout to shared

--- apps/gatographql/app/(nobackground)/layout.tsx
+++ apps/gatographql/app/(nobackground)/layout.tsx
@@ -1,36 +1,3 @@
-'use client'
+import DefaultLayout from 'gatoapp/app/(nobackground)/layout'
 
-import { useEffect } from 'react'
-
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-
-import AppSettings from 'gatoapp/app/app.settings'
-
-export default function DefaultLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {  
-
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      disable: 'phone',
-      duration: 1000,
-      easing: 'ease-out-cubic',
-    })
-  })
-
-  return (
-    <div className={`flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip`}>
-      
-      <main className="grow">
-
-        {children}
-
-      </main>
-
-    </div>
-  )
-}
+export default DefaultLayout
