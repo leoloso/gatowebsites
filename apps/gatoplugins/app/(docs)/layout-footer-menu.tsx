ecfcb44e962dec4b8ec8168b706cd016df12d669
leoloso <leo@getpop.org>
2024-11-20 15:31:30 +0800

    added layout

---
+++ apps/gatoplugins/app/(docs)/layout-footer-menu.tsx
@@ -0,0 +1,7 @@
+import FooterMenu from '@/components/menu/footer-menu'
+
+export default function LayoutFooterMenu() {  
+  return (
+    <FooterMenu columnClassname="col-span-4 lg:col-span-2" />
+  );
+}
