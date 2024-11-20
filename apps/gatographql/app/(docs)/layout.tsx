4cbae5efe73b8c4ea4523629a4b77946611bdb31
leoloso <leo@getpop.org>
2024-11-20 15:11:14 +0800

    use shared

--- apps/gatographql/app/(docs)/layout.tsx
+++ apps/gatographql/app/(docs)/layout.tsx
@@ -1,10 +1,5 @@
-import 'gatoapp/app/(docs)/css/style.css'
-
-import { Nothing_You_Could_Do } from 'next/font/google'
 import AppProvider from 'gatoapp/app/(docs)/app-provider'
-import Sidebar from 'gatoapp/components/ui/docs/sidebar'
-import Header from 'gatoapp/components/ui/header'
-import StunningBackground from 'gatoapp/components/stunning-background'
+import RootLayout from 'gatoapp/app/(docs)/layout'
 import { createSEOPageTitle } from '@/utils/content/metadata'
 import LayoutFooterMenu from './layout-footer-menu'
 import HeaderMenu from '@/components/ui/header-menu'
@@ -12,20 +7,13 @@ import HeaderMobileMenu from '@/components/ui/header-mobile-menu'
 import AppComponentProvider from 'gatoapp/app/appcomponent-provider'
 import LogoImg from '@/public/assets/GatoGraphQL-logo-suki-text-rectangular.png'
 
-const nycd = Nothing_You_Could_Do({
-  subsets: ['latin'],
-  variable: '--font-nycd',
-  weight: '400',
-  display: 'swap'
-})
-
 const pageTitle = 'Documentation'
 export const metadata = {
   title: createSEOPageTitle(pageTitle),
   description: 'Guides, tutorials, GraphQL queries, and reference docs, to learn how to use Gato GraphQL',
 }
 
-export default function RootLayout({
+export default function AppRootLayout({
   children,
 }: {
   children: React.ReactNode
@@ -42,36 +30,9 @@ export default function RootLayout({
           mobileMenu: <HeaderMobileMenu />,
         }}
       >
-        <div className={`${nycd.variable} text-slate-800 font-[350] bg-white dark:bg-transparent dark:text-slate-200`}>
-          <div className="flex flex-col min-h-screen overflow-hidden">
-
-            <Header />
-
-            {/*  Page content */}
-            <main className="grow">
-              <section className="relative">
-                
-                <StunningBackground />
-
-                <div className="max-w-7xl mx-auto px-4 sm:px-6">
-                  {/* Main content */}
-                  <div>
-                    {/* Sidebar */}
-                    <Sidebar />
-
-                    {/* Page container */}
-                    <div className="md:grow md:pl-64 lg:pr-6 xl:pr-0">
-                      <div className="pt-24 md:pt-28 pb-8 md:pl-6 lg:pl-12">
-                        {children}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </section>
-            </main>
-
-          </div>
-        </div>
+        <RootLayout>
+          {children}
+        </RootLayout>
       </AppComponentProvider>
     </AppProvider>
   )
