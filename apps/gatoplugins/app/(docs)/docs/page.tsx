60ab97f1fb4ea0ae04f45e17a62d5c21abe918f1
leoloso <leo@getpop.org>
2024-11-20 16:15:20 +0800

    added docs/page

---
+++ apps/gatoplugins/app/(docs)/docs/page.tsx
@@ -0,0 +1,17 @@
+import { notFound, redirect } from 'next/navigation'
+import { getGuideDocuments, sortDocuments } from '@/utils/content/document'
+
+// @todo Do this page!
+
+// Redirect to the first item on the docs collection
+export default function RedirectToFirstDocItem() {
+
+  // Sort docs (this already takes into account the DocTopic)
+  const docs = getGuideDocuments().sort(sortDocuments);
+
+  if (docs.length === 0) notFound()
+
+  // Redirect to the first one
+  const doc = docs[0];
+  redirect(doc.urlPath)
+}
