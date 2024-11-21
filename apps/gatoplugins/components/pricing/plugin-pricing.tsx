fd0784891e219262d441bb3689871b9900313660
leoloso <leo@getpop.org>
2024-11-22 04:20:11 +0800

    pass specific plugin

--- apps/gatoplugins/components/pricing/plugin-pricing.tsx
+++ apps/gatoplugins/components/pricing/plugin-pricing.tsx
@@ -1,24 +1,15 @@
 "use client";
 
 import { getShopURL, getShopAnchorClassname } from "gatoapp/utils/shop/shop"
-import { allPlugins, Plugin } from "@/.contentlayer/generated"
-import { sortByOrderAndTitle } from 'gatoapp/utils/content/sort'
-
-import { useState } from "react";
+import { Plugin } from "@/.contentlayer/generated"
 import PricingTier from "gatoapp/components/pricing/product-dropdown/pricing-tier";
 import AppConfig from "@/app/app.config"
 
 export default function PluginDropdownPricing({
-  fixedPlugin,
+  plugin,
 }: {
-  fixedPlugin?: Plugin,
+  plugin: Plugin,
 }) {
-  const plugins = allPlugins.sort(sortByOrderAndTitle)
-
-  const [selectedPluginIndex, setSelectedPluginIndex] = useState<number>(fixedPlugin === undefined ? 0 : plugins.findIndex((ext) => ext.slug === fixedPlugin.slug))
-
-  const selectedPlugin = plugins[selectedPluginIndex]
-
   const isProd = process.env.NODE_ENV === 'production'
 
   return (
@@ -27,11 +18,11 @@ export default function PluginDropdownPricing({
         {/* Pricing table 1 */}
         <PricingTier
           tierName='Personal'
-          productName={ selectedPlugin.title }
-          price={ selectedPlugin.prices.tier1.yearly }
+          productName={ plugin.title }
+          price={ plugin.prices.tier1.yearly }
           originalPrice={  undefined }
           tierDomainNumber={ AppConfig.shop.licenseDomainNumber.tier1 }
-          buttonURL={ getShopURL(isProd ? selectedPlugin.shopURLs.tier1.yearly : selectedPlugin.shopURLs.dev) }
+          buttonURL={ getShopURL(isProd ? plugin.shopURLs.tier1.yearly : plugin.shopURLs.dev) }
           buttonClassname={ getShopAnchorClassname() }
           productNameClassname={ "text-blue-300" }
           isLTD={ false }
@@ -39,11 +30,11 @@ export default function PluginDropdownPricing({
         {/* Pricing table 2 */}
         <PricingTier
           tierName='Organization'
-          productName={ selectedPlugin.title }
-          price={ selectedPlugin.prices.tier2.yearly }
+          productName={ plugin.title }
+          price={ plugin.prices.tier2.yearly }
           originalPrice={  undefined }
           tierDomainNumber={ AppConfig.shop.licenseDomainNumber.tier2 }
-          buttonURL={ getShopURL(isProd ? selectedPlugin.shopURLs.tier2.yearly : selectedPlugin.shopURLs.dev) }
+          buttonURL={ getShopURL(isProd ? plugin.shopURLs.tier2.yearly : plugin.shopURLs.dev) }
           buttonClassname={ getShopAnchorClassname() }
           productNameClassname={ "text-blue-300" }
           isLTD={ false }
@@ -51,11 +42,11 @@ export default function PluginDropdownPricing({
         {/* Pricing table 3 */}
         <PricingTier
           tierName='Professional'
-          productName={ selectedPlugin.title }
-          price={ selectedPlugin.prices.tier3.yearly }
+          productName={ plugin.title }
+          price={ plugin.prices.tier3.yearly }
           originalPrice={  undefined }
           tierDomainNumber={ AppConfig.shop.licenseDomainNumber.tier3 }
-          buttonURL={ getShopURL(isProd ? selectedPlugin.shopURLs.tier3.yearly : selectedPlugin.shopURLs.dev) }
+          buttonURL={ getShopURL(isProd ? plugin.shopURLs.tier3.yearly : plugin.shopURLs.dev) }
           buttonClassname={ getShopAnchorClassname() }
           productNameClassname={ "text-blue-300" }
           isLTD={ false }
@@ -64,11 +55,11 @@ export default function PluginDropdownPricing({
         {/* Pricing table 4 */}
         <PricingTier
           tierName='Agency'
-          productName={ selectedPlugin.title }
-          price={ selectedPlugin.prices.tier4.yearly }
+          productName={ plugin.title }
+          price={ plugin.prices.tier4.yearly }
           originalPrice={  undefined }
           tierDomainNumber={ AppConfig.shop.licenseDomainNumber.tier4 }
-          buttonURL={ getShopURL(isProd ? selectedPlugin.shopURLs.tier4.yearly : selectedPlugin.shopURLs.dev) }
+          buttonURL={ getShopURL(isProd ? plugin.shopURLs.tier4.yearly : plugin.shopURLs.dev) }
           buttonClassname={ getShopAnchorClassname() }
           productNameClassname={ "text-blue-300" }
           isLTD={ false }
