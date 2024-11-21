526f5074d3cebf37fc2bfc1bbdb6fcb824a4420d
leoloso <leo@getpop.org>
2024-11-21 18:15:21 +0800

    added plugin-backgrounds

---
+++ apps/gatoplugins/utils/content/plugin-backgrounds.tsx
@@ -0,0 +1,43 @@
+import { allPlugins } from '@/.contentlayer/generated'
+import { sortByOrderAndTitle } from 'gatoapp/utils/content/sort'
+import BgImage01 from '@/public/assets/theme/customer-bg-01.png'
+import BgImage02 from '@/public/assets/theme/customer-bg-02.png'
+import BgImage03 from '@/public/assets/theme/customer-bg-03.png'
+import BgImage04 from '@/public/assets/theme/customer-bg-04.png'
+import BgImage05 from '@/public/assets/theme/customer-bg-05.png'
+import BgImage06 from '@/public/assets/theme/customer-bg-06.png'
+import BgImage07 from '@/public/assets/theme/customer-bg-07.png'
+import BgImage08 from '@/public/assets/theme/customer-bg-08.png'
+import BgImage09 from '@/public/assets/theme/customer-bg-09.png'
+import BgImage10 from '@/public/assets/theme/customer-bg-10.png'
+import { StaticImageData } from 'next/image'
+
+const backgrounds = [
+  BgImage01,
+  BgImage02,
+  BgImage03,
+  BgImage04,
+  BgImage05,
+  BgImage06,
+  BgImage07,
+  BgImage08,
+  BgImage09,
+  BgImage10,
+]
+
+export function getPluginBackgrounds() {
+  const pluginNames = allPlugins.sort(sortByOrderAndTitle).map((plugin) => plugin.title)
+  let pluginNameBackgrounds : { [key: string]: StaticImageData } = {}
+  pluginNames.forEach(function (pluginName: string, index: number) {
+    pluginNameBackgrounds[pluginName] = backgrounds[index % backgrounds.length]
+  })
+  return pluginNameBackgrounds
+}
+
+export function getPluginBackground(pluginName: string) {
+  const pluginNameBackgrounds = getPluginBackgrounds()
+  if (!pluginNameBackgrounds[pluginName]) {
+    throw new Error(`There is no plugin with name '${pluginName}'`)
+  }
+  return pluginNameBackgrounds[pluginName]
+}
