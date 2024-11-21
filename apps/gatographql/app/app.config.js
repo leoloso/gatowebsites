2806bcb57f5e524fd32b0413f9c3296baf5b94fc
leoloso <leo@getpop.org>
2024-11-21 16:17:36 +0800

    removed docs path from config

--- apps/gatographql/app/app.config.js
+++ apps/gatographql/app/app.config.js
@@ -10,7 +10,6 @@ module.exports = {
     blog: "blog",
     demoPosts: "demos",
     features: "features",
-    docs: "docs",
     // ----------------------------------------
 
     highlights: "highlights",
