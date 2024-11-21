542004ec383ce4250c22cc754f3a9696d9d1f6c8
leoloso <leo@getpop.org>
2024-11-21 18:40:15 +0800

    added layout to Gato Plugins

---
+++ apps/gatoplugins/app/(default)/layout.tsx
@@ -0,0 +1,35 @@
+import AppComponentProvider from 'gatoapp/app/appcomponent-provider'
+import LogoImg from '@/public/assets/GatoPlugins-logo-suki-text-rectangular.webp'
+import FooterMenu from '@/components/menu/footer-menu'
+import FooterMenuFirstColumn from 'gatoapp/components/menus/footer-menu-1st-column'
+import HeaderMenu from '@/components/menu/header-menu'
+import HeaderMobileMenu from '@/components/menu/header-mobile-menu'
+import DefaultLayout from 'gatoapp/app/(default)/layout'
+
+export default function AppDefaultLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {  
+
+  return (
+    <AppComponentProvider
+      footer={{
+        menu: (
+          <FooterMenu>
+            <FooterMenuFirstColumn />
+          </FooterMenu>
+        ),
+      }}
+      header={{
+        logoImage: LogoImg,
+        menu: <HeaderMenu />,
+        mobileMenu: <HeaderMobileMenu />,
+      }}
+    >
+      <DefaultLayout>
+        {children}
+      </DefaultLayout>
+    </AppComponentProvider>
+  )
+}
