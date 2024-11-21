03bf447c0d92fc5ea363a620c8f47071c7c50eb8
leoloso <leo@getpop.org>
2024-11-22 04:55:07 +0800

    about page

---
+++ apps/gatoplugins/app/(default)/about/page.tsx
@@ -0,0 +1,36 @@
+import Hero from 'gatoapp/components/hero-about'
+import Story from 'gatoapp/components/story'
+import Cta from 'gatoapp/components/cta-03'
+import AboutPageSchemaJsonLdScript from 'gatoapp/components/schema/aboutpage-schema-json-ld'
+import { createSEOPageTitle } from '@/utils/content/metadata'
+import WordPressLogo from '@/public/assets/external-logos/wordpress-logo.svg'
+import Logo from '@/public/assets/GatoPlugins-logo-suki-text-square.webp'
+
+const pageTitle = 'About us'
+export const metadata = {
+  title: createSEOPageTitle(pageTitle),
+  description: 'Meet the Gato Plugins team, and learn about the history of Gato Plugins',
+}
+
+export default function About() {
+  return (
+    <>
+      <AboutPageSchemaJsonLdScript
+        headline={metadata.title}
+        description={metadata.description}
+      />
+
+      <Hero
+        title="Building a collection of super useful plugins for WordPress"
+        includeAnimation={false}
+        includeThumbnail={true}
+        targetImageSources={[WordPressLogo]}
+        logoImage={Logo}
+      />
+
+      <Story includeHeader={true} />
+      
+      <Cta />
+    </>
+  )
+}
