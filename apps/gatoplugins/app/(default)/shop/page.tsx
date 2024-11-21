21fa3124921ef583dfaf9066d8ff185f0cfd240e
leoloso <leo@getpop.org>
2024-11-22 05:17:45 +0800

    shop redirect

---
+++ apps/gatoplugins/app/(default)/shop/page.tsx
@@ -0,0 +1,12 @@
+import { redirect } from "next/navigation"
+import { createSEOPageTitle } from '@/utils/content/metadata'
+
+const pageTitle = 'Shop'
+export const metadata = {
+  title: createSEOPageTitle(pageTitle),
+  description: 'Purchase a product from Gato Plugins, and access all your orders from our shop',
+}
+
+export default function Shop() {
+  redirect('/shop/my-orders')
+}
