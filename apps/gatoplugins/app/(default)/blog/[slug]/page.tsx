cc99167c224f41a7e9f44aafbf85ca6e231fd5bc
leoloso <leo@getpop.org>
2024-11-22 05:57:54 +0800

    added blog

---
+++ apps/gatoplugins/app/(default)/blog/[slug]/page.tsx
@@ -0,0 +1,71 @@
+import type { Metadata, ResolvingMetadata } from 'next'
+import {
+  allBlogPosts,
+} from '@/.contentlayer/generated'
+import { notFound } from 'next/navigation'
+import BlogSinglePost from 'gatoapp/components/blog/blog-single-post'
+import Newsletter from 'gatoapp/components/newsletter'
+import { sortByPublishedAt } from 'gatoapp/utils/content/sort'
+import generateAppRssFeed from '@/utils/rss'
+import { createSEOPageTitle, createOpenGraphPageTitle } from '@/utils/content/metadata'
+
+export async function generateStaticParams() {
+  // Generate the RSS feed
+  await generateAppRssFeed();
+
+  return allBlogPosts.map((blogPost) => ({
+    slug: blogPost.slug,
+  }))
+}
+
+export async function generateMetadata(
+  { params }: { params: { slug: string } },
+  parent: ResolvingMetadata,
+): Promise<Metadata | undefined> {
+
+  const blogPost = allBlogPosts.find((blogPosts) => blogPosts.slug === params.slug)
+
+  if (!blogPost) return
+
+  const { title, seoTitle, description, seoDescription } = blogPost
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: createSEOPageTitle(title, seoTitle),
+    description: seoDescription || description,
+    openGraph: {
+      title: createOpenGraphPageTitle(title),
+      description,
+      images: blogPost.image ? [blogPost.image] : previousImages,
+    },
+    twitter: {
+      title: createOpenGraphPageTitle(title),
+      description,
+      images: blogPost.image ? [blogPost.image] : previousImages,
+    },
+  }
+}
+
+export default async function SinglePost({ params }: {
+  params: { slug: string }
+}) {
+
+  // Sort posts. Needed to find the prev/next items below
+  const blogPosts = allBlogPosts.sort(sortByPublishedAt)
+  const blogPostIndex = blogPosts.findIndex((blogPost) => blogPost.slug === params.slug)
+
+  if (blogPostIndex === -1) notFound()
+
+  const blogPost = blogPosts[blogPostIndex]
+  
+  return (
+    <>
+      <BlogSinglePost
+        blogPost={blogPost}
+      />
+      <Newsletter label="Want more posts & tutorials?" />
+    </>
+  )
+}
---
+++ apps/gatoplugins/app/(default)/blog/page.tsx
@@ -0,0 +1,41 @@
+import StunningBackground from 'gatoapp/components/stunning-background'
+import Newsletter from 'gatoapp/components/newsletter'
+import BlogSchemaJsonLdScript from 'gatoapp/components/schema/blog-schema-json-ld'
+import { createSEOPageTitle } from '@/utils/content/metadata'
+import BlogSection from 'gatoapp/components/blog/blog-section'
+import { Suspense } from 'react'
+import AppConfig from '@/app/app.config'
+import { allBlogPosts } from '@/.contentlayer/generated'
+import { sortByPublishedAt } from 'gatoapp/utils/content/sort'
+
+const pageTitle = 'Blog'
+export const metadata = {
+  title: createSEOPageTitle(pageTitle),
+  description: 'Stay up to date on the latest from Gato Plugins',
+  alternates: {
+    canonical: `/${AppConfig.paths.blog}`,
+  },
+}
+
+export default function Blog() {
+  const sortedBlogPosts = allBlogPosts.sort(sortByPublishedAt)
+
+  return (
+    <>
+      <BlogSchemaJsonLdScript
+        headline={metadata.title}
+        description={metadata.description}
+      />
+
+      <section className="relative">
+        <StunningBackground />    
+        <Suspense>
+          <BlogSection
+            blogPosts={sortedBlogPosts}
+          />
+        </Suspense>
+      </section>
+      <Newsletter label="Want more posts & tutorials?" />
+    </>
+  )
+}
