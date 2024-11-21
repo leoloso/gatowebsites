ba8b31ed191982a45f46c1332ea6ccba0fb99634
leoloso <leo@getpop.org>
2024-11-21 18:00:31 +0800

    remove link to Pricing

--- apps/gatoplugins/components/menu/header-mobile-menu.tsx
+++ apps/gatoplugins/components/menu/header-mobile-menu.tsx
@@ -6,9 +6,6 @@ export default function HeaderMobileMenu() {
   return (
     <ul className="border border-transparent [background:linear-gradient(theme(colors.slate.100),_theme(colors.slate.100))_padding-box,_conic-gradient(theme(colors.slate.500),_theme(colors.slate.300)_25%,_theme(colors.slate.300)_75%,_theme(colors.slate.500)_100%)_border-box] dark:[background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box] rounded-lg px-4 py-1.5">
       <li>
-        <Link className="flex font-medium text-slate-500 hover:text-slate-900 dark:text-slate-300 dark:hover:text-white py-1.5" href="/pricing">Pricing</Link>
-      </li>
-      <li>
         <Link className="flex font-medium text-slate-500 hover:text-slate-900 dark:text-slate-300 dark:hover:text-white py-1.5" href={`/${AppConfig.paths.plugins}`}>Plugins</Link>
       </li>
       {/* <li>
