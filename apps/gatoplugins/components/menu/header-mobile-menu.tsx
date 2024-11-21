0508f43bde0fef128faa4fc23326d899b649e976
leoloso <leo@getpop.org>
2024-11-22 04:41:45 +0800

    added all plugins to mobile header

--- apps/gatoplugins/components/menu/header-mobile-menu.tsx
+++ apps/gatoplugins/components/menu/header-mobile-menu.tsx
@@ -6,7 +6,14 @@ export default function HeaderMobileMenu() {
   return (
     <ul className="border border-transparent [background:linear-gradient(theme(colors.slate.100),_theme(colors.slate.100))_padding-box,_conic-gradient(theme(colors.slate.500),_theme(colors.slate.300)_25%,_theme(colors.slate.300)_75%,_theme(colors.slate.500)_100%)_border-box] dark:[background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box] rounded-lg px-4 py-1.5">
       <li>
-        <Link className="flex font-medium text-slate-500 hover:text-slate-900 dark:text-slate-300 dark:hover:text-white py-1.5" href={`/${AppConfig.paths.plugins}`}>Plugins</Link>
+      <Link className="flex font-medium text-slate-500 hover:text-slate-900 dark:text-slate-300 dark:hover:text-white py-1.5" href={`/${AppConfig.paths.plugins}`}>Plugins</Link>
+        <ul className="pl-4">
+          {allPlugins.map((plugin, index) => (
+            <li key={index}>
+              <Link className="flex font-medium text-slate-500 hover:text-slate-900 dark:text-slate-300 dark:hover:text-white py-1.5" href={plugin.urlPath}>{plugin.title}</Link>
+            </li>
+          ))}
+        </ul>
       </li>
       {/* <li>
         <Link className="flex font-medium text-slate-500 hover:text-slate-900 dark:text-slate-300 dark:hover:text-white py-1.5" href={`/${AppConfig.paths.demoPosts}`}>Demos</Link>
