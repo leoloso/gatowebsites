8c0e83350bd2c124229f81cbf5188a4c07f3e150
leoloso <leo@getpop.org>
2024-11-21 18:30:48 +0800

    Externalize the default layout into shared

--- apps/gatographql/app/(default)/layout.tsx
+++ apps/gatographql/app/(default)/layout.tsx
@@ -1,32 +1,15 @@
-'use client'
-
-import { useEffect } from 'react'
-
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-
-import Footer from 'gatoapp/components/ui/footer'
-import Header from 'gatoapp/components/ui/header'
 import AppComponentProvider from 'gatoapp/app/appcomponent-provider'
 import LogoImg from '@/public/assets/GatoGraphQL-logo-suki-text-rectangular.png'
 import LayoutFooterMenu from './layout-footer-menu'
 import HeaderMenu from '@/components/menu/header-menu'
 import HeaderMobileMenu from '@/components/menu/header-mobile-menu'
+import DefaultLayout from 'gatoapp/app/(default)/layout'
 
-export default function DefaultLayout({
+export default function AppDefaultLayout({
   children,
 }: {
   children: React.ReactNode
 }) {  
-
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      disable: 'phone',
-      duration: 1000,
-      easing: 'ease-out-cubic',
-    })
-  })
 
   return (
     <AppComponentProvider
@@ -39,16 +22,9 @@ export default function DefaultLayout({
         mobileMenu: <HeaderMobileMenu />,
       }}
     >
-      <div className={`flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip`}>
-        <Header />
-        
-        <main className="grow">
-
-            {children}
-
-        </main>
-        <Footer />
-      </div>
+      <DefaultLayout>
+        {children}
+      </DefaultLayout>
     </AppComponentProvider>
   )
 }
---
+++ packages/shared/gatoapp/app/(default)/layout.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import React, { useEffect } from 'react'
+
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+import Footer from 'gatoapp/components/ui/footer'
+import Header from 'gatoapp/components/ui/header'
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {  
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 1000,
+      easing: 'ease-out-cubic',
+    })
+  })
+
+  return (
+    <div className={`flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip`}>
+      <Header />
+      
+      <main className="grow">
+
+          {children}
+
+      </main>
+      <Footer />
+    </div>
+  )
+}
