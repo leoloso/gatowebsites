54d800f6d3756ce178d814a328f6ae41878b9116
leoloso <leo@getpop.org>
2024-11-26 08:30:37 +0800

    removed unneeded components

--- apps/gatoplugins/app/(default)/page.tsx
+++ apps/gatoplugins/app/(default)/page.tsx
@@ -1,8 +1,5 @@
 import Hero from 'gatoapp/components/hero'
-import UseCases from '@/components/use-cases/use-cases'
-import FeaturesCarousel from 'gatoapp/components/features-carousel'
-import PricingSection from '@/components/pricing-section'
-import Cta from '@/components/cta'
+// import Cta from '@/components/cta'
 import HomeExtensionsSection from '@/components/home-extensions-section'
 import AppConfig from '@/app/app.config'
 
@@ -18,14 +15,8 @@ export default function Home() {
         title="A collection of super useful plugins for WordPress"
         description={<span>Empower your <strong>WordPress</strong> site with <strong>Gato Plugins</strong>.</span>}
       />
-      <UseCases />
-      <FeaturesCarousel
-        linkURL={`/${AppConfig.paths.features}`}
-        lastSlideText='Browse the list of all features, discover how Gato GraphQL can empower and protect your application.'
-      />
       <HomeExtensionsSection />
-      <PricingSection />
-      <Cta />
+      {/* <Cta /> */}
     </>
   )
 }
