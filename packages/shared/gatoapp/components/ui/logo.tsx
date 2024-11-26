54b14749ec0810d32e07cc3b4303c6868676c7b5
leoloso <leo@getpop.org>
2024-11-26 16:29:34 +0800

    opacity-75

--- packages/shared/gatoapp/components/ui/logo.tsx
+++ packages/shared/gatoapp/components/ui/logo.tsx
@@ -8,7 +8,7 @@ export default function Logo() {
   const AppComponent = useAppComponentProvider()
   return (
     <Link className="inline-flex" href="/" aria-label="Logo">
-      <Image className="max-w-none opacity-70 hover:opacity-100 transition duration-150 ease-in-out" src={AppComponent.header.logoImage} width={210} height={40} priority alt="Logo" />
+      <Image className="max-w-none opacity-75 hover:opacity-100 transition duration-150 ease-in-out" src={AppComponent.header.logoImage} width={210} height={40} priority alt="Logo" />
     </Link>
   )
 }
