1e0e4b0eff60abba5111181b7fcd32cba88c4382
leoloso <leo@getpop.org>
2024-12-04 14:06:34 +0800

    hero header

--- apps/gatoplugins/components/hero.tsx
+++ apps/gatoplugins/components/hero.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import LogoPic from 'gatoapp/public/assets/Gato-logo-suki-rectangular.png'
 import CampaignBanner from 'gatoapp/components/campaigns/campaign-banner'
+import Illustration from '@/public/images/layout/hero-illustration.svg'
 
 export default function Hero({
   title,
@@ -10,12 +11,18 @@ export default function Hero({
   description: React.ReactNode
 }) {
   return (
-    <section
-      className="bg-gradient-to-r from-purple-500/60 via-purple-500 to-purple-500/60"
-    >
-      <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
 
-        <div className="pt-32 pb-16 md:pt-32 md:pb-24">
+    <section className="relative overflow-hidden">
+      {/* Bg */}
+      <div className="absolute inset-0 bg-blue-600 pointer-events-none -z-10" aria-hidden="true" />
+
+      {/* Illustration */}
+      <div className="absolute left-1/2 -translate-x-1/3 md:-translate-x-1/2 pointer-events-none -z-10" aria-hidden="true">
+        <object type="image/svg+xml" data={Illustration.src} width="1440" height="1214" />
+      </div>
+
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-28 pb-16 md:pt-40 md:pb-20">
 
           <div className="mb-8">
             <CampaignBanner
@@ -30,8 +37,12 @@ export default function Hero({
                 <Image src={LogoPic} width={250} height={175} alt="Logo" />
               </div>
             </div>
-            <h1 className="h1 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4" data-aos="fade-down">{title}</h1>
-            <p className="text-lg text-slate-300 mb-8" data-aos="fade-down" data-aos-delay="200">{description}</p>
+            <h1 className="h1 font-cabinet-grotesk text-white mb-2" data-aos="fade-up">
+              {title}
+            </h1>
+            <p className="text-lg md:text-xl text-white text-opacity-80 mb-8" data-aos="fade-up" data-aos-delay="100">
+              {description}
+            </p>
 
           </div>
         </div>
