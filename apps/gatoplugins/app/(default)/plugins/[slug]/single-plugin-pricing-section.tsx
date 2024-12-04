111f53d5527d01033029e2444e41db50032240f3
leoloso <leo@getpop.org>
2024-12-04 14:34:47 +0800

    moved section below

--- apps/gatoplugins/app/(default)/plugins/[slug]/single-plugin-pricing-section.tsx
+++ apps/gatoplugins/app/(default)/plugins/[slug]/single-plugin-pricing-section.tsx
@@ -12,36 +12,43 @@ export default function SinglePluginPricing({
 }) {
   const testimonials = getTestimonials()
   return (
-    
-    <section className="relative">
-      {/* Bg */}
-      <div className="absolute inset-0 bg-blue-600 -z-10" aria-hidden="true" />
+    <>
+      <section className="relative">
+        {/* Bg */}
+        <div className="absolute inset-0 bg-blue-600 -z-10" aria-hidden="true" />
 
-      {/* Illustration */}
-      <div className="absolute top-0 left-1/2 -translate-x-1/2 pointer-events-none -z-10" aria-hidden="true">
-        <Image className="max-w-none" src={Illustration} alt="Illsutration" />
-      </div>
+        {/* Illustration */}
+        <div className="absolute top-0 left-1/2 -translate-x-1/2 pointer-events-none -z-10" aria-hidden="true">
+          <Image className="max-w-none" src={Illustration} alt="Illsutration" />
+        </div>
 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-20">
-          {/* Section header */}
-          <div className="pb-12 md:pb-16 text-center">
-            <h2 className="h2 font-cabinet-grotesk text-white pb-2 md:pb-4">Get your plugin</h2>
-            <p className="text-lg md:text-xl text-white text-opacity-80 mb-8">Purchase <span className='font-bold'>{ plugin.title }</span></p>
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="py-12 md:py-20">
+            {/* Section header */}
+            <div className="pb-12 md:pb-16 text-center">
+              <h2 className="h2 font-cabinet-grotesk text-white pb-2 md:pb-4">Get your plugin</h2>
+              <p className="text-lg md:text-xl text-white text-opacity-80 mb-8">Purchase <span className='font-bold'>{ plugin.title }</span></p>
+            </div>
+          
+            <PluginPricing
+              plugin={ plugin }
+            />
+
           </div>
-        
-          <PluginPricing
-            plugin={ plugin }
+
+        </div>
+      </section>
+
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <PricingMoneyBackGuarantee />
+
+          <Testimonial
+            testimonials={testimonials}
           />
 
         </div>
-        <PricingMoneyBackGuarantee />
-
-        <Testimonial
-          testimonials={testimonials}
-        />
-
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
