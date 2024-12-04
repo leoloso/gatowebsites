7d935d0fa327461db25805ef2c3b0ad6dc9945c2
leoloso <leo@getpop.org>
2024-12-04 17:35:07 +0800

    yellow = amber

--- apps/gatoplugins/components/menu/footer-menu.tsx
+++ apps/gatoplugins/components/menu/footer-menu.tsx
@@ -19,21 +19,21 @@ export default function FooterMenu({
         <h6 className="text-sm text-white font-bold mb-2">Plugins</h6>
         <ul className="text-sm space-y-2">
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.plugins}`}>Overview</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.plugins}`}>Overview</a>
           </li>
           {allPlugins.map((plugin, index) => (
             <li key={index}>
-              <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={plugin.urlPath}>{plugin.title}</a>
+              <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={plugin.urlPath}>{plugin.title}</a>
             </li>
           ))}
           {/* <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.demoPosts}`}>Demos</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.demoPosts}`}>Demos</a>
           </li> */}
           {/* <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/changelog">Changelog</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/changelog">Changelog</a>
           </li> */}
           {/* <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.instawpSandboxDemo} target="_blank">Try out</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.instawpSandboxDemo} target="_blank">Try out</a>
           </li> */}
         </ul>
       </div>
@@ -43,21 +43,21 @@ export default function FooterMenu({
         <h6 className="text-sm text-white font-bold mb-2">Documentation</h6>
         <ul className="text-sm space-y-2">
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.docs}`}>Overview</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.docs}`}>Overview</a>
           </li>
           {allPlugins.map((plugin, index) => (
             <li key={index}>
-              <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={plugin.docUrlPath}>{plugin.title}</a>
+              <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={plugin.docUrlPath}>{plugin.title}</a>
             </li>
           ))}
         </ul>
         <h6 className="text-sm text-white font-bold mb-2 mt-8 lg:mt-6">Resources</h6>
         <ul className="text-sm space-y-2">
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.gatoGraphQL} target="_blank">Gato GraphQL</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.gatoGraphQL} target="_blank">Gato GraphQL</a>
           </li>
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="https://www.youtube.com/@GatoGraphQL" target="_blank">@GatoGraphQL YouTube channel</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="https://www.youtube.com/@GatoGraphQL" target="_blank">@GatoGraphQL YouTube channel</a>
           </li>
         </ul>
       </div>
@@ -67,19 +67,19 @@ export default function FooterMenu({
         <h6 className="text-sm text-white font-bold mb-2">Company</h6>
         <ul className="text-sm space-y-2">
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.blog}`}>Blog</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={`/${AppConfig.paths.blog}`}>Blog</a>
           </li>
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/about">About us</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/about">About us</a>
           </li>
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/contact">Contact us</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/contact">Contact us</a>
           </li>
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/newsletter">Newsletter</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/newsletter">Newsletter</a>
           </li>
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.shopAffiliateProgram} target="_blank">Affiliate Program</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href={AppConfig.urls.shopAffiliateProgram} target="_blank">Affiliate Program</a>
           </li>
         </ul>
       </div>
@@ -89,16 +89,16 @@ export default function FooterMenu({
         <h6 className="text-sm text-white font-bold mb-2">Support</h6>
         <ul className="text-sm space-y-2">
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/shop/my-orders">My orders</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/shop/my-orders">My orders</a>
           </li>
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/shop/customer-portal">Customer portal</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/shop/customer-portal">Customer portal</a>
           </li>
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/support">Support request</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/support">Support request</a>
           </li>
           <li>
-            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-200 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/refund-policy">Refund policy</a>
+            <a className="text-fuchsia-100 dark:text-slate-400 hover:text-amber-300 dark:hover:text-slate-200 transition duration-150 ease-in-out" href="/refund-policy">Refund policy</a>
           </li>
         </ul>
       </div>
