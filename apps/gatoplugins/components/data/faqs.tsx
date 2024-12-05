bad5c88960c6ccf6f5b60ff83af6b5ad00ab6dce
leoloso <leo@getpop.org>
2024-12-05 15:31:56 +0800

    show faqs in 1 column

--- apps/gatoplugins/components/data/faqs.tsx
+++ apps/gatoplugins/components/data/faqs.tsx
@@ -12,22 +12,18 @@ export function getFAQs({
       {
         question: 'What is Gato Multilingual for Polylang?',
         answer: <span>It is a plugin for WordPress that automatically translates new posts into all languages configured in your Polylang settings.</span>,
-        column: 1,
-      },
-      {
-        question: 'Can I ask for a refund?',
-        answer: <span>If you are not happy with the purchased plugin, you have 30 days to <a className="text-blue-500 font-medium hover:underline" href="/refund-policy">request a refund</a>.</span>,
-        column: 1,
       },
       {
         question: 'How is the translation performed?',
         answer: <span>The translation is performed by calling the Google Translate API.</span>,
-        column: 2,
+      },
+      {
+        question: 'Can I ask for a refund?',
+        answer: <span>If you are not happy with the purchased plugin, you have 30 days to <a className="text-blue-500 font-medium hover:underline" href="/refund-policy">request a refund</a>.</span>,
       },
       {
         question: 'What happens if I don\'t renew the yearly license?',
         answer: <span>If you do not renew the yearly license, you can keep using the plugin, however you won't be able to request support, or download/install product updates.</span>,
-        column: 2,
       },
     ]
   }
