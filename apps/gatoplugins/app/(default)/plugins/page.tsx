2322a5fefef1e116e240c6487e0dc3959b18883c
leoloso <leo@getpop.org>
2024-12-05 14:31:52 +0800

    Commented out "Solve your problems"

--- apps/gatoplugins/app/(default)/plugins/page.tsx
+++ apps/gatoplugins/app/(default)/plugins/page.tsx
@@ -26,7 +26,7 @@ export default function Extensions() {
 
             {/*  Page header */}
             <PageHeader
-              leading='Solve your problems'
+              // leading='Solve your problems'
               title='Plugins'
               description='Our collection of super userful plugins for WordPress'
             />
