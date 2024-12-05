cf6469b98ca6eb12ade15e273e1e69f0b968bfeb
leoloso <leo@getpop.org>
2024-12-05 17:23:46 +0800

    Blurred shape with dark mode

--- packages/shared/gatoapp/components/cta-02.tsx
+++ packages/shared/gatoapp/components/cta-02.tsx
@@ -3,6 +3,7 @@
 import Particles from './particles'
 import SectionHeader from './section-header'
 import { useAppConfigProvider } from 'gatoapp/app/appconfig-provider'
+import { usingDarkColorThemeMode } from 'gatoapp/utils/context/style'
 
 export default function Cta02({
   tryoutProduct,
@@ -10,28 +11,36 @@ export default function Cta02({
   tryoutProduct: string
 }) {
   const { config: AppConfig } = useAppConfigProvider()
+  const isDarkColorThemeMode = usingDarkColorThemeMode()
   return (
     <section className="relative">
-
-      {/* Particles animation */}
-      <div className="absolute left-1/2 -translate-x-1/2 top-0 -z-10 w-80 h-80 -mt-24">
-        <Particles className="absolute inset-0 -z-10" quantity={6} staticity={30} />
-      </div>
+      { isDarkColorThemeMode && (
+        <>
+          {/* Particles animation */}
+          <div className="absolute left-1/2 -translate-x-1/2 top-0 -z-10 w-80 h-80 -mt-24">
+            <Particles className="absolute inset-0 -z-10" quantity={6} staticity={30} />
+          </div>
+        </>
+      )}
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="relative px-8 py-12 md:py-20 pt-16 md:pt-32 border-t border-b [border-image:linear-gradient(to_right,transparent,theme(colors.slate.800),transparent)1]">
-          {/* Blurred shape */}
-          <div className="absolute top-0 -mt-24 left-1/2 -translate-x-1/2 ml-24 blur-2xl opacity-70 pointer-events-none -z-10" aria-hidden="true">
-            <svg xmlns="http://www.w3.org/2000/svg" width="434" height="427">
-              <defs>
-                <linearGradient id="bs4-a" x1="19.609%" x2="50%" y1="14.544%" y2="100%">
-                  <stop offset="0%" stopColor="#A855F7"></stop>
-                  <stop offset="100%" stopColor="#6366F1" stopOpacity="0"></stop>
-                </linearGradient>
-              </defs>
-              <path fill="url(#bs4-a)" fillRule="evenodd" d="m0 0 461 369-284 58z" transform="matrix(1 0 0 -1 0 427)"></path>
-            </svg>
-          </div>
+          { isDarkColorThemeMode && (
+            <>
+              {/* Blurred shape */}
+              <div className="absolute top-0 -mt-24 left-1/2 -translate-x-1/2 ml-24 blur-2xl opacity-70 pointer-events-none -z-10" aria-hidden="true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="434" height="427">
+                  <defs>
+                    <linearGradient id="bs4-a" x1="19.609%" x2="50%" y1="14.544%" y2="100%">
+                      <stop offset="0%" stopColor="#A855F7"></stop>
+                      <stop offset="100%" stopColor="#6366F1" stopOpacity="0"></stop>
+                    </linearGradient>
+                  </defs>
+                  <path fill="url(#bs4-a)" fillRule="evenodd" d="m0 0 461 369-284 58z" transform="matrix(1 0 0 -1 0 427)"></path>
+                </svg>
+              </div>
+            </>
+          )}
           {/* Content */}
           <SectionHeader
             leading='Discover the power'
