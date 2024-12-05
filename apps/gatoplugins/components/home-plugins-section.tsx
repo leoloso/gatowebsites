b9647e744953b7a51241033cd5086b0deccbdd00
leoloso <leo@getpop.org>
2024-12-05 13:59:24 +0800

    removed commented code

--- apps/gatoplugins/components/home-plugins-section.tsx
+++ apps/gatoplugins/components/home-plugins-section.tsx
@@ -8,13 +8,6 @@ export default function HomePluginsSection() {
   return (
     <section className="relative">
 
-      {/* Particles animation */}
-      {/* <div className="absolute left-1/2 -translate-x-1/2 top-0 -z-10 w-80 h-80 -mt-24 -ml-32">
-        <Particles className="absolute inset-0 -z-10" quantity={6} staticity={30} />    
-      </div> */}
-      
-      {/* Radial gradient */}
-      {/* <RadiantGradient />       */}
       <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-16 md:pt-32">
           
