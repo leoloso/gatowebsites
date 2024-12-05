952c529c9854eef09e835550550bfc9fe4f66238
leoloso <leo@getpop.org>
2024-12-05 17:27:01 +0800

    Blurred shape with dark mode

--- packages/shared/gatoapp/components/story.tsx
+++ packages/shared/gatoapp/components/story.tsx
@@ -5,6 +5,7 @@ import FounderPic from 'gatoapp/public/assets/team/Leo-square.jpg'
 import SectionHeader from './section-header'
 import { getSnippet } from 'gatoapp/utils/content/snippet'
 import { SnippetMdx } from './mdx/snippet-mdx'
+import { usingDarkColorThemeMode } from 'gatoapp/utils/context/style'
 
 export default function Story({
   includeHeader = true
@@ -12,21 +13,26 @@ export default function Story({
   includeHeader?: boolean
 }) {
   const snippet = getSnippet('story')
+  const isDarkColorThemeMode = usingDarkColorThemeMode()
   return (
     <section className="relative">
 
-      {/* Blurred shape */}
-      <div className="absolute top-0 -mt-32 left-1/2 -translate-x-1/2 ml-10 blur-2xl opacity-70 pointer-events-none -z-10" aria-hidden="true">
-        <svg xmlns="http://www.w3.org/2000/svg" width="434" height="427">
-          <defs>
-            <linearGradient id="bs4-a" x1="19.609%" x2="50%" y1="14.544%" y2="100%">
-              <stop offset="0%" stopColor="#A855F7"></stop>
-              <stop offset="100%" stopColor="#6366F1" stopOpacity="0"></stop>
-            </linearGradient>
-          </defs>
-          <path fill="url(#bs4-a)" fillRule="evenodd" d="m0 0 461 369-284 58z" transform="matrix(1 0 0 -1 0 427)"></path>
-        </svg>
-      </div>
+      { isDarkColorThemeMode && (
+        <>
+          {/* Blurred shape */}
+          <div className="absolute top-0 -mt-32 left-1/2 -translate-x-1/2 ml-10 blur-2xl opacity-70 pointer-events-none -z-10" aria-hidden="true">
+            <svg xmlns="http://www.w3.org/2000/svg" width="434" height="427">
+              <defs>
+                <linearGradient id="bs4-a" x1="19.609%" x2="50%" y1="14.544%" y2="100%">
+                  <stop offset="0%" stopColor="#A855F7"></stop>
+                  <stop offset="100%" stopColor="#6366F1" stopOpacity="0"></stop>
+                </linearGradient>
+              </defs>
+              <path fill="url(#bs4-a)" fillRule="evenodd" d="m0 0 461 369-284 58z" transform="matrix(1 0 0 -1 0 427)"></path>
+            </svg>
+          </div>
+        </>
+      )}
 
       <div className="px-4 sm:px-6">
         <div className="max-w-5xl mx-auto">
