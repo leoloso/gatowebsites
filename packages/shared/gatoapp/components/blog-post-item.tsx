c90b3ec528d9fa684d1a9f261b7e18f46156fbc6
leoloso <leo@getpop.org>
2024-12-06 17:53:58 +0800

    arrow

--- packages/shared/gatoapp/components/blog-post-item.tsx
+++ packages/shared/gatoapp/components/blog-post-item.tsx
@@ -41,7 +41,7 @@ export default function BlogPostItem({ post }: {
       </div>
       <Link href={post.urlPath} className="block shrink-0 ml-6">
         <span className="sr-only">Read more</span>
-        <svg className="w-4 h-4 fill-current text-purple-400" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-4 h-4 fill-current text-blue-400 dark:text-purple-400" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
           <path d="M9.3 14.7l-1.4-1.4L12.2 9H0V7h12.2L7.9 2.7l1.4-1.4L16 8z" />
         </svg>
       </Link>
