8f65c6ba6d7604536e3f4bdf8475b5a5699a57b3
leoloso <leo@getpop.org>
2024-12-06 17:56:47 +0800

    no border on last item

--- packages/shared/gatoapp/components/blog/popular-posts.tsx
+++ packages/shared/gatoapp/components/blog/popular-posts.tsx
@@ -15,7 +15,7 @@ export default function PopularPosts({
       <h4 className="text-lg font-bold leading-snug tracking-tight mb-4">Popular on the blog</h4>
       <ul className="-my-2">
         {sortedBlogPosts.map((post, index) => (
-          <li className="flex py-2 border-b border-gray-300 dark:border-slate-200" key={index}>
+          <li className="flex py-2 border-b border-gray-300 dark:border-slate-200 [&:last-child]:border-b-0" key={index}>
             <svg className="w-4 h-4 shrink-0 fill-current text-gray-500 dark:text-slate-400 mt-1 mr-3" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
               <path d="M15.686 5.695L10.291.3c-.4-.4-.999-.4-1.399 0s-.4.999 0 1.399l.6.599-6.794 3.697-1-1c-.4-.399-.999-.399-1.398 0-.4.4-.4 1 0 1.4l1.498 1.498 2.398 2.398L.6 13.988 2 15.387l3.696-3.697 3.997 3.996c.5.5 1.199.2 1.398 0 .4-.4.4-.999 0-1.398l-.999-1 3.697-6.694.6.6c.599.6 1.199.2 1.398 0 .3-.4.3-1.1-.1-1.499zM8.493 11.79L4.196 7.494l6.695-3.697 1.298 1.299-3.696 6.694z" />
             </svg>
