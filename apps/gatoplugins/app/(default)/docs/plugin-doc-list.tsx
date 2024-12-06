dea0cebac86d0b4cd7e2353fff263013682ca7ef
leoloso <leo@getpop.org>
2024-12-06 10:59:45 +0800

    use style3

--- apps/gatoplugins/app/(default)/docs/plugin-doc-list.tsx
+++ apps/gatoplugins/app/(default)/docs/plugin-doc-list.tsx
@@ -1,10 +1,10 @@
-import PluginsSection, { style2, svgEffect2 } from "@/app/(default)/plugins/plugins-section";
+import PluginsSection, { style3, svgEffect2 } from "@/app/(default)/plugins/plugins-section";
 
 export default function PluginDocList() {
   return (
     <PluginsSection
       applyThumbEffect={svgEffect2}
-      applyStyle={style2}
+      applyStyle={style3}
     />
   )
 }
